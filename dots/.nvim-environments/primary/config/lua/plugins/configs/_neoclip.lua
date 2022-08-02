66d22882a672de25f3262e9392bdf1865276e799
Price Hiller <price@orion-technologies.io>
2022-08-02 17:06:52 -0500

    fix(nvim): resolve merge conflict

--- dots/.nvim-environments/primary/config/lua/plugins/configs/_neoclip.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/configs/_neoclip.lua
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-local loaded, neoclip = pcall(require, "neoclip")
-if not loaded then
-    return
-end
-neoclip.setup({
-||||||| b136610 (feat(nvim): use loaded, plugin format)
-local neoclip = pcall(require, "neoclip")
-if not neoclip then
-    return
-end
-neoclip.setup({
-=======
-require('neoclip').setup({
->>>>>>> parent of b136610 (feat(nvim): use loaded, plugin format)
+require("neoclip").setup({
     enable_persistent_history = true,
 })
