b6e84cc7ad1cd1f2ccb482de2052b886c67cdf7d
Price Hiller <price@price-hiller.com>
2022-08-02 16:44:29 -0500

    feat(zsh): use XDG spec

--- dots/.config/zsh/config/init.zsh
+++ dots/.config/zsh/config/init.zsh
@@ -1,18 +1,23 @@
 #!/usr/bin/env zsh
 
 configure() {
-	autoload -U +X bashcompinit && bashcompinit
+	export XDG_CONFIG_HOME="${HOME}/.config"
+	export XDG_CACHE_HOME="${HOME}/.cache"
+	export XDG_DATA_HOME="${HOME}/.local/share"
+	export XDG_STATE_HOME="${HOME}/.local/state"
+	export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+	export XDG_CONFIG_DIRS="/etc/xdg"
 }
 
 init() {
+	configure
 	local wkdir="${BASE_ZSH_CONFIG_DIR}/config"
 	source "${wkdir}/themes/init.zsh"
-	source "${wkdir}/profile/init.zsh"
 	source "${wkdir}/style/init.zsh"
 	source "${wkdir}/omz/init.zsh"
 	source "${wkdir}/plugins/init.zsh"
+	source "${wkdir}/profile/init.zsh"
 	source "${wkdir}/completions/init.zsh"
-	configure
 }
 
 init
