bdbbac0d7db903f356cd88e0342b1eb9000607ce
Price Hiller <price@orion-technologies.io>
2022-08-02 00:39:33 -0500

    refactor(fzf): improve pathing

--- dots/.config/fzf/fzf.zsh
+++ dots/.config/fzf/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/pricehiller/.local/share/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/pricehiller/.local/share/fzf/bin"
+if [[ ! "$PATH" == *"${HOME}/.local/share/fzf/bin"* ]]; then
+  PATH="${PATH:+${PATH}:}${HOME}/.local/share/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/pricehiller/.local/share/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.local/share/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/pricehiller/.local/share/fzf/shell/key-bindings.zsh"
+source "${HOME}/.local/share/fzf/shell/key-bindings.zsh"
