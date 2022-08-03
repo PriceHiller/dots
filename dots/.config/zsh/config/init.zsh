1818d1ee48e76bbb79bb9b35aec27aa5afd25b1b
Price Hiller <price@orion-technologies.io>
2022-08-03 01:23:49 -0500

    feat: set XDG_BIN_HOME

--- dots/.config/zsh/config/init.zsh
+++ dots/.config/zsh/config/init.zsh
@@ -3,6 +3,7 @@
 configure() {
 	export XDG_CONFIG_HOME="${HOME}/.config"
 	export XDG_CACHE_HOME="${HOME}/.cache"
+	export XDG_BIN_HOME="${HOME}/.local/bin"
 	export XDG_DATA_HOME="${HOME}/.local/share"
 	export XDG_STATE_HOME="${HOME}/.local/state"
 	export XDG_DATA_DIRS="/usr/local/share:/usr/share"
