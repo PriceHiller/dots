e1e81de6ce4ad50d19dca3c85bc0e13a4c22843b
Price Hiller <price@orion-technologies.io>
2022-08-07 01:29:49 -0500

    fix(zsh): show all history

--- dots/.config/zsh/config/profile/profile/options.zsh
+++ dots/.config/zsh/config/profile/profile/options.zsh
@@ -1,7 +1,10 @@
 #!/usr/bin/env options
 
 export SAVEHIST=10000
+export HISTSIZE=10000
+export HISTFILE="${HOME}/.zsh_history"
 setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
 setopt EXTENDED_HISTORY
 setopt HIST_FIND_NO_DUPS
 
