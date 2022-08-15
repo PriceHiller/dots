c06998e67537fa033714a3fbae746ab9e7e0f789
Price Hiller <price@price-hiller.com>
2022-08-14 21:27:22 -0500

    fix(zsh): integrate zsh vi mode correctly

--- dots/.config/zsh/config/plugins/init.zsh
+++ dots/.config/zsh/config/plugins/init.zsh
@@ -1,4 +1,13 @@
 configure() {
+	zvm_after_init() {
+		local wkdir="${BASE_ZSH_CONFIG_DIR}/config/plugins"
+
+		# source fzf
+		[[ -r "${XDG_CONFIG_HOME}/fzf/fzf.zsh" ]]  && source "${XDG_CONFIG_HOME}/fzf/fzf.zsh"
+
+		# activate syntax highlighting
+		source "${wkdir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+	}
 
 }
 
@@ -7,7 +16,6 @@ init() {
 	source "${wkdir}/fzf-tab/fzf-tab.plugin.zsh"
 	source "${wkdir}/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	source "${wkdir}/zsh-completions/zsh-completions.plugin.zsh"
-	source "${wkdir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 	source "${wkdir}/zsh-vi-mode/zsh-vi-mode.zsh"
 	eval "$(lua "${wkdir}/z.lua/z.lua" --init zsh enhanced once)"
 
