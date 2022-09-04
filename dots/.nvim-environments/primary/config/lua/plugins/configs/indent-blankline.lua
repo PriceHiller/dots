ec7432d704bffa08de07f66989863f54f505cc70
Price Hiller <price@price-hiller.com>
2022-09-04 15:17:51 -0500

    feat(nvim): better indent lines

--- dots/.nvim-environments/primary/config/lua/plugins/configs/indent-blankline.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/configs/indent-blankline.lua
@@ -1,24 +1,25 @@
 local g = vim.g
 
-g.indent_blankline_char = '▏'
-g.indent_blankline_context_char = '▏'
+-- g.indent_blankline_char = '▏'
+-- g.indent_blankline_context_char = '▏'
 
 -- Disable indent-blankline on these pages.
 g.indent_blankline_filetype_exclude = {
-    'help',
-    'terminal',
-    'alpha',
-    'packer',
-    'lsp-installer',
-    'lspinfo',
-    'mason.nvim',
+    "help",
+    "terminal",
+    "alpha",
+    "packer",
+    "lsp-installer",
+    "lspinfo",
+    "mason.nvim",
+    "mason",
 }
 
-g.indent_blankline_buftype_exclude = { 'terminal' }
+g.indent_blankline_buftype_exclude = { "terminal" }
 g.indent_blankline_show_trailing_blankline_indent = false
 g.indent_blankline_show_first_indent_level = true
 
-require('indent_blankline').setup({
+require("indent_blankline").setup({
     -- space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
