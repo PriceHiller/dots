2d4dd679df26a506fceae391516e5fa0e6b5c4ee
Price Hiller <price@price-hiller.com>
2022-09-05 03:46:31 -0500

    feat(nvim): massively improve cmp appearance

--- dots/.nvim-environments/primary/config/lua/plugins/plugins.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/plugins.lua
@@ -82,6 +82,44 @@ return packer.startup({
                         NeogitDiffDeleteHighlight = { fg = colors.git.removed, bg = colors.diff.delete },
                         NeogitDiffAddHighlight = { fg = colors.git.added, bg = colors.diff.add },
                         NeogitCommitViewHeader = { fg = colors.git.changed, bg = colors.diff.text },
+                        menuSel = { bg = "#282C34", fg = "NONE" },
+                        Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+                        CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE" },
+                        CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE" },
+                        CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE" },
+                        CmpItemMenu = { fg = "#C792EA", bg = "NONE" },
+                        CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+                        CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+                        CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+
+                        CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+                        CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+                        CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+
+                        CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+                        CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+                        CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+
+                        CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+                        CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+                        CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+                        CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+                        CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+
+                        CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+                        CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+
+                        CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+                        CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+                        CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+
+                        CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+                        CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+                        CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+
+                        CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+                        CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+                        CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
                         WinSeparator = { fg = "#54546D", bg = "NONE" },
                     },
                 })
@@ -381,6 +419,7 @@ return packer.startup({
                 "dmitmel/cmp-cmdline-history",
                 "David-Kunz/cmp-npm",
                 "lukas-reineke/cmp-rg",
+                "onsails/lspkind.nvim",
             },
             config = function()
                 require("plugins.configs._cmp")
