0d2345a0f9d288b24bdf79e2b59e311f335bcd9c
Price Hiller <price@orion-technologies.io>
2022-09-05 10:13:47 -0500

    Revert "refactor(wezterm): decrease opacity"
    This reverts commit ab682c6bf5161dadbaa192887a61dc6d74696a9f.

--- dots/.config/wezterm/config/theme/theme.lua
+++ dots/.config/wezterm/config/theme/theme.lua
@@ -3,7 +3,7 @@ local colors = require("config.theme.colors")
 return {
     enable_scroll_bar = true,
     window_decorations = "RESIZE",
-    window_background_opacity = 0.80,
+    window_background_opacity = 0.90,
     text_background_opacity = 1.0,
     inactive_pane_hsb = {
         brightness = 0.75,
