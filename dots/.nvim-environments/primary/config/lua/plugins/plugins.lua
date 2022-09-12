bb2ffd603c466ba1ffc78c7bbf01667830aabd98
Price Hiller <price@orion-technologies.io>
2022-09-12 02:28:01 -0500

    feat(nvim): use xmldoc for C# neogen annotations

--- dots/.nvim-environments/primary/config/lua/plugins/plugins.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/plugins.lua
@@ -640,6 +640,13 @@ return packer.startup({
             config = function()
                 require("neogen").setup({
                     snippet_engine = "luasnip",
+                    languages = {
+                        cs = {
+                            template = {
+                                annotation_convention = "xmldoc",
+                            },
+                        },
+                    },
                 })
             end,
         })
