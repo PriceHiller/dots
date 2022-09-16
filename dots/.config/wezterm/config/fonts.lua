f98b047d4ab87c69ef978a64f544de6362dd4a72
Price Hiller <price@orion-technologies.io>
2022-09-16 06:23:50 -0500

    fix(wezterm): use correct line height

--- dots/.config/wezterm/config/fonts.lua
+++ dots/.config/wezterm/config/fonts.lua
@@ -4,6 +4,7 @@ local lib = require("wlib")
 return {
     -- use_cap_height_to_scale_fallback_fonts = true,
     unicode_version = 14,
+    line_height = 0.9,
     font = wezterm.font_with_fallback({
         {
             family = "JetBrains Mono",
