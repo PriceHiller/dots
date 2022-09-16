cec69e915a0a775557f107ae2d39ed2b345c2505
Price Hiller <price@orion-technologies.io>
2022-09-16 12:26:01 -0500

    feat(pwsh): add fzf completion bindings

--- dots/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ dots/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,3 @@
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Install-Module -Name PSFzf -RequiredVersion 2.5.10
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
