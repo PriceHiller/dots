8eec595d7809630bbbaea64e165b75c6565cf2ff
Price Hiller <price@price-hiller.com>
2022-09-26 09:46:34 -0500

    chore(wezterm): remove commented code

--- dots/.config/wezterm/config/rendering.lua
+++ dots/.config/wezterm/config/rendering.lua
@@ -7,7 +7,4 @@ return {
         bottom = 0,
     },
     enable_scroll_bar = false,
-    -- HACK: This lets custom chars like '' render better,
-    -- HACK: but wez intends to resolve and remove this at a later time
-    -- anti_alias_custom_block_glyphs = false,
 }
