6650490c3aa5c6ef1b0c6c66326eb532358350df
Price Hiller <price@price-hiller.com>
2022-09-27 11:07:17 -0500

    refactor(nvim): disable lsp line highlight

--- dots/.nvim-environments/primary/config/lua/core/lsp.lua
+++ dots/.nvim-environments/primary/config/lua/core/lsp.lua
@@ -2,28 +2,28 @@ local M = {}
 
 M.setup = function()
     local function lspSymbol(name, icon, linehlbg)
-        local hl = 'DiagnosticSign' .. name
-        local linehl = 'DiagnosticSignLineHl' .. name
-        vim.api.nvim_set_hl(0, linehl, {
-            bg = linehlbg,
-        })
+        local hl = "DiagnosticSign" .. name
+        -- local linehl = 'DiagnosticSignLineHl' .. name
+        -- vim.api.nvim_set_hl(0, linehl, {
+        --     bg = linehlbg,
+        -- })
         vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl, linehl = linehl })
     end
 
-    lspSymbol('Error', '', '#2d202a')
-    lspSymbol('Warn', '', '#2e2a2d')
-    lspSymbol('Info', '', '#192b38')
-    lspSymbol('Hint', '', '#1a2b32')
+    lspSymbol("Error", "", "#2d202a")
+    lspSymbol("Warn", "", "#2e2a2d")
+    lspSymbol("Info", "", "#192b38")
+    lspSymbol("Hint", "", "#1a2b32")
 
     local border = {
-        { '╭', 'FloatBorder' },
-        { '─', 'FloatBorder' },
-        { '╮', 'FloatBorder' },
-        { '│', 'FloatBorder' },
-        { '╯', 'FloatBorder' },
-        { '─', 'FloatBorder' },
-        { '╰', 'FloatBorder' },
-        { '│', 'FloatBorder' },
+        { "╭", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╮", "FloatBorder" },
+        { "│", "FloatBorder" },
+        { "╯", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╰", "FloatBorder" },
+        { "│", "FloatBorder" },
     }
 
     local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
@@ -40,11 +40,11 @@ M.setup = function()
         update_in_insert = false,
         float = {
             focusable = false,
-            style = 'minimal',
-            border = 'rounded',
-            source = 'always',
-            header = '',
-            prefix = '',
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
         },
     })
 end
