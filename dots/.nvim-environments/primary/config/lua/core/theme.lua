dc065644fdee0bbd664ed453f240bc87c64786b2
Price Hiller <price@price-hiller.com>
2022-09-27 19:17:04 -0500

    refactor(nvim): use `vim.cmd` for calling colorscheme

--- dots/.nvim-environments/primary/config/lua/core/theme.lua
+++ dots/.nvim-environments/primary/config/lua/core/theme.lua
@@ -7,7 +7,7 @@ M.setup = function()
     vim.g.tokyonight_transparent_sidebar = true
 
     local colorscheme_name = "kanagawa"
-    local loaded, _ = pcall(vim.cmd, "colorscheme " .. colorscheme_name)
+    local loaded, _ = pcall(vim.cmd.colorscheme, colorscheme_name)
 
     if not loaded then
         vim.notify('Colorscheme "' .. colorscheme_name .. '" could not be loaded!', vim.lsp.log_levels.WARN, {
