0f81ff7cd1aa444ed2f11963c8afa71e90baaff4
Price Hiller <price@orion-technologies.io>
2022-10-25 09:02:53 -0500

    refactor(wezterm): wipe out linux config

--- dots/.config/wezterm/config/os/linux.lua
+++ dots/.config/wezterm/config/os/linux.lua
@@ -1,9 +1,1 @@
-local os = require("os")
-
-local linux_config = {}
-
-if os.getenv("HYPRLAND_INSTANCE_SIGNATURE") then
-    linux_config.window_background_opacity = 0.70
-end
-
-return linux_config
+return {}
