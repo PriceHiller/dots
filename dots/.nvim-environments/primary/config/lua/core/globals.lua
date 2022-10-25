f339fd3f4e80caf76a82b71f562333ef6fb98860
Price Hiller <price@orion-technologies.io>
2022-10-25 16:34:36 -0500

    fix(nvim)!: disable netrw

--- dots/.nvim-environments/primary/config/lua/core/globals.lua
+++ dots/.nvim-environments/primary/config/lua/core/globals.lua
@@ -4,6 +4,9 @@ local M = {}
 
 M.setup = function()
     -- g.python3_host_prog = ("(%s)/.pyenv/shims/python3"):format(os.getenv("HOME"))
+    -- Disable netrw
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 end
 
 return M
