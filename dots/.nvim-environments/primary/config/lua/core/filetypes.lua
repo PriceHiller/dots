12a0f2a9810c62e7e7bea1d6101b8e8b30998999
Price Hiller <price@orion-technologies.io>
2022-11-07 02:58:47 -0600

    feat(nvim): set dockerignore fts to gitignore

--- dots/.nvim-environments/primary/config/lua/core/filetypes.lua
+++ dots/.nvim-environments/primary/config/lua/core/filetypes.lua
@@ -4,6 +4,7 @@ M.setup = function()
     vim.filetype.add({
         pattern = {
             [".*%.dockerfile"] = "dockerfile",
+            [".*%.dockerignore"] = "gitignore"
         },
     })
 end
