a0cb34c2778414c91f4477b59b06d7704f007b2f
Price Hiller <price@price-hiller.com>
2022-12-20 20:35:46 -0600

    refactor(zsh)!: remove zsh-vi-mode

--- dots/.config/zsh/config/plugins/init.zsh
+++ dots/.config/zsh/config/plugins/init.zsh
@@ -1,25 +1,17 @@
-configure() {
-	zvm_after_init() {
-		local wkdir="${BASE_ZSH_CONFIG_DIR}/config/plugins"
-
-		# source fzf
-		[[ -r "${XDG_CONFIG_HOME}/fzf/fzf.zsh" ]]  && source "${XDG_CONFIG_HOME}/fzf/fzf.zsh"
-
-		# activate syntax highlighting
-		source "${wkdir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-	}
-
-}
+configure() {}
 
 init() {
 	local wkdir="${BASE_ZSH_CONFIG_DIR}/config/plugins"
 	source "${wkdir}/fzf-tab/fzf-tab.plugin.zsh"
 	source "${wkdir}/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	source "${wkdir}/zsh-completions/zsh-completions.plugin.zsh"
-	source "${wkdir}/zsh-vi-mode/zsh-vi-mode.zsh"
+	source "${wkdir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 	source "${wkdir}/nix-zsh-completions/nix.plugin.zsh"
 	FPATH="${FPATH}:${wkdir}/nix-zsh-completions"
 	eval "$(lua "${wkdir}/z.lua/z.lua" --init zsh enhanced once)"
+
+	# source fzf
+	[[ -r "${XDG_CONFIG_HOME}/fzf/fzf.zsh" ]]  && source "${XDG_CONFIG_HOME}/fzf/fzf.zsh"
 
 	configure
 }
