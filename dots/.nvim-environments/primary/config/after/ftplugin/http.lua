20f2e05f9a01ef4f0f284efe0651013c6f5205fc
Price Hiller <price@price-hiller.com>
2023-01-23 15:16:52 -0600

    feat: set commentstring for http to #

--- dots/.nvim-environments/primary/config/after/ftplugin/http.lua
+++ dots/.nvim-environments/primary/config/after/ftplugin/http.lua
@@ -7,3 +7,5 @@ vim.keymap.set('n', '<leader>fp', '<Plug>RestNvimPreview', {
 vim.keymap.set('n', '<leader>fl', '<Plug>RestNvimLast', {
     buffer = true,
 })
+
+vim.api.nvim_buf_set_option(0, "commentstring", "# %s")
