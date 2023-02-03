0aefa4729d659ba6d30b0aa208ed5a2af329f711
Price Hiller <price@orion-technologies.io>
2023-02-03 17:02:41 -0600

    feat(nvim): save folds between sessions

--- dots/.nvim-environments/primary/config/lua/core/autocmds.lua
+++ dots/.nvim-environments/primary/config/lua/core/autocmds.lua
@@ -12,6 +12,20 @@ M.setup = function()
     vim.api.nvim_create_autocmd("BufWritePre", {
         command = "%s/\\s\\+$//e",
     })
+
+    -- NOTE: Remember folds
+    vim.api.nvim_create_augroup("SaveFolds", { clear = true })
+    vim.api.nvim_create_autocmd("BufWinLeave", {
+        pattern = "*.*",
+        command = "mkview",
+        group = "SaveFolds"
+    })
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+        pattern = "*.*",
+        command = "silent! loadview",
+        group = "SaveFolds"
+    })
+
 end
 
 return M
