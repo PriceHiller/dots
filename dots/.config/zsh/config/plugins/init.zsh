90e0da3594e4270ebb816ae3c6bcaf248409e058
Price Hiller <price@orion-technologies.io>
2023-02-12 13:32:19 -0600

    feat(zsh): add forgit's subcommand to git

--- dots/.config/zsh/config/plugins/init.zsh
+++ dots/.config/zsh/config/plugins/init.zsh
@@ -7,7 +7,10 @@ init() {
 	source "${wkdir}/zsh-completions/zsh-completions.plugin.zsh"
 	source "${wkdir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 	source "${wkdir}/nix-zsh-completions/nix.plugin.zsh"
+
+	export PATH="${PATH}:${wkdir}/forgit/bin"
 	source "${wkdir}/forgit/forgit.plugin.zsh"
+
 	FPATH="${FPATH}:${wkdir}/nix-zsh-completions"
 	eval "$(lua "${wkdir}/z.lua/z.lua" --init zsh enhanced once)"
 
