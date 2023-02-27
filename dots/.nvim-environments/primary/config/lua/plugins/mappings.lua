20705cfcf9a6af7ae695b2d6f8dfb52bb781d774
Price Hiller <price@orion-technologies.io>
2023-02-27 03:40:13 -0600

    refactor(nvim): properly call silicon with range

--- dots/.nvim-environments/primary/config/lua/plugins/mappings.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/mappings.lua
@@ -216,4 +216,4 @@ wk.register({
 vim.keymap.set("n", "<leader>or", vim.cmd.OverseerRun, { silent = true, desc = "Overseer: Run" })
 vim.keymap.set("n", "<leader>ot", vim.cmd.OverseerToggle, { silent = true, desc = "Overseer: Toggle" })
 
-vim.keymap.set("v", "<leader>sc", "<cmd>Silicon<CR>", { silent = true, desc = "Silicon: Copy" })
+vim.keymap.set("v", "<leader>sc", ":'<,'>Silicon<CR>", { silent = true, desc = "Silicon: Copy" })
