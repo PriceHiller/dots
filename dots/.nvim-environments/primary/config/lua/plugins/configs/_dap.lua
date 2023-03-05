d84fa8bcbbfaa8354aa25bb5d14f57878a72c55a
Price Hiller <price@price-hiller.com>
2023-03-05 06:33:30 -0600

    feat(nvim): add bash debug adapter support

--- dots/.nvim-environments/primary/config/lua/plugins/configs/_dap.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/configs/_dap.lua
@@ -23,6 +23,13 @@ dap.adapters.coreclr = {
     args = { "--interpreter=vscode" },
 }
 
+
+dap.adapters.bashdb = {
+  type = 'executable',
+  command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+  name = 'bashdb'
+}
+
 -- configurations
 dap.configurations.cpp = {
     {
@@ -107,3 +114,25 @@ local config = {
 
 dap.configurations.cs = config
 dap.configurations.fsharp = config
+
+dap.configurations.sh = {
+  {
+    type = 'bashdb';
+    request = 'launch';
+    name = "Launch file";
+    showDebugOutput = true;
+    pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb';
+    pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir';
+    trace = true;
+    file = "${file}";
+    program = "${file}";
+    cwd = '${workspaceFolder}';
+    pathCat = "cat";
+    pathBash = "/bin/bash";
+    pathMkfifo = "mkfifo";
+    pathPkill = "pkill";
+    args = {};
+    env = {};
+    terminalKind = "integrated";
+  }
+}
