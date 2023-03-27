94291da3784886c5fcf92c5ad5bd44a0fd8d3a32
Price Hiller <price@orion-technologies.io>
2023-03-27 14:28:32 -0500

    feat(nvim): add cspell to null_ls

--- dots/.nvim-environments/primary/config/lua/plugins/configs/null_ls.lua
+++ dots/.nvim-environments/primary/config/lua/plugins/configs/null_ls.lua
@@ -13,5 +13,7 @@ null_ls.setup({
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.cspell,
+        null_ls.builtins.code_actions.cspell
     },
 })
