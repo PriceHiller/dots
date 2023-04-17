c06f34720ddf4ff3239da15044fd3cade8a5f795
Price Hiller <price@price-hiller.com>
2023-04-17 06:27:41 -0500

    refactor(nvim): do not disable netrw
    This previously disabled the ability to edit over SSH/SCP

--- dots/.nvim-environments/primary/config/lua/core/globals.lua
+++ dots/.nvim-environments/primary/config/lua/core/globals.lua
@@ -3,10 +3,6 @@ local g = vim.g
 local M = {}
 
 M.setup = function()
-    -- g.python3_host_prog = ("(%s)/.pyenv/shims/python3"):format(os.getenv("HOME"))
-    -- Disable netrw
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
 end
 
 return M
