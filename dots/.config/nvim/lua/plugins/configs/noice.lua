2c2015f5e12ad6419cd0a33426ebe28f0073c9a7
Price Hiller <price@orion-technologies.io>
2023-05-05 04:40:10 -0500

    feat(nvim): improve some noice.nvim functionality

--- dots/.config/nvim/lua/plugins/configs/noice.lua
+++ dots/.config/nvim/lua/plugins/configs/noice.lua
@@ -1,1 +1,15 @@
-require("noice").setup({})
+require("noice").setup({
+    lsp = {
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+        },
+    },
+
+    presets = {
+        long_message_to_split = true,
+        bottom_search = true,
+        inc_rename = true
+    },
+})
