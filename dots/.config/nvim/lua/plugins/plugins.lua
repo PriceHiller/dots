f3d6edc5d0374dc8e6fad87813a9ae72c0a927ed
Price Hiller <price@price-hiller.com>
2023-05-22 16:32:06 -0500

    feat(nvim|zathura): better latex integration

--- dots/.config/nvim/lua/plugins/plugins.lua
+++ dots/.config/nvim/lua/plugins/plugins.lua
@@ -590,12 +590,22 @@ lazy.setup({
         "gaoDean/autolist.nvim",
         ft = {
             "markdown",
-            "tex",
             "text",
+            "tex",
             "plaintex",
-            "norg",
         },
-        opts = {},
+        config = function()
+            local autolist = require("autolist")
+            autolist.setup()
+            autolist.create_mapping_hook("i", "<CR>", autolist.new)
+            autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
+            autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
+            autolist.create_mapping_hook("n", "o", autolist.new)
+            autolist.create_mapping_hook("n", "O", autolist.new_before)
+            autolist.create_mapping_hook("n", ">>", autolist.indent)
+            autolist.create_mapping_hook("n", "<<", autolist.indent)
+            autolist.create_mapping_hook("n", "<C-r>", autolist.force_recalculate)
+        end,
     },
 
     -- Tint inactive windows
