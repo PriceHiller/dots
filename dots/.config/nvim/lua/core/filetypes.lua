dc7709887710d17ca353bc42199572c4a9455ad8
Price Hiller <price@orion-technologies.io>
2023-05-22 17:54:53 -0500

    feat(nvim): better attach azure pipelines ls for azure pipeline files

--- dots/.config/nvim/lua/core/filetypes.lua
+++ dots/.config/nvim/lua/core/filetypes.lua
@@ -2,11 +2,18 @@ local M = {}
 
 M.setup = function()
     vim.filetype.add({
+        filename = {
+            ['.dockerignore'] = "dockerignore"
+        },
         pattern = {
             [".*%.dockerfile"] = "dockerfile",
-            [".*%.dockerignore"] = "gitignore",
+            [".*/Azure%-Pipelines/.*%.yml"] = "azure-pipelines",
+            [".*/Azure%-Pipelines/.*%.yaml"] = "azure-pipelines",
         },
     })
+
+    vim.treesitter.language.register("yaml", "azure-pipelines")
+    vim.treesitter.language.register("gitignore", "dockerignore")
 end
 
 return M
