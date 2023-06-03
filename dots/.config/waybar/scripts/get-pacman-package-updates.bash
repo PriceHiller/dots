0e797ea18febbb371edb942988bb91f98466184a
Price Hiller <price@orion-technologies.io>
2023-06-03 14:54:12 -0500

    feat(waybar): show pacman updates available

---
+++ dots/.config/waybar/scripts/get-pacman-package-updates.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+main() {
+	local package_updates
+	package_updates="$(checkupdates | cut -d " " -f1)"
+	package_update_number="$(printf "%s" "${package_updates}" | wc -l)"
+
+	# Limit the number of results shown to ten, add trailing ellipsis
+	if ((package_update_number > 10)); then
+		package_updates="$(printf "%s" "${package_updates}" | head -n 10)"
+		package_updates+="\n..."
+	fi
+
+
+	printf '{"text": "%s", "tooltip": "%s"}\n' "${package_update_number}" "${package_updates//$'\n'/\\n}"
+}
+
+main
