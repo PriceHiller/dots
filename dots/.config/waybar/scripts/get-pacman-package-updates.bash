4e9b87ae1a01394be559a182906aaf69454d4db0
Price Hiller <price@orion-technologies.io>
2023-06-05 02:58:36 -0500

    refactor(waybar): do not return package updates if none are available

--- dots/.config/waybar/scripts/get-pacman-package-updates.bash
+++ dots/.config/waybar/scripts/get-pacman-package-updates.bash
@@ -9,8 +9,10 @@ main() {
 	if ((package_update_number > 10)); then
 		package_updates="$(printf "%s" "${package_updates}" | head -n 10)"
 		package_updates+="\n..."
+	elif (( package_update_number == 0 )); then
+		# Don't return anything if there are no updates available
+		return 0
 	fi
-
 
 	printf '{"text": "%s", "tooltip": "%s"}\n' "${package_update_number}" "${package_updates//$'\n'/\\n}"
 }
