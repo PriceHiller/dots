e72d0e77f02f0f86230e349850036aa856db6279
Price Hiller <price@price-hiller.com>
2023-06-30 12:16:34 -0500

    refactor(nvim): update autolist mappings

--- dots/.config/nvim/lua/plugins/plugins.lua
+++ dots/.config/nvim/lua/plugins/plugins.lua
@@ -575,18 +575,7 @@ lazy.setup({
             "tex",
             "plaintex",
         },
-        config = function()
-            local autolist = require("autolist")
-            autolist.setup()
-            autolist.create_mapping_hook("i", "<CR>", autolist.new)
-            autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
-            autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
-            autolist.create_mapping_hook("n", "o", autolist.new)
-            autolist.create_mapping_hook("n", "O", autolist.new_before)
-            autolist.create_mapping_hook("n", ">>", autolist.indent)
-            autolist.create_mapping_hook("n", "<<", autolist.indent)
-            autolist.create_mapping_hook("n", "<C-r>", autolist.force_recalculate)
-        end,
+        opts = {}
     },
 
     -- Tint inactive windows
