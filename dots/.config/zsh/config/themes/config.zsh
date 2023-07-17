f65bba3afa40eadac3a57cdd40bbaad0b511b3eb
Price Hiller <price@orion-technologies.io>
2023-07-17 10:14:29 -0500

    fix: remove invalid battery symbols

--- dots/.config/zsh/config/themes/config.zsh
+++ dots/.config/zsh/config/themes/config.zsh
@@ -111,7 +111,7 @@
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
+    # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
