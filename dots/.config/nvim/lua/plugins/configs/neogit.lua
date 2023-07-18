ae91d0c59503f26e17af20ebf0088e5f60496097
Price Hiller <price@orion-technologies.io>
2023-07-18 15:12:11 -0500

    refactor(nvim): update neogit config

--- dots/.config/nvim/lua/plugins/configs/neogit.lua
+++ dots/.config/nvim/lua/plugins/configs/neogit.lua
@@ -1,15 +1,16 @@
-require("neogit").setup({
+local neogit = require("neogit")
+neogit.setup({
     disable_insert_on_commit = "auto",
     disable_commit_confirmation = true,
-    use_telescope = true,
     integrations = {
         diffview = true,
+        telescope = true,
     },
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*Neogit*",
-    callback = function ()
+    callback = function()
         vim.opt_local.list = false
-    end
+    end,
 })
