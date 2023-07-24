a2a9b9e13db94c515e02445676fee8013320614c
Price Hiller <price@orion-technologies.io>
2023-07-24 17:28:25 -0500

    feat(nvim): add key for markdown open

--- dots/.config/nvim/after/ftplugin/markdown.lua
+++ dots/.config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,8 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.textwidth = 0
+
+vim.keymap.set("n", "<leader>fr", "<cmd>MarkdownPreview<CR>", {
+    buffer = true,
+})
+
--- dots/.config/nvim/lua/plugins/plugins.lua
+++ dots/.config/nvim/lua/plugins/plugins.lua
@@ -456,10 +456,6 @@ lazy.setup({
     {
         "iamcco/markdown-preview.nvim",
         build = "cd app && npm install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-            vim.g.mkdp_browser = "firefox-developer-edition"
-        end,
         ft = { "markdown" },
     },
 
