88a870eb6934374714be974981f528d15fe0b637
Price Hiller <price@orion-technologies.io>
2023-07-24 16:44:12 -0500

    feat(nvim): add run key for asm

--- dots/.config/nvim/after/ftplugin/asm.lua
+++ dots/.config/nvim/after/ftplugin/asm.lua
@@ -1,1 +1,20 @@
 vim.opt_local.commentstring = "# %s"
+
+vim.keymap.set("n", "<leader>fr", function()
+    local curr_file = vim.fn.expand("%:p")
+    vim.fn.expand("%:p")
+    local curr_dir = vim.fn.expand("%:p:h")
+    vim.fn.expand("%:p")
+    require("toggleterm").exec(
+        string.format(
+            "as %s -o %s/obj.o && ld %s/obj.o -o %s/out && %s/out",
+            curr_file,
+            curr_dir,
+            curr_dir,
+            curr_dir,
+            curr_dir
+        )
+    )
+end, {
+    buffer = true,
+})
