d05e582e7f94ce34e108936cbd78444ac4072199
Price Hiller <price@price-hiller.com>
2023-07-24 19:50:28 -0500

    refactor(wezterm): minor visual improvements

--- dots/.config/wezterm/config/rendering.lua
+++ dots/.config/wezterm/config/rendering.lua
@@ -8,4 +8,7 @@ return {
     },
     enable_scroll_bar = false,
     underline_position = "250%",
+    custom_block_glyphs = true,
+    anti_alias_custom_block_glyphs = true,
+    detect_password_input = true,
 }
