f9897c1225d6308b7f813ae62e0f0bcaae62112f
Price Hiller <price@price-hiller.com>
2023-08-06 11:39:58 -0500

    refactor(nvim): do not use null-ls for bash

--- dots/.config/nvim/lua/plugins/configs/null_ls.lua
+++ dots/.config/nvim/lua/plugins/configs/null_ls.lua
@@ -2,10 +2,6 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.shfmt.with({
-            extra_args = { "-i 4" },
-        }),
-        null_ls.builtins.code_actions.shellcheck,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.formatting.black,
