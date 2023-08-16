000ba6539b73d8ebac1a274b9af32c5d412b3561
Price Hiller <price@price-hiller.com>
2023-08-15 23:05:55 -0500

    fix(nvim): correctly enable cmp ghost text

--- dots/.config/nvim/lua/plugins/configs/cmp.lua
+++ dots/.config/nvim/lua/plugins/configs/cmp.lua
@@ -147,7 +147,7 @@ return {
                         side_padding = 0,
                     },
                 },
-                experimental = { ghost_text = "Comment", native_menu = false },
+                experimental = { ghost_text = { hl_group = "Comment" }, native_menu = false },
                 snippet = {
                     -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
