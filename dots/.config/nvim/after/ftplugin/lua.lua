dfa7fd5f208d42233198f7240014395200da764f
Price Hiller <price@orion-technologies.io>
2023-08-18 23:23:52 -0500

    feat(nvim): add keybind to run luafile nvim invocation

--- dots/.config/nvim/after/ftplugin/lua.lua
+++ dots/.config/nvim/after/ftplugin/lua.lua
@@ -1,3 +1,8 @@
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
+
+vim.keymap.set("n", "<leader>fr", ":luafile %<CR>", {
+    buffer = true,
+    silent = true
+})
