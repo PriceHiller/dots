ad2fd7ae6caba2e9be50f1e1b4b01ac2023ffee0
Price Hiller <price@price-hiller.com>
2023-08-26 08:58:54 -0500

    feat(nvim): add gx.nvim

---
+++ dots/.config/nvim/lua/plugins/configs/gx.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "chrishrb/gx.nvim",
+        keys = {
+            { "gx", desc = "Open filepath or URI" }
+        },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = true, -- default settings
+    },
+}
