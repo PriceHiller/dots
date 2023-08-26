4c04c5a3a57e8865939e4b974406bb8952e1f9ea
Price Hiller <price@price-hiller.com>
2023-08-26 08:58:12 -0500

    fix(nvim): add ccc.nvim to cover for issues in nvim-highlight-colors

--- dots/.config/nvim/lua/plugins/configs/highlight-colors.lua
+++ dots/.config/nvim/lua/plugins/configs/highlight-colors.lua
@@ -1,4 +1,7 @@
 return {
+    -- HACK: Yeah, I know there's two color plugins here. nvim-highlight-colors supports css variables and ccc.nvim
+    -- does not. Ccc, on the other hand, ensures colors are ALWAYS shown even in split windows whereas
+    -- nvim-highlight-colors does not 😦
     {
         "brenoprata10/nvim-highlight-colors",
         event = { "BufReadPre", "BufNewFile" },
@@ -8,5 +11,22 @@ return {
             "HighlightColorsToggle",
         },
         config = true,
+    },{
+        "uga-rosa/ccc.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local ccc = require("ccc")
+            ccc.setup({
+                pickers = {
+                    ccc.picker.hex,
+                    ccc.picker.css_rgb,
+                    ccc.picker.css_hsl,
+                },
+                highlighter = {
+                    auto_enable = true,
+                    lsp = true
+                },
+            })
+        end,
     },
 }
