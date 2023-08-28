dab7611441373a6f950eecc5e563b049c4dbad47
Price Hiller <price@orion-technologies.io>
2023-08-28 15:56:07 -0500

    feat(nvim): add nvim lint

---
+++ dots/.config/nvim/after/ftplugin/dockerfile.lua
@@ -0,0 +1,9 @@
+local file_loc = vim.fn.expand("%:p:h")
+
+vim.opt_local.makeprg = "docker build " .. file_loc
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI"}, {
+    callback = function()
+        require("lint").try_lint()
+    end
+})
