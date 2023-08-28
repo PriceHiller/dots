d05ebce27b5016dd587e07c001e69d127b3609d6
Price Hiller <price@price-hiller.com>
2023-08-28 15:56:07 -0500

    feat(nvim): add nvim lint

---
+++ dots/.config/nvim/lua/plugins/configs/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("lint").linters_by_ft = {
+                markdown = {"proselint"}
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end
+            })
+        end
+    }
+}
