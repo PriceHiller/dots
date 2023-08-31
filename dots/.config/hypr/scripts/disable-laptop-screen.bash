527aa25902f15509b99a38539a65009678028210
Price Hiller <price@price-hiller.com>
2023-08-31 11:53:56 -0500

    fix(hypr): properly disable laptop monitor on close
    This only shuts off the laptop monitor if there's more than 1 monitor
    detected. If there's only 1 detected then that's the laptop screen and
    that will get shut off on lid close by systemd for suspend.

---
+++ dots/.config/hypr/scripts/disable-laptop-screen.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+main() {
+	local mon_count
+	mon_count="$(hyprctl monitors -j | jq length)"
+	if (( mon_count > 1 )); then
+		hyprctl keyword monitor "eDP-1, disable"
+	fi
+}
+
+main
