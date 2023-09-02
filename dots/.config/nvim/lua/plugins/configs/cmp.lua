cb835535984e19d9f7f2eee3a7dce5d5f4222711
Price Hiller <price@price-hiller.com>
2023-09-02 16:38:01 -0500

    refactor(nvim): make cmp docs easier to scroll
    These bindings are not exactly "Vim" styled, but they are *much* easier
    to reach and use.

--- dots/.config/nvim/lua/plugins/configs/cmp.lua
+++ dots/.config/nvim/lua/plugins/configs/cmp.lua
@@ -192,8 +192,8 @@ return {
                     end,
                 },
                 mapping = {
-                    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-                    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+                    ["<C-s>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
                     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
                     ["<C-e>"] = cmp.mapping({
