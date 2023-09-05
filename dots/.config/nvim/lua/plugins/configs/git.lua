21a7b2f320204142fa666503c3abfc691f2b7d28
Price Hiller <price@price-hiller.com>
2023-09-04 19:00:49 -0500

    refactor(nvim): remove vim-fugitive

--- dots/.config/nvim/lua/plugins/configs/git.lua
+++ dots/.config/nvim/lua/plugins/configs/git.lua
@@ -55,10 +55,6 @@ return {
         },
     },
     {
-        "tpope/vim-fugitive",
-        -- event = "VeryLazy",
-    },
-    {
         "lewis6991/gitsigns.nvim",
         event = { "BufReadPre", "BufNewFile" },
         keys = {
