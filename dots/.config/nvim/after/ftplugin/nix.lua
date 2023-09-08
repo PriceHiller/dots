88320d8ed0fb235ef526f42763418938282b2a61
Price Hiller <price@price-hiller.com>
2023-09-08 08:44:53 -0500

    feat(nvim): add nix json run option for pretty formatting

--- dots/.config/nvim/after/ftplugin/nix.lua
+++ dots/.config/nvim/after/ftplugin/nix.lua
@@ -5,13 +5,31 @@ opt_local.shiftwidth = 2
 
 vim.api.nvim_buf_set_option(0, "commentstring", "# %s")
 
-vim.keymap.set("n", "<leader>fr", function ()
+vim.keymap.set("n", "<leader>fr", function()
     local cmd = {
         "nix-instantiate",
         "--eval",
+        "--strict",
         vim.api.nvim_buf_get_name(0)
     }
     require("toggleterm").exec(table.concat(cmd, " "))
 end, {
     buffer = true,
+    desc = "Nix: Run with default output"
+})
+
+vim.keymap.set("n", "<leader>fj", function()
+    local cmd = {
+        "nix-instantiate",
+        "--eval",
+        "--strict",
+        "--json",
+        vim.api.nvim_buf_get_name(0),
+        "|",
+        "jq"
+    }
+    require("toggleterm").exec(table.concat(cmd, " "))
+end, {
+    buffer = true,
+    desc = "Nix: Run with json output"
 })
