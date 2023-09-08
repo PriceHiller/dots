a7abeec87054db6705654a228ccf0a7ab73aebb8
Price Hiller <price@price-hiller.com>
2023-09-08 02:40:58 -0500

    feat(nvim): add run command for nix filetypes

--- dots/.config/nvim/after/ftplugin/nix.lua
+++ dots/.config/nvim/after/ftplugin/nix.lua
@@ -4,3 +4,14 @@ opt_local.tabstop = 2
 opt_local.shiftwidth = 2
 
 vim.api.nvim_buf_set_option(0, "commentstring", "# %s")
+
+vim.keymap.set("n", "<leader>fr", function ()
+    local cmd = {
+        "nix-instantiate",
+        "--eval",
+        vim.api.nvim_buf_get_name(0)
+    }
+    require("toggleterm").exec(table.concat(cmd, " "))
+end, {
+    buffer = true,
+})
