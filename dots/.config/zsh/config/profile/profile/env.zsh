372cc85a3e36c6e05ac827b95ce4c543f61df68b
Price Hiller <price@orion-technologies.io>
2023-09-15 02:05:00 -0500

    refactor(zsh): use new paths for rustup and cargo

--- dots/.config/zsh/config/profile/profile/env.zsh
+++ dots/.config/zsh/config/profile/profile/env.zsh
@@ -131,9 +131,9 @@ else
 fi
 
 ### Rust ###
-export CARGO_HOME="${HOME}/.local/share/cargo"
+export CARGO_HOME="${HOME}/.cargo"
 source "${CARGO_HOME}/env"
-export RUSTUP_HOME="${HOME}/.local/share/rustup"
+export RUSTUP_HOME="${HOME}/.rustup"
 export PATH="${PATH}:${CARGO_HOME}/bin"
 
 ## Dotnet ###
