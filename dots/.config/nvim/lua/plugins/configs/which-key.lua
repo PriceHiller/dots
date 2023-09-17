5451d2eba05b50e6c278f20181369cb728ae5a22
Price Hiller <price@orion-technologies.io>
2023-09-17 04:06:48 -0500

    refactor(nvim): speed up activation of which-key

--- dots/.config/nvim/lua/plugins/configs/which-key.lua
+++ dots/.config/nvim/lua/plugins/configs/which-key.lua
@@ -3,9 +3,9 @@ return {
         "folke/which-key.nvim",
         event = "VeryLazy",
         config = function()
+            vim.opt.timeoutlen = 250
             local wk = require("which-key")
             wk.setup()
         end,
-        opts = {},
     },
 }
