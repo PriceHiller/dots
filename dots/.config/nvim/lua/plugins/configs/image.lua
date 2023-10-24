3b655497887715653cfbdf3760830e54f28b6de3
Price Hiller <price@orion-technologies.io>
2023-10-24 04:31:43 -0500

    refactor(nvim): improve handling of image.nvim installation failure

--- dots/.config/nvim/lua/plugins/configs/image.lua
+++ dots/.config/nvim/lua/plugins/configs/image.lua
@@ -5,10 +5,16 @@ return {
             ---@type vim.SystemCompleted
             local out = vim.system({ "luarocks", "--lua-version", "5.1", "--local", "install", "magick" }):wait()
             if out.code ~= 0 then
+                -- Delete it to ensure this builder script is ran again on next update/sync/etc.
+                vim.fn.delete(vim.fn.stdpath("data") .. "/lazy/image.nvim", "rf")
                 error(
-                string.format(
-                "Failed to install `magick` luarock for image.nvim!\n---STDOUT---\n%s\n\n---STDERR---\n\n%s", out.stdout,
-                    out.stderr), vim.log.levels.ERROR)
+                    string.format(
+                        "Failed to install `magick` luarock for image.nvim!\n---STDOUT---\n%s\n\n---STDERR---\n\n%s",
+                        out.stdout,
+                        out.stderr
+                    ),
+                    vim.log.levels.ERROR
+                )
             end
         end,
         ft = { "markdown", "norg" },
