a4d5505613b573cbb20f959c32d00f29d6acc418
Price Hiller <price@price-hiller.com>
2023-10-31 05:06:04 -0500

    feat(nvim): set table mode header fillchar

--- dots/.config/nvim/lua/plugins/configs/vim-table-mode.lua
+++ dots/.config/nvim/lua/plugins/configs/vim-table-mode.lua
@@ -3,6 +3,7 @@ return {
         "dhruvasagar/vim-table-mode",
         init = function ()
             vim.g.table_mode_always_active = 1
+            vim.g.table_mode_header_fillchar = '-'
         end
     }
 }
