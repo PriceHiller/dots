c35fa9799120c0a77edcdf900467d5e5334cd2a0
Price Hiller <price@orion-technologies.io>
2023-11-01 08:05:34 -0500

    refactor(nvim): use '=' for table header fillchar

--- dots/.config/nvim/lua/plugins/configs/vim-table-mode.lua
+++ dots/.config/nvim/lua/plugins/configs/vim-table-mode.lua
@@ -6,7 +6,7 @@ return {
         },
         init = function()
             vim.g.table_mode_always_active = 1
-            vim.g.table_mode_header_fillchar = '-'
+            vim.g.table_mode_header_fillchar = '='
             vim.g.table_mode_corner = '|'
         end
     }
