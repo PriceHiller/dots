c072fdcb6630b9a002e70fb9db6492e97d2085b2
Price Hiller <price@orion-technologies.io>
2023-11-09 15:16:04 -0600

    Revert "feat(nvim): use `osc52` copy for vim clipboard"
    This reverts commit f63c87750ed8077b075abc5fe7a071ec7bbcefc0.

--- dots/.config/nvim/lua/core/globals.lua
+++ dots/.config/nvim/lua/core/globals.lua
@@ -2,18 +2,6 @@ local g = vim.g
 
 local M = {}
 
-M.setup = function()
-    vim.g.clipboard = {
-        name = 'OSC 52',
-        copy = {
-            ['+'] = require('vim.clipboard.osc52').copy,
-            ['*'] = require('vim.clipboard.osc52').copy,
-        },
-        paste = {
-            ['+'] = require('vim.clipboard.osc52').paste,
-            ['*'] = require('vim.clipboard.osc52').paste,
-        },
-    }
-end
+M.setup = function() end
 
 return M
