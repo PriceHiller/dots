f63c87750ed8077b075abc5fe7a071ec7bbcefc0
Price Hiller <price@orion-technologies.io>
2023-11-09 15:05:47 -0600

    feat(nvim): use `osc52` copy for vim clipboard
    See https://github.com/neovim/neovim/pull/25872

--- dots/.config/nvim/lua/core/globals.lua
+++ dots/.config/nvim/lua/core/globals.lua
@@ -2,6 +2,18 @@ local g = vim.g
 
 local M = {}
 
-M.setup = function() end
+M.setup = function()
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.clipboard.osc52').copy,
+            ['*'] = require('vim.clipboard.osc52').copy,
+        },
+        paste = {
+            ['+'] = require('vim.clipboard.osc52').paste,
+            ['*'] = require('vim.clipboard.osc52').paste,
+        },
+    }
+end
 
 return M
