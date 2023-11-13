01b74367b50ed2524dafb30fbc916dc7b4cd0fa4
Price Hiller <price@orion-technologies.io>
2023-11-13 15:39:28 -0600

    fix(hypr): ensure desktop portals run

--- dots/.config/hypr/scripts/launchers/launch-desktop-portal.bash
+++ dots/.config/hypr/scripts/launchers/launch-desktop-portal.bash
@@ -3,6 +3,9 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-hyprland --verbose &
 sleep 2
 /usr/lib/xdg-desktop-portal &
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+notify-send "Hyprland Desktop Portal" "Hyprland  Desktop Portals Are Ready" -a "Hyprland"
