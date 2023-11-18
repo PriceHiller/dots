143f61d4432ddd0d2ba8216ec232b88fb87a447e
Price Hiller <price@orion-technologies.io>
2023-11-18 17:37:34 -0600

    fix(nvim): use correct key for fundo deps

--- dots/.config/nvim/lua/plugins/configs/fundo.lua
+++ dots/.config/nvim/lua/plugins/configs/fundo.lua
@@ -3,7 +3,7 @@ return {
     {
         "kevinhwang91/nvim-fundo",
         event = { "BufReadPre", "BufNewFile" },
-        dependences = { "kevinhwang91/promise-async" },
+        dependencies = { "kevinhwang91/promise-async" },
         config = true,
         build = function()
             require("fundo").install()
