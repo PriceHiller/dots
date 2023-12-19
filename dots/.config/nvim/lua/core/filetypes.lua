54b9973503eab6b932ce3251cf0c43df4a54738b
Price Hiller <price@orion-technologies.io>
2023-12-19 00:20:11 -0600

    refactor(nvim): remove psql filetype definition

--- dots/.config/nvim/lua/core/filetypes.lua
+++ dots/.config/nvim/lua/core/filetypes.lua
@@ -11,14 +11,12 @@ M.setup = function()
             [".*/Azure%-Pipelines/.*%.yaml"] = "azure-pipelines",
             [".*/waybar/config"] = "jsonc",
             [".*/etc/systemd/.*"] = "systemd",
-            [".*%.psql"] = "sql"
         },
     })
 
     vim.treesitter.language.register("yaml", "azure-pipelines")
     vim.treesitter.language.register("gitignore", "dockerignore")
     vim.treesitter.language.register("html", "xml")
-    vim.treesitter.language.register("sql", "psql")
 end
 
 return M
