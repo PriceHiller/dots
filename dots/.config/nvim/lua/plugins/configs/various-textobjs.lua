c3d2e5b74142e392061baa38c68b344c92acfc15
Price Hiller <price@price-hiller.com>
2023-12-26 16:54:54 -0600

    feat(nvim): add bindings for visual mode macro invocations
    NOTE: This is implementing https://github.com/neovim/neovim/pull/26495
    early. Once that is merged this is no longer needed.

--- dots/.config/nvim/lua/plugins/configs/various-textobjs.lua
+++ dots/.config/nvim/lua/plugins/configs/various-textobjs.lua
@@ -3,7 +3,7 @@ return {
         "chrisgrieser/nvim-various-textobjs",
         opts = {
             useDefaultKeymaps = true,
-            disabledKeymaps = { "r", "R" },
+            disabledKeymaps = { "r", "R", "Q" },
         },
     },
 }
