65f1d3c25364e4b9574789c48e2a8e2837891359
Price Hiller <price@orion-technologies.io>
2024-01-16 16:41:19 -0600

    refactor(zsh): improve handling of XDG dirs

--- dots/.config/zsh/config/init.zsh
+++ dots/.config/zsh/config/init.zsh
@@ -1,13 +1,12 @@
 #!/usr/bin/env zsh
 
 configure() {
-	export XDG_CONFIG_HOME="${HOME}/.config"
-	export XDG_CACHE_HOME="${HOME}/.cache"
-	export XDG_BIN_HOME="${HOME}/.local/bin"
-	export XDG_DATA_HOME="${HOME}/.local/share"
-	export XDG_STATE_HOME="${HOME}/.local/state"
-	export XDG_DATA_DIRS="${XDG_DATA_DIRS}:/usr/local/share:/usr/share"
-	export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:/etc/xdg"
+	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+	export XDG_CACHE_HOME="${XDG_CONFIG_HOME:-$HOME/.cache}"
+	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+	export XDG_STATE_HOME="${XDG_DATA_HOME:-$HOME/.local/state}"
+	export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+	export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
 }
 
 init() {
