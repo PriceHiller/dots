803b07ee70759b1a3143f3f61cddd3045d795dee
Price Hiller <price@orion-technologies.io>
2024-01-21 01:51:16 -0600

    refactor(nvim): use "..." for folded text

--- dots/.config/nvim/lua/plugins/configs/nvim-ufo.lua
+++ dots/.config/nvim/lua/plugins/configs/nvim-ufo.lua
@@ -84,7 +84,7 @@ return {
             -- Show numbers for fold text
             local handler = function(virtText, lnum, endLnum, width, truncate)
                 local newVirtText = {}
-                local suffix = (" {...} 󰁂 %d "):format(endLnum - lnum)
+                local suffix = ("... 󰁂 %d "):format(endLnum - lnum)
                 local sufWidth = vim.fn.strdisplaywidth(suffix)
                 local targetWidth = width - sufWidth
                 local curWidth = 0
