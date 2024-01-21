93eb82f02b090f3f3c5adce3bb72656c024253d7
Price Hiller <price@orion-technologies.io>
2024-01-21 04:24:54 -0600

    feat(nvim): underline typst titles

--- dots/.config/nvim/after/ftplugin/typst.lua
+++ dots/.config/nvim/after/ftplugin/typst.lua
@@ -1,1 +1,10 @@
 vim.opt_local.shiftwidth = 2
+vim.api.nvim_set_hl(
+    0,
+    "@text.title.typst",
+    vim.tbl_deep_extend(
+        "force",
+        vim.api.nvim_get_hl(0, { name = "@text.title.typst", link = false }),
+        { underline = true }
+    )
+)
