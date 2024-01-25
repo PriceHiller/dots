08817b7add465651afe377220fea284a6ef135e9
Price Hiller <price@orion-technologies.io>
2024-01-25 16:46:00 -0600

    fix(emacs): use correct font family for Fira Code

--- dots/.config/emacs/early-init.el
+++ dots/.config/emacs/early-init.el
@@ -1,3 +1,3 @@
-(set-face-attribute 'default nil :family "FiraCode Nerd Font" :height 120)
-(set-face-attribute 'fixed-pitch nil :family "FiraCode Nerd Font":height 130)
-(set-face-attribute 'variable-pitch nil :family "FiraCode Nerd Font":height 130)
+(set-face-attribute 'default nil :family "Fira Code" :height 120)
+(set-face-attribute 'fixed-pitch nil :family "Fira Code":height 130)
+(set-face-attribute 'variable-pitch nil :family "Fira Code":height 130)
