ec2ecd2dd0f15ac85a1ceec4094cdd61c75029fb
Price Hiller <price@orion-technologies.io>
2024-01-28 00:18:27 -0600

    refactor(nvim): define hurl commands in ftplugin

--- dots/.config/nvim/lua/plugins/configs/hurl.lua
+++ dots/.config/nvim/lua/plugins/configs/hurl.lua
@@ -15,13 +15,5 @@ return {
                 },
             },
         },
-        keys = {
-            { "<leader>fr",  "<cmd>HurlRunner<CR>",        desc = "Hurl: Runner" },
-            { "<leader>fa",  "<cmd>HurlRunnerAt<CR>",      desc = "Hurl: Run Api request" },
-            { "<leader>fe", "<cmd>HurlRunnerToEntry<CR>", desc = "Hurl: Run Api request to entry" },
-            { "<leader>fm", "<cmd>HurlToggleMode<CR>",    desc = "Hurl: Toggle Mode" },
-            { "<leader>rv", "<cmd>HurlVerbose<CR>",       desc = "Hurl: Run Api in verbose mode" },
-            { "<leader>fr",  ":HurlRunner<CR>",            desc = "Hurl: Runner",             mode = "v" },
-        },
     }
 }
