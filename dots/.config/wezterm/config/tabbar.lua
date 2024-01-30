24831bf12c6a1aefdc8c0faf85ef7a4be6722c5c
Price Hiller <price@price-hiller.com>
2024-01-30 14:28:09 -0600

    fix(wezterm): correctly truncate tab titles

--- dots/.config/wezterm/config/tabbar.lua
+++ dots/.config/wezterm/config/tabbar.lua
@@ -47,7 +47,7 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
     local edge_fg = color_names.kanagawa.oniViolet
 
     local title = tab_title(tab)
-    if #title > max_width - 1 then
+    if #title > max_width - 2 then
         title = " " .. wezterm.truncate_right(title, max_width - 6) .. "…"
     else
         title = " " .. title .. " "
