9151a1f99c3ef78d4b36b42c5acbfc38028e9491
Price Hiller <price@orion-technologies.io>
2024-02-09 00:08:50 -0600

    refactor(nvim): use headlines.nvim for heading bullets

--- dots/.config/nvim/lua/plugins/configs/headlines.lua
+++ dots/.config/nvim/lua/plugins/configs/headlines.lua
@@ -2,6 +2,30 @@ return {
     {
         "lukas-reineke/headlines.nvim",
         dependencies = "nvim-treesitter/nvim-treesitter",
+        opts = function()
+            local bullets = {
+                        "󰀘",
+                        "",
+                        "󰺕",
+                        "",
+                        "󰬪",
+                        "󱆭",
+            }
+            return {
+                markdown = {
+                    bullets = bullets,
+                },
+                rmd = {
+                    bullets = bullets,
+                },
+                norg = {
+                    bullets = bullets
+                },
+                org = {
+                    bullets = bullets
+                }
+            }
+        end,
         config = true,
         ft = { "markdown", "norg", "rmd", "org" },
     },
