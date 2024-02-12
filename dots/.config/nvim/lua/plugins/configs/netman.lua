4a76aba7e4a09ef3412e48f00b0372c548def717
Price Hiller <price@orion-technologies.io>
2024-02-11 22:53:50 -0600

    refactor(nvim): lazy load netman.nvim

--- dots/.config/nvim/lua/plugins/configs/netman.lua
+++ dots/.config/nvim/lua/plugins/configs/netman.lua
@@ -1,6 +1,15 @@
 return {
     {
         "miversen33/netman.nvim",
+        lazy = true,
+        event = "CmdlineEnter",
+        cmd = {
+            "NmloadProvider",
+            "Nmlogs",
+            "Nmdelete",
+            "Nmread",
+            "Nmwrite"
+        },
         config = function()
             require("netman")
         end,
