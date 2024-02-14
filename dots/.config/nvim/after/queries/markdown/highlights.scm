59296430545976703ccc271949f8aef889b412ec
Price Hiller <price@price-hiller.com>
2024-02-14 14:31:26 -0600

    feat(nvim): add markup concealments

--- dots/.config/nvim/after/queries/markdown/highlights.scm
+++ dots/.config/nvim/after/queries/markdown/highlights.scm
@@ -4,11 +4,11 @@
     (list_marker_star)
     (list_marker_plus)
     (list_marker_minus)
-  ]) @markup.list.markdown
-  (#offset! @markup.list.markdown 0 0 0 -1)
+  ]) @markup.list
+  (#offset! @markup.list 0 0 0 -1)
   (#set! conceal "•")
 )
 
 ; checkboxes
-((task_list_marker_unchecked) @markup.list.unchecked.markdown (#set! conceal ""))
-((task_list_marker_checked) @markup.list.checked.markdown (#set! conceal "󰄲"))
+((task_list_marker_unchecked) @markup.list.unchecked (#set! conceal ""))
+((task_list_marker_checked) @markup.list.checked (#set! conceal "󰄲"))
--- dots/.config/nvim/after/queries/org/highlights.scm
+++ dots/.config/nvim/after/queries/org/highlights.scm
@@ -3,3 +3,27 @@
  @punctuation.special
  (#any-of? @punctuation.special "-" "*" "+")
  (#set! conceal "•"))
+
+((checkbox !status) @markup.list.unchecked (#set! conceal ""))
+
+((checkbox
+   status:
+   (expr)
+   @org-checkbox-content (#any-of? @org-checkbox-content "x" "X")) @markup.list.checked
+ (#set! conceal "󰄲"))
+
+((checkbox
+   status:
+   (expr)
+   @org-checkbox-content (#any-of? @org-checkbox-content "-")) @markup.list.indeterminate
+ (#set! conceal "󰍵"))
+
+; Table highlights
+(row
+  "|" @punctuation.special)
+(cell
+  "|" @punctuation.special)
+(table
+  (row
+    (cell (contents) @markup.heading))
+  (hr) @punctuation.special)
