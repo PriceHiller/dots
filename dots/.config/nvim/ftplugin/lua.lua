8f0f1de6bce63b1bd9b64dc683f906c8b32c595d
Price Hiller <price@orion-technologies.io>
2024-02-24 16:03:49 -0600

    feat(nvim): update lua run keybind to support ranges, unsaved files

--- dots/.config/nvim/ftplugin/lua.lua
+++ dots/.config/nvim/ftplugin/lua.lua
@@ -2,7 +2,12 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 
-vim.keymap.set("n", "<leader>fr", ":luafile %<CR>", {
+vim.keymap.set("n", "<leader>fr", ":%lua<CR>", {
+    buffer = true,
+    silent = true,
+})
+
+vim.keymap.set("v", "<leader>fr", ":lua<CR>", {
     buffer = true,
     silent = true,
 })
