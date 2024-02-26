c18d278aaaa770073601e4f8a8528357177eb990
Price Hiller <price@orion-technologies.io>
2024-02-26 17:13:57 -0600

    feat(nvim): allow keyboard copy/paste in Neovide

--- dots/.config/nvim/lua/neovide.lua
+++ dots/.config/nvim/lua/neovide.lua
@@ -1,1 +1,14 @@
 vim.opt.linespace = -1
+
+-- Allow clipboard copy paste in neovim
+vim.keymap.set("n", "<D-s>", ":w<CR>", { noremap = true, silent = true }) -- Save
+vim.keymap.set("v", "<D-c>", '"+y', { noremap = true, silent = true }) -- Copy
+vim.keymap.set("n", "<D-v>", '"+P', { noremap = true, silent = true }) -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P', { noremap = true, silent = true }) -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+", { noremap = true, silent = true }) -- Paste command mode
+vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli', { noremap = true, silent = true }) -- Paste insert mode
+vim.keymap.set('t', '<D-v>', '<C-\\><C-n>"+Pi', { noremap = true, silent = true }) -- Paste terminal mode
+
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
