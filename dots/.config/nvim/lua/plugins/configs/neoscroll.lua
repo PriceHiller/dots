16186c3f0f8a33db11c0c7e9fb09be79509ff260
Price Hiller <price@orion-technologies.io>
2024-02-26 22:47:31 -0600

    refactor(nvim): disable neoscroll when using Neovide

--- dots/.config/nvim/lua/plugins/configs/neoscroll.lua
+++ dots/.config/nvim/lua/plugins/configs/neoscroll.lua
@@ -2,6 +2,11 @@ return {
     {
         "karb94/neoscroll.nvim",
         event = "WinScrolled",
+        config = function(opts)
+            if not vim.g.neovide then
+                require("neoscroll").setup(opts)
+            end
+        end,
         opts = {
             easing_function = "circular",
         },
