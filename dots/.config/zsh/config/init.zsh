63b19315f2f15ec8d7d13bc177288a34e5645595
Price Hiller <price@orion-technologies.io>
2024-03-01 04:14:21 -0600

    fix(zsh): set correct XDG_VARS

--- dots/.config/zsh/config/init.zsh
+++ dots/.config/zsh/config/init.zsh
@@ -2,9 +2,9 @@
 
 configure() {
 	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-	export XDG_CACHE_HOME="${XDG_CONFIG_HOME:-$HOME/.cache}"
+	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-	export XDG_STATE_HOME="${XDG_DATA_HOME:-$HOME/.local/state}"
+	export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 	export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
 	export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
 }
