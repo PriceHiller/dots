f52081720036205428d4d556ce9da29a06b2b3e5
Price Hiller <price@orion-technologies.io>
2024-03-16 11:59:29 -0500

    feat: add formatting scripts

---
+++ scripts/check-fmt-git-files.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#git --command bash
+# vim: ft=sh
+
+set -euo pipefail
+
+main() {
+	local root
+	root="$(git rev-parse --show-toplevel)"
+
+	local files=()
+
+	while IFS= read -r file; do
+		local fpath="${root}/${file}"
+		if [[ -r "${fpath}" ]]; then
+			files+=("${fpath}")
+		fi
+	done < <(git ls-tree --full-name --full-tree --name-only -r HEAD)
+
+	"${root}/scripts/fmt.bash" -- "${files[@]}"
+}
+
+main "${@}"
