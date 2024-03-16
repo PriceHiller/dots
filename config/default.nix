d26117af855f5f47819fe4838a82bb55a9a17f61
Price Hiller <price@price-hiller.com>
2024-03-16 00:33:25 -0500

    refactor(nix-hm): use `pinentry-qt` for gpg pinentry

--- config/default.nix
+++ config/default.nix
@@ -334,7 +334,7 @@ in {
       enable = true;
       enableSshSupport = true;
       enableZshIntegration = true;
-      pinentryPackage = pkgs.pinentry-gtk2;
+      pinentryPackage = pkgs.pinentry-qt;
       maxCacheTtl = 14400;
       maxCacheTtlSsh = 14400;
       sshKeys = [ "530D3EC95C32AB9EC33714AAF865738D6E77680A" ];
