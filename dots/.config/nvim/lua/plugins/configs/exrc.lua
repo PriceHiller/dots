1def30f6f171cf5c7191eab4a79ff66c9ee18386
Price Hiller <price@price-hiller.com>
2024-03-19 13:19:50 -0500

    feat(nvim): add `exrc.nvim`

---
+++ dots/.config/nvim/lua/plugins/configs/exrc.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "jedrzejboczar/exrc.nvim",
+        opts = {
+            -- Already handled by Neovim just fine imo
+            on_vim_enter = false,
+        },
+    },
+}
