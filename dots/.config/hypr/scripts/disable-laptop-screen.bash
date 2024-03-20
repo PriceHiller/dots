e7551df63ab60f6ef18ba6f4b6ac3e35b442b0a5
Price Hiller <price@price-hiller.com>
2024-03-20 07:40:03 -0500

    refactor(hypr): update to match upstream breaking changes

--- dots/.config/hypr/scripts/disable-laptop-screen.bash
+++ dots/.config/hypr/scripts/disable-laptop-screen.bash
@@ -4,7 +4,7 @@ main() {
 	local mon_count
 	mon_count="$(hyprctl monitors -j | jq length)"
 	if ((mon_count > 1)); then
-		hyprctl keyword monitor "eDP-1, disable"
+		hyprctl dpms off eDP-1
 	fi
 }
 
