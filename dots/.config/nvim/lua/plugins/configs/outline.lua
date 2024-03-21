534c3b66372e207dca37074261d845245840f4f0
Price Hiller <price@price-hiller.com>
2024-03-21 16:34:22 -0500

    feat(nvim): add `outline.nvim`

---
+++ dots/.config/nvim/lua/plugins/configs/outline.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "hedyhli/outline.nvim",
+        cmd = { "Outline" },
+        keys = { { "<localleader>o", "<cmd>Outline<CR>", desc = "Toggle Outline" } },
+        config = true
+    },
+}
