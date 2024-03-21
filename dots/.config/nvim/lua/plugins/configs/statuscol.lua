1f3689d6164a7fcd39b38bff3cd178f4d060eab1
Price Hiller <price@orion-technologies.io>
2024-03-21 16:34:33 -0500

    refactor(nvim): improve hack around neogit statuscol

--- dots/.config/nvim/lua/plugins/configs/statuscol.lua
+++ dots/.config/nvim/lua/plugins/configs/statuscol.lua
@@ -7,12 +7,11 @@ return {
 
             vim.api.nvim_create_autocmd({ "BufEnter" }, {
                 desc = "Ensure Neogit Status doesn't fuck the gutter up -- 🤮",
-                pattern = "*NeogitStatus*",
+                pattern = "*Neogit*",
                 callback = function()
                     local win = vim.api.nvim_get_current_win()
                     local set_opts = function()
                         vim.wo[win].statuscolumn = [[%!v:lua.StatusCol()]]
-                        vim.wo[win].signcolumn = "yes"
                         vim.wo[win].foldcolumn = "1"
                     end
                     set_opts()
