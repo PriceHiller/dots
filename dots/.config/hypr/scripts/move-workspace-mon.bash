6bea524df061ac3caf1869b159746d0236b9c40f
Price Hiller <price@orion-technologies.io>
2024-03-24 11:57:46 -0500

    feat(hypr): add keybind to move window and focus

--- dots/.config/hypr/scripts/move-workspace-mon.bash
+++ dots/.config/hypr/scripts/move-workspace-mon.bash
@@ -2,12 +2,17 @@
 
 main() {
 	local workspace_id="${1}"
+	local should_change_focus="${2:-0}"
 
 	local focused_monitor
 	focused_monitor="$(hyprctl monitors -j | jq -c '.[] | select(.focused)' | jq -r '.name')"
 
 	hyprctl dispatch movetoworkspacesilent "${workspace_id}"
 	hyprctl dispatch movecurrentworkspacetomonitor "${focused_monitor}"
+
+	if (( should_change_focus == 1 )); then
+		hyprctl dispatch workspace "${workspace_id}"
+	fi
 }
 
 main "${@}"
