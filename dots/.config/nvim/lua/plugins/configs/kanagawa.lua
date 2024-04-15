bffe048b0514ac76fb49e20f3ef5ae878a29f1c8
Price Hiller <price@orion-technologies.io>
2024-04-15 06:49:37 -0500

    refactor(nvim): add highlight for `NeogitBranchHead`

--- dots/.config/nvim/lua/plugins/configs/kanagawa.lua
+++ dots/.config/nvim/lua/plugins/configs/kanagawa.lua
@@ -67,6 +67,7 @@ return {
                 { "NeogitHunkHeader", { fg = colors.crystalBlue, bg = colors.sumiInk2 } },
                 { "NeogitHunkHeaderHighlight", { fg = colors.roninYellow, bg = colors.sumiInk1 } },
                 { "NeogitBranch", { fg = colors.autumnYellow, bold = true } },
+                { "NeogitBranchHead", { fg = colors.autumnYellow, bold = true, underline = true } },
                 { "NeogitUnmergedInto", { fg = colors.surimiOrange, bold = true } },
                 { "NeogitRemote", { fg = colors.carpYellow, bold = true } },
                 { "NeogitDiffContext", { bg = colors.sumiInk3 } },
