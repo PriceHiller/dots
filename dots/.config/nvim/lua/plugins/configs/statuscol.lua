107ab1fefd92e1ae7361008e2c22155bd42f8b0b
Price Hiller <price@orion-technologies.io>
2024-04-19 04:49:49 -0500

    refactor(nvim): remove leading space for statuscol folds

--- dots/.config/nvim/lua/plugins/configs/statuscol.lua
+++ dots/.config/nvim/lua/plugins/configs/statuscol.lua
@@ -37,7 +37,7 @@ return {
                 segments = {
                     { text = { "%s" }, click = "v:lua.ScSa" },
                     { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-                    { text = { " ", builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+                    { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
                     {
                         text = {
                             function(args)
