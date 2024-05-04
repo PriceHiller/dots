5f417ee90cff40ad51796ced99f5f212119617fe
Price Hiller <price@price-hiller.com>
2024-05-04 13:41:06 -0500

    refactor(nix): overlay `wezterm` with wezterm upstream

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,7 @@
         wrapProgram $out/bin/lxappearance --prefix GDK_BACKEND : x11
       '';
     });
+    wezterm = inputs.wezterm.packages.${final.system}.default;
     opensnitch-ui = prev.opensnitch-ui.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ prev.python311Packages.qt-material ];
     });
