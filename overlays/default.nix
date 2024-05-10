6f283a9f9e38eec3b64cc76adcd582fe5932ce0f
Price Hiller <price@price-hiller.com>
2024-05-10 02:41:46 -0500

    fix(nix): ensure `agenix-plugin-yubikey` gets deps

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,5 +13,13 @@
     opensnitch-ui = prev.opensnitch-ui.overrideAttrs (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ prev.python311Packages.qt-material ];
     });
+    age-plugin-yubikey = prev.age-plugin-yubikey.overrideAttrs (oldAttrs: {
+      nativeBuildInputs = oldAttrs.nativeBuildInputs or [ ] ++ [ final.makeWrapper ];
+      postInstall =
+        oldAttrs.postInstall or ""
+        + ''
+          wrapProgram $out/bin/age-plugin-yubikey --prefix LD_LIBRARY_PATH : ${final.pcsclite.lib}/lib
+        '';
+    });
   };
 }
