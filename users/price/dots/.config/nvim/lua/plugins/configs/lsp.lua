391695e03519c8e056199e471ea8e1721bf0f3ef
Price Hiller <price@orion-technologies.io>
2024-05-24 14:31:53 -0500

    feat(nvim): install `nvim-java`

--- users/price/dots/.config/nvim/lua/plugins/configs/lsp.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/lsp.lua
@@ -149,6 +149,29 @@ return {
             "Decodetalkers/csharpls-extended-lsp.nvim",
             "williamboman/mason-lspconfig.nvim",
             {
+                "nvim-java/nvim-java",
+                config = true,
+                dependencies = {
+                    "nvim-java/lua-async-await",
+                    "nvim-java/nvim-java-refactor",
+                    "nvim-java/nvim-java-core",
+                    "nvim-java/nvim-java-test",
+                    "nvim-java/nvim-java-dap",
+                    "MunifTanjim/nui.nvim",
+                    "neovim/nvim-lspconfig",
+                    "mfussenegger/nvim-dap",
+                    {
+                        "williamboman/mason.nvim",
+                        opts = {
+                            registries = {
+                                "github:nvim-java/mason-registry",
+                                "github:mason-org/mason-registry",
+                            },
+                        },
+                    },
+                },
+            },
+            {
                 "williamboman/mason.nvim",
                 cmd = {
                     "Mason",
