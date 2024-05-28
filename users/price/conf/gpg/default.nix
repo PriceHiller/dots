72d29e70a2e5b72419ed86467a457d26467822d2
Price Hiller <price@price-hiller.com>
2024-05-28 16:54:32 -0500

    feat(nix/hm/price): install and enable `passwordstore`

---
+++ users/price/conf/gpg/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+{
+  programs.gpg = {
+    enable = true;
+    mutableKeys = false;
+    mutableTrust = false;
+    publicKeys = [
+      {
+        source = ./public-gpg-yubikey.asc;
+        trust = "ultimate";
+      }
+    ];
+  };
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableZshIntegration = true;
+    pinentryPackage = pkgs.pinentry-qt;
+    maxCacheTtl = 14400;
+    maxCacheTtlSsh = 14400;
+    sshKeys = [ "530D3EC95C32AB9EC33714AAF865738D6E77680A" ];
+    extraConfig = ''
+      allow-loopback-pinentry
+    '';
+  };
+}
