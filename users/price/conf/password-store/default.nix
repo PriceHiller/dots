180e748b7e64f364d1f798bcb60ee08fe9661634
Price Hiller <price@price-hiller.com>
2024-05-28 17:37:53 -0500

    feat(nix/hm/price): add `pass-file` extension to `pass`

--- users/price/conf/password-store/default.nix
+++ users/price/conf/password-store/default.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions(exts: [
+      exts.pass-file
+    ]);
     settings = {
       PASSWORD_STORE_KEY = "C3FADDE7A8534BEB";
     };
