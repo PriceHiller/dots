66ebdfef98e0c6acb8177686eca57df74c7f6925
Price Hiller <price@orion-technologies.io>
2024-05-29 05:48:02 +0000

    fix(nix/host/orion): use correct mullvad dns servers

--- hosts/orion/modules/networking.nix
+++ hosts/orion/modules/networking.nix
@@ -39,7 +39,7 @@ in
     enable = true;
     domains = [ "~." ];
     extraConfig = ''
-      DNS=2a07:e340::2:853#dns.mullvad.net 194.242.2.2#dns.mullvad.net
+      DNS=2a07:e340::2#dns.mullvad.net 194.242.2.2#dns.mullvad.net
       FallbackDNS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
       Cache=yes
       CacheFromLocalhost=no
