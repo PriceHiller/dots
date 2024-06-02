e9a86cba4a82bcacd8d743dfb3cdc8ffa235d601
Price Hiller <price@orion-technologies.io>
2024-06-02 12:53:18 -0500

    feat(nix/hm/price): add UTSA ssh servers

--- users/price/conf/ssh/default.nix
+++ users/price/conf/ssh/default.nix
@@ -1,14 +1,33 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.ssh = {
     enable = true;
-    matchBlocks = rec {
-      luna = {
-        hostname = "luna.hosts.orion-technologies.io";
-        user = "price";
-        port = 2200;
-      };
-      "luna.hosts.orion-technologies.io" = luna;
-    };
+    matchBlocks =
+      rec {
+        luna = {
+          hostname = "luna.hosts.orion-technologies.io";
+          user = "price";
+          port = 2200;
+        };
+        "luna.hosts.orion-technologies.io" = luna;
+      }
+      # NOTE: UTSA Hosts behind VPN server
+      // builtins.listToAttrs (
+        builtins.map (
+          num:
+          let
+            hostname = "fox${
+              if (num > 0 && num < 10) then "0${builtins.toString num}" else builtins.toString num
+            }.cs.utsarr.net";
+          in
+          {
+            name = hostname;
+            value = {
+              user = "zfp106";
+              inherit hostname;
+            };
+          }
+        ) (lib.range 1 4)
+      );
   };
 }
