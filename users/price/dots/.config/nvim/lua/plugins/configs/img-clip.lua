1e2eea5fca2b3de5722689c3c64ecd18fb567925
Price Hiller <price@price-hiller.com>
2024-06-03 23:41:24 -0500

    feat(nvim): install `img-clip.nvim`

---
+++ users/price/dots/.config/nvim/lua/plugins/configs/img-clip.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "HakonHarnes/img-clip.nvim",
+        opts = {},
+        cmd = {
+            "PasteImage",
+            "ImgClipDebug",
+            "ImgClipConfig",
+        },
+        keys = {
+            { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste Image" },
+        },
+    },
+}
