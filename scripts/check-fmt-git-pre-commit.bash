9363385018ec3398385b4f94ac069aa94d36b23f
Price Hiller <price@price-hiller.com>
2024-06-12 03:21:15 -0500

    feat: allow exclusions in check fmt

--- scripts/check-fmt-git-pre-commit.bash
+++ scripts/check-fmt-git-pre-commit.bash
@@ -8,11 +8,32 @@ main() {
 	root="$(git rev-parse --show-toplevel)"
 
 	local files=()
+	local excluded_files=()
+	local excluded_dirs=(
+		"${root}/users/price/dots/.config/vesktop"
+	)
 
 	while IFS= read -r file; do
+		local add_file=true
 		local fpath="${root}/${file}"
-		if [[ -r "${fpath}" ]]; then
-			files+=("${fpath}")
+		if [[ -r "$fpath" ]]; then
+			for excluded_dir in "${excluded_dirs[@]}"; do
+				if [[ "$fpath" == "$excluded_dir"* ]]; then
+					add_file=false
+					break
+				fi
+			done
+
+			for excluded_file in "${excluded_files[@]}"; do
+				if [[ "$fpath" == "$excluded_file" ]]; then
+					add_file=false
+					break
+				fi
+			done
+
+			if [[ "$add_file" == true ]]; then
+				files+=("$fpath")
+			fi
 		fi
 	done < <(git diff --name-only --staged --diff-filter=d)
 
