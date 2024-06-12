8a592cc2788ef5ac933befd663bbe3d90cdf774f
Price Hiller <price@price-hiller.com>
2024-06-12 09:19:40 -0500

    refactor(nix/host/orion): disable `DNSSEC`

--- hosts/orion/modules/networking.nix
+++ hosts/orion/modules/networking.nix
@@ -43,7 +43,7 @@ in
       FallbackDNS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
       Cache=yes
       CacheFromLocalhost=no
-      DNSSEC=allow-downgrade
+      DNSSEC=no
       DNSOverTLS=opportunistic
       MulticastDNS=yes
     '';
