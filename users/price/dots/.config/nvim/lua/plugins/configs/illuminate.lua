5b4ecc05ee9be777fff5c498a7423f99f1553216
Price Hiller <price@orion-technologies.io>
2024-06-15 00:43:14 -0500

    perf(nvim): disable illuminate in large files

--- users/price/dots/.config/nvim/lua/plugins/configs/illuminate.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/illuminate.lua
@@ -2,5 +2,8 @@ return {
     {
         "RRethy/vim-illuminate",
         event = { "CursorHold", "BufReadPre", "BufNewFile" },
+        config = function()
+            require("illuminate").configure({ large_file_cutoff = 10000 })
+        end,
     },
 }
