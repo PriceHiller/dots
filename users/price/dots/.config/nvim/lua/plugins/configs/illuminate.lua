1df02c851cef88dcac737e3bdba0f8fd6573132d
Price Hiller <price@orion-technologies.io>
2024-06-17 15:45:24 -0500

    refactor(nvim): remove `vim-illuminate`

--- users/price/dots/.config/nvim/lua/plugins/configs/illuminate.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "RRethy/vim-illuminate",
-        event = { "CursorHold", "BufReadPre", "BufNewFile" },
-        config = function()
-            require("illuminate").configure({ large_file_cutoff = 10000 })
-        end,
-    },
-}
