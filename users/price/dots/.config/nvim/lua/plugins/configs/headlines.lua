99123f186055136199a8d30f44757b39b7291010
Price Hiller <price@price-hiller.com>
2024-06-19 00:23:00 -0500

    fix(nvim): make headlines quote query work for nested lists in markdown

--- users/price/dots/.config/nvim/lua/plugins/configs/headlines.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/headlines.lua
@@ -26,6 +26,29 @@ return {
                     bullets = bullets,
                     bullet_highlights = bullet_highlights,
                     fat_headline_lower_string = "▀",
+                    query = vim.treesitter.query.parse(
+                        "markdown",
+                        [[
+                            (atx_heading [
+                                (atx_h1_marker)
+                                (atx_h2_marker)
+                                (atx_h3_marker)
+                                (atx_h4_marker)
+                                (atx_h5_marker)
+                                (atx_h6_marker)
+                            ] @headline)
+
+                            (thematic_break) @dash
+
+                            (fenced_code_block) @codeblock
+
+                            (block_quote_marker) @quote
+                            (block_quote (paragraph (inline (block_continuation) @quote)))
+                            (block_quote (paragraph (block_continuation) @quote))
+                            (block_quote (list (list_item (paragraph (inline (block_continuation) @quote)))))
+                            (block_quote (block_continuation) @quote)
+                        ]]
+                    ),
                 },
                 rmd = {
                     bullets = bullets,
