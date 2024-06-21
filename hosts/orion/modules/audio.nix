3a87e6e317da4e5ebc16e70b95686d8e9a26b84f
Price Hiller <price@orion-technologies.io>
2024-06-21 15:41:14 -0500

    refactor(nix/host/orion): cleanup `audio` settings

--- hosts/orion/modules/audio.nix
+++ hosts/orion/modules/audio.nix
@@ -1,21 +1,8 @@
+{ ... }:
 {
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-
-{
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    pulse.enable = true;
-    wireplumber.enable = true;
-    jack.enable = true;
+    alsa.enable = true;
   };
 }
