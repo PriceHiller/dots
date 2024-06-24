1ce511e45142dc70f0b9d706047dd9e6d0742cc9
Price Hiller <price@orion-technologies.io>
2024-06-24 14:41:50 -0500

    fix(nvim): remove call to removed plugin

--- users/price/dots/.config/nvim/after/ftplugin/dockerfile.lua
+++ users/price/dots/.config/nvim/after/ftplugin/dockerfile.lua
@@ -1,9 +1,3 @@
 local file_loc = vim.fn.expand("%:p:h")
 
 vim.opt_local.makeprg = "docker build " .. file_loc
-
-vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-    callback = function()
-        require("lint").try_lint()
-    end,
-})
