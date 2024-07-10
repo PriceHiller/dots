7a5e718ad1f565fd5ddfdb72ec802ee32a6a64ef
Price Hiller <price@orion-technologies.io>
2024-07-09 20:24:02 -0500

    refactor(emacs): declare config bankruptcy

--- users/price/dots/.config/emacs/early-init.el
+++ users/price/dots/.config/emacs/early-init.el
@@ -1,4 +1,1 @@
-;;; -*- lexical-binding: t -*-
-(set-face-attribute 'default nil :family "Fira Code" :height 120)
-(set-face-attribute 'fixed-pitch nil :family "Fira Code":height 130)
-(set-face-attribute 'variable-pitch nil :family "Fira Code":height 130)
+(setq package-enable-at-startup nil)
