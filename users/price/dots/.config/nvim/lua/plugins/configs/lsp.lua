f3309aa37b7ee52868d729247ccf61ef0d282b16
Price Hiller <price@price-hiller.com>
2024-07-17 02:30:34 -0500

    refactor(nvim): do not specify deps for `nvim-java`
    Upstream provides a `lazy.lua` file, see
    https://github.com/nvim-java/nvim-java/blob/main/lazy.lua

--- users/price/dots/.config/nvim/lua/plugins/configs/lsp.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/lsp.lua
@@ -157,17 +157,6 @@ return {
     {
         "nvim-java/nvim-java",
         ft = { "java" },
-        dependencies = {
-            "nvim-java/lua-async-await",
-            "nvim-java/nvim-java-refactor",
-            "nvim-java/nvim-java-core",
-            "nvim-java/nvim-java-test",
-            "nvim-java/nvim-java-dap",
-            "MunifTanjim/nui.nvim",
-            "mfussenegger/nvim-dap",
-            "williamboman/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
     },
     {
         "neovim/nvim-lspconfig",
