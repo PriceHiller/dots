4de0e4db4b3d24df3ff72433237b37a5ad6250ae
Price Hiller <price@orion-technologies.io>
2024-07-17 01:27:44 -0500

    refactor(nix): use `neovide` from upstream

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,6 @@
     fetchFromGitHub = pkgs.fetchFromGitHub;
   };
   screen-cap = pkgs.callPackage ./screen-cap/default.nix { };
-  neovide = pkgs.callPackage ./neovide/package.nix { };
   Fmt = pkgs.writeShellApplication {
     name = "Fmt";
     runtimeInputs = with pkgs; [
