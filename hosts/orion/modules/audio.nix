63036fc3f786a5c7f14399b732435cb2c9543ce1
Price Hiller <price@price-hiller.com>
2024-07-19 00:46:44 -0500

    refactor(nixos/orion): specify pipewire clock settings

--- hosts/orion/modules/audio.nix
+++ hosts/orion/modules/audio.nix
@@ -5,5 +5,26 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+    extraConfig.pipewire."90-default-clock" = {
+      "context.properties" = {
+        "default.clock" = {
+          "allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            176400
+            192000
+            352800
+            384000
+            705600
+            768000
+          ];
+          "quantum" = 32;
+          "min-quantum" = 32;
+          "max-quantum" = 1024;
+        };
+      };
+    };
   };
 }
