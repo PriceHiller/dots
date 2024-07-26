7118eff64516d4582341a951d2a69d63d1228d6a
Price Hiller <price@price-hiller.com>
2024-07-26 12:08:18 -0500

    feat(host/luna): strip `.html` extension for blog

--- hosts/luna/modules/services/nginx.nix
+++ hosts/luna/modules/services/nginx.nix
@@ -22,6 +22,12 @@
     forceSSL = true;
     enableACME = true;
     root = inputs.blog.packages.${pkgs.system}.default;
+    locations."/".extraConfig = ''
+      if ($request_uri ~ ^/(.*)\.html(\?|$)) {
+          return 302 /$1;
+      }
+      try_files $uri $uri.html $uri/ =404;
+    '';
     locations."/".index = "home.html";
   };
 }
