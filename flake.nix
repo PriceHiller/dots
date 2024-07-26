d6e21da3be59931459a08e8b7132dbb5043bb2a5
Price Hiller <price@price-hiller.com>
2024-07-26 01:05:18 -0500

    Revert "refactor(nix/hm/price): do not use `wezterm` from upstream"
    This reverts commit 0f9fd6d017b762e63f514dd221bd7cda7d389134.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
       url = "github:Alexays/Waybar";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    wezterm = {
+      url = "github:wez/wezterm?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     agenix = {
       url = "github:yaxitech/ragenix";
       inputs.nixpkgs.follows = "nixpkgs";
