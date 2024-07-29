f4e3c4a7eb02936fd7c8a8cd0d4a792d699cf898
Price Hiller <price@orion-technologies.io>
2024-07-29 16:38:16 -0500

    refactor(hm/price): do not provide a custom compInit for zsh

--- users/price/conf/zsh/default.nix
+++ users/price/conf/zsh/default.nix
@@ -3,21 +3,6 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    completionInit = ''
-      () {
-        autoload -U compinit
-        local zsh_cache="''${XDG_CACHE_HOME:-$HOME}/zsh/"
-        if [[ ! -d "$zsh_cache" ]]; then
-          mkdir -p "$zsh_cache"
-        fi
-        local _comp_files=($zsh_cache/zcompcache(Nm-20))
-        if (( $#_comp_files )); then
-          compinit -i -C -d "$zsh_cache/zcompcache"
-        else
-          compinit -i -d "$zsh_cache/zcompcache"
-        fi
-      }
-    '';
     initExtra = builtins.readFile ./init-extra.zsh;
   };
 }
