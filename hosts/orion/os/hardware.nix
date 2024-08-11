f0f81d446c1d1944a46c99583ed02e1c21c32fc2
Price Hiller <price@price-hiller.com>
2024-08-11 17:03:30 -0500

    feat(hosts/orion): enable powersave for wifi

--- hosts/orion/os/hardware.nix
+++ hosts/orion/os/hardware.nix
@@ -3,4 +3,7 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   services.fstrim.enable = true;
+  boot.extraModprobeConfig = ''
+    options iwlwifi power_save=1
+  '';
 }
