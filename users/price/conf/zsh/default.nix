49972b75fa78383c64a59e264128024e559380d9
Price Hiller <price@orion-technologies.io>
2024-08-29 23:37:14 -0500

    refactor(hm/price): ensure `zcompdump` gets put under $XDG_CACHE_HOME

--- users/price/conf/zsh/default.nix
+++ users/price/conf/zsh/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     initExtra = builtins.readFile ./init-extra.zsh;
+    completionInit =
+      let
+        zsh-cache-dir = "${config.xdg.cacheHome}/zsh";
+      in
+      "mkdir -p ${zsh-cache-dir} && autoload -U compinit && compinit -d ${zsh-cache-dir}/zcompdump-$ZSH_VERSION";
   };
 }
