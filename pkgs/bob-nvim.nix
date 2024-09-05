36b8e0076fa9b89fbd4fa52c189e82ad359edcf2
Price Hiller <price@price-hiller.com>
2024-09-05 10:58:07 -0500

    feat(nix/pkgs): add shell completions for `bob-nvim`

--- pkgs/bob-nvim.nix
+++ pkgs/bob-nvim.nix
@@ -1,9 +1,11 @@
-{ rustPlatform, fetchFromGitHub }:
+{ installShellFiles, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   pname = "bob";
   version = "3.0.1";
 
+
+  buildInputs = [ installShellFiles ];
   src = fetchFromGitHub {
     owner = "MordechaiHadad";
     repo = "bob";
@@ -11,4 +13,12 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-Op/NXWssylgAOb1BccSOz7JqXFranzAsGICFMF3o/K8=";
   };
   cargoLock.lockFile = "${src}/Cargo.lock";
+
+  postInstall = # bash
+    ''
+      installShellCompletion --cmd bob \
+        --bash <($out/bin/bob complete bash) \
+        --zsh <($out/bin/bob complete zsh) \
+        --fish <($out/bin/bob complete fish)
+    '';
 }
