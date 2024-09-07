fa1d3ef143e42d072de25feabafce60765db5a00
Price Hiller <price@price-hiller.com>
2024-09-07 13:35:01 -0500

    refactor(hm/price): use `z.lua` via home manager not zsh

--- users/price/conf/zsh/default.nix
+++ users/price/conf/zsh/default.nix
@@ -1,13 +1,22 @@
 { config, ... }:
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    initExtra = builtins.readFile ./init-extra.zsh;
-    completionInit =
-      let
-        zsh-cache-dir = "${config.xdg.cacheHome}/zsh";
-      in
-      "mkdir -p ${zsh-cache-dir} && autoload -U compinit && compinit -d ${zsh-cache-dir}/zcompdump-$ZSH_VERSION";
+  programs = {
+    z-lua = {
+      enable = true;
+      options = [
+        "enhanced"
+        "once"
+      ];
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      initExtra = builtins.readFile ./init-extra.zsh;
+      completionInit =
+        let
+          zsh-cache-dir = "${config.xdg.cacheHome}/zsh";
+        in
+        "mkdir -p ${zsh-cache-dir} && autoload -U compinit && compinit -d ${zsh-cache-dir}/zcompdump-$ZSH_VERSION";
+    };
   };
 }
