31129ffe4c3a010f1556cf13068b0ff1168c0ee1
Price Hiller <price@price-hiller.com>
2024-10-11 10:18:40 -0500

    refactor(nvim): set `tw` for markdown to 0

--- users/price/dots/.config/nvim/after/ftplugin/markdown.lua
+++ users/price/dots/.config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
-vim.opt_local.textwidth = 120
+vim.opt_local.textwidth = 0
 
 vim.keymap.set("n", "<leader>fr", "<cmd>MarkdownPreview<CR>", {
     buffer = true,
