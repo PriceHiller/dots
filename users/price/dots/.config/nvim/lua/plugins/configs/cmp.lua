f29578d5f7cad6fb6b1a8c4226a7294bbc2142ca
Price Hiller <price@price-hiller.com>
2024-10-15 16:54:13 -0500

    refactor(nvim): use `C-j` & `C-k` to scroll cmp docs
    Note: this does mean that C-k binds for entering raw codes no longer works by default.

--- users/price/dots/.config/nvim/lua/plugins/configs/cmp.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/cmp.lua
@@ -240,8 +240,8 @@ return {
                     end,
                 },
                 mapping = {
-                    ["<C-s>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-                    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+                    ["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                    ["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
                     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
                     ["<C-e>"] = cmp.mapping({
