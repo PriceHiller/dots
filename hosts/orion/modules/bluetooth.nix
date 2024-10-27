bc04b905eb36c8959dc96a93a21525aa04bc2d26
Price Hiller <price@price-hiller.com>
2024-10-27 18:11:23 -0500

    feat(hosts/orion): enable bluetooth on boot

--- hosts/orion/modules/bluetooth.nix
+++ hosts/orion/modules/bluetooth.nix
@@ -1,12 +1,13 @@
 {
-  inputs,
-  lib,
   pkgs,
   ...
 }:
 
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
   services.blueman.enable = true;
   environment.systemPackages = with pkgs; [ bluez ];
 
