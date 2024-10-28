f3fde3123db9673c61635631959d1af3eaa56335
Price Hiller <price@price-hiller.com>
2024-10-28 16:13:34 -0500

    feat(hosts/orion): use cachix substituters

--- users/price/conf/nix/default.nix
+++ users/price/conf/nix/default.nix
@@ -10,6 +10,13 @@
         "nix-command"
         "flakes"
       ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       auto-optimise-store = true;
       use-xdg-base-directories = true;
       trusted-users = [ "@wheel" ];
