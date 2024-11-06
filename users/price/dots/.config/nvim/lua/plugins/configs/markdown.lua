f291dade99293d34a2d72b162e4c9ebaa5b99236
Price Hiller <price@price-hiller.com>
2024-11-06 15:51:10 -0600

    refactor(nvim): migrate markview into markdown plugin file

--- users/price/dots/.config/nvim/lua/plugins/configs/markdown.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/markdown.lua
@@ -5,6 +5,24 @@ return {
         ft = { "markdown" },
         build = function()
             vim.fn["mkdp#util#install"]()
+    {
+        "OXY2DEV/markview.nvim",
+        ft = "markdown",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("markview").setup({
+                hybrid_modes = { "n" },
+                checkboxes = {
+                    enable = false,
+                },
+                list_items = {
+                    enable = false,
+                },
+            })
+            vim.cmd("Markview hybridDisable")
         end,
     },
 }
