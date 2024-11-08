5689cf47e6fc85e03b06abe808ebdc6a7b92f5a5
Price Hiller <price@price-hiller.com>
2024-11-08 00:55:37 -0600

    fix(nvim): handle rust analyzer cancellation issues

--- users/price/dots/.config/nvim/lua/core/lsp.lua
+++ users/price/dots/.config/nvim/lua/core/lsp.lua
@@ -1,6 +1,17 @@
 local M = {}
 
 M.setup = function()
+    -- HACK: See https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+    -- This fixes an issue with "server cancelled the request" emissions from `rust-analyzer`
+    for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(error, result, ctx, config)
+            if error ~= nil and error.code == -32802 then
+                return
+            end
+            return default_diagnostic_handler(error, result, ctx, config)
+        end
+    end
     vim.diagnostic.config({
         severity_sort = true,
         underline = true,
