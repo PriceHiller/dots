ff0d9fc3f7a63350d7d46354ef1fee11966984d9
Price Hiller <price@price-hiller.com>
2024-11-13 03:24:34 -0600

    feat(nvim): add key to toggle org indent mode

--- users/price/dots/.config/nvim/after/ftplugin/org.lua
+++ users/price/dots/.config/nvim/after/ftplugin/org.lua
@@ -1,2 +1,8 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.modeline = true
+vim.keymap.set("n", "<leader>ff", function()
+    vim.b.org_indent_mode = not vim.b.org_indent_mode
+end, {
+    buffer = true,
+    desc = "Org: Toggle Indent Mode",
+})
