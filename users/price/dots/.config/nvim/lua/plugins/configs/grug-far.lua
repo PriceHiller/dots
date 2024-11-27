25fba0d06f2e6c5c6f1b89d312527eeed227b815
Price Hiller <price@price-hiller.com>
2024-11-26 22:40:29 -0600

    refactor(nvim): make `grug-far` use `,,`

--- users/price/dots/.config/nvim/lua/plugins/configs/grug-far.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/grug-far.lua
@@ -3,7 +3,7 @@ return {
         "MagicDuck/grug-far.nvim",
         cmd = { "GrugFar" },
         keys = {
-            { "<localleader><localleader>", "<cmd>GrugFar<CR>", desc = "GrugFar: Open" },
+            { ",,", "<cmd>GrugFar<CR>", desc = "GrugFar: Open" },
         },
         config = function()
             require("grug-far").setup({})
