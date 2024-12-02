9fe2a8d9e9dcba87832d0990adc3f8470d3092c0
Price Hiller <price@price-hiller.com>
2024-12-01 22:15:38 -0600

    fix(hm/price): ensure neovim gets current env vars

--- users/price/conf/nvim/default.nix
+++ users/price/conf/nvim/default.nix
@@ -1,12 +1,38 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim;
+    extraWrapperArgs = [
+      "--suffix"
+      "LD_LIBRARY_PATH"
+      ":"
+      "${lib.makeLibraryPath [
+        pkgs.sqlite
+      ]}"
+      "--suffix"
+      "LIBRARY_PATH"
+      ":"
+      "${lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+        pkgs.zlib
+      ]}"
+      "--suffix"
+      "PKG_CONFIG_PATH"
+      ":"
+      "${lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
+        pkgs.stdenv.cc.cc
+        pkgs.zlib
+      ]}"
+    ];
+  };
   home = {
     packages = with pkgs; [
       neovide
-      neovim
       bun
     ];
   };
