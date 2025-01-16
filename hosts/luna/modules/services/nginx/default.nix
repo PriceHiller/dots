7119a6b221d61c653601aa3deaa93ce4f2e6ff7e
Price Hiller <price@price-hiller.com>
2025-01-16 01:23:31 -0600

    feat(hosts/luna): advertise pgp key via WKD

---
+++ hosts/luna/modules/services/nginx/default.nix
@@ -0,0 +1,45 @@
+{ inputs, pkgs, ... }:
+{
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedTlsSettings = true;
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "price@price-hiller.com";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  services.nginx.virtualHosts = {
+    "price-hiller.com" = {
+      forceSSL = true;
+      enableACME = true;
+      root = inputs.blog.packages.${pkgs.system}.default;
+      locations = {
+        "/" = {
+          extraConfig = ''
+            if ($request_uri ~ ^/(.*)\.html(\?|$)) {
+                return 302 /$1;
+            }
+            try_files $uri $uri.html $uri/ =404;
+          '';
+          index = "home.html";
+        };
+        "/.well-known/openpgpkey/hu/rnmhgp3dsaq8hjgu49j8oongugr5cg4" = {
+          index = pkgs.writeText "pgp-pub-wkd" builtins.readFile ./public-pgp-key.asc;
+          extraConfig = ''
+            default_type application/octet-stream;
+            add_header Access-Control-Allow-Origin "*";
+          '';
+        };
+      };
+    };
+  };
+}
