b75c86cdb301be05049bbc19fb06d60182557730
Price Hiller <price@price-hiller.com>
2025-01-17 15:14:33 -0600

    feat(host/luna): setup fzf & zsh shell

--- hosts/luna/modules/programs.nix
+++ hosts/luna/modules/programs.nix
@@ -7,7 +7,22 @@
       enable = true;
       defaultEditor = true;
     };
+    fzf = {
+      keybindings = true;
+      fuzzyCompletion = true;
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableBashCompletion = true;
+      syntaxHighlighting.enable = true;
+      shellInit = ''
+        source "${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh"
+      '';
+    };
   };
+
+  system.userActivationScripts.zshrc = "touch .zshrc";
 
   environment.systemPackages = with pkgs; [
     vim
--- hosts/luna/modules/users.nix
+++ hosts/luna/modules/users.nix
@@ -1,26 +1,28 @@
 { pkgs, config, ... }:
 {
   security.sudo.wheelNeedsPassword = false;
-  users.mutableUsers = false;
-  users.users = {
-    root.hashedPasswordFile = config.age.secrets.users-root-pw.path;
-    price = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      shell = pkgs.bash;
-      hashedPasswordFile = config.age.secrets.users-price-pw.path;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkWsSntg1ufF40cALcIBA7WZhiU/f0cncqq0pcp+DZY openpgp:0x15993C90"
-      ];
+  users = {
+    mutableUsers = false;
+    defaultUserShell = pkgs.zsh;
+    users = {
+      root.hashedPasswordFile = config.age.secrets.users-root-pw.path;
+      price = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        hashedPasswordFile = config.age.secrets.users-price-pw.path;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkWsSntg1ufF40cALcIBA7WZhiU/f0cncqq0pcp+DZY openpgp:0x15993C90"
+        ];
+      };
     };
   };
   environment.persistence.ephemeral.users = {
     price = {
-      files = [ ".bash_history" ];
+      files = [ ".bash_history" ".zsh_history" ];
     };
     root = {
       home = "/root";
-      files = [ ".bash_history" ];
+      files = [ ".bash_history" ".zsh_history" ];
     };
   };
 }
