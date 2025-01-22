56ff4669038f2f23a871d8494cf71414edde6d27
Price Hiller <price@price-hiller.com>
2025-01-22 11:40:09 -0600

    refactor(librewolf): ensure xdg defaults are set

--- users/price/conf/librewolf/default.nix
+++ users/price/conf/librewolf/default.nix
@@ -2,23 +2,29 @@
 {
 
   xdg.mimeApps.defaultApplications = lib.mkIf (config.programs.librewolf.enable) {
-    defaultApplications = {
-      "default-web-browser" = [ "librewolf.desktop" ];
-      "text/html" = [ "librewolf.desktop" ];
-      "x-scheme-handler/http" = [ "librewolf.desktop" ];
-      "x-scheme-handler/https" = [ "librewolf.desktop" ];
-      "x-scheme-handler/about" = [ "librewolf.desktop" ];
-      "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
-    };
+    "default-web-browser" = [ "librewolf.desktop" ];
+    "text/html" = [ "librewolf.desktop" ];
+    "x-scheme-handler/http" = [ "librewolf.desktop" ];
+    "x-scheme-handler/https" = [ "librewolf.desktop" ];
+    "x-scheme-handler/about" = [ "librewolf.desktop" ];
+    "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+    "x-scheme-handler/chrome" = [ "librewolf.desktop" ];
+    "application/x-extension-htm" = [ "librewolf.desktop" ];
+    "application/x-extension-html" = [ "librewolf.desktop" ];
+    "application/x-extension-shtml" = [ "librewolf.desktop" ];
+    "application/xhtml+xml" = [ "librewolf.desktop" ];
+    "application/x-extension-xhtml" = [ "librewolf.desktop" ];
+    "application/x-extension-xht" = [ "librewolf.desktop" ];
   };
   programs.librewolf = {
     enable = true;
     settings = {
       "webgl.disabled" = false;
       "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.downloads" = false;
       "privacy.clearOnShutdown.cookies" = false;
       "network.cookie.lifetimePolicy" = 0;
-      "network.trr.mode" = 2;
+      "network.trr.mode" = 3;
       "network.trr.uri" = "https://dns.mullvad.net/dns-query";
       "network.trr.default_provider_uri" = "https://dns10.quad9.net/dns-query";
       "network.trr.strict_native_fallback" = false;
