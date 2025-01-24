ba22f75e8f2e74a67b265eb1f2bb14ce2cc3c797
Price Hiller <price@price-hiller.com>
2025-01-24 17:47:56 -0600

    fix(nvim): use `localleader` for rust binds

--- users/price/dots/.config/nvim/after/ftplugin/rust.lua
+++ users/price/dots/.config/nvim/after/ftplugin/rust.lua
@@ -2,30 +2,30 @@ vim.opt_local.textwidth = 80
 vim.keymap.set("n", "<localleader>fr", ":RustLsp runnables<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fd", ":RustLsp debuggables<CR>", {
+vim.keymap.set("n", "<localleader>fd", ":RustLsp debuggables<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fp", ":RustLsp openCargo<CR>", {
+vim.keymap.set("n", "<localleader>fp", ":RustLsp openCargo<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fJ", ":RustLsp joinLines<CR>", {
+vim.keymap.set("n", "<localleader>fJ", ":RustLsp joinLines<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fh", ":RustLsp hover actions<CR>", {
+vim.keymap.set("n", "<localleader>fh", ":RustLsp hover actions<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fH", ":RustLsp hover range<CR>", {
+vim.keymap.set("n", "<localleader>fH", ":RustLsp hover range<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fm", ":RustLsp expandMacro<CR>", {
+vim.keymap.set("n", "<localleader>fm", ":RustLsp expandMacro<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fc", ":RustLsp openCargo<CR>", {
+vim.keymap.set("n", "<localleader>fc", ":RustLsp openCargo<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fk", ":RustLsp moveItem up<CR>", {
+vim.keymap.set("n", "<localleader>fk", ":RustLsp moveItem up<CR>", {
     buffer = true,
 })
-vim.keymap.set("n", "<leader>fj", ":RustLsp moveItem down<CR>", {
+vim.keymap.set("n", "<localleader>fj", ":RustLsp moveItem down<CR>", {
     buffer = true,
 })
