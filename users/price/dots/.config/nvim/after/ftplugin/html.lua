52cab26b2233a78b71d8bff314f036d08aaee231
Price Hiller <price@price-hiller.com>
2025-01-26 23:02:43 -0600

    refactor(nvim): properly launch `xdg-open` for html filetypes

--- users/price/dots/.config/nvim/after/ftplugin/html.lua
+++ users/price/dots/.config/nvim/after/ftplugin/html.lua
@@ -1,7 +1,9 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 
-vim.keymap.set("n", "<localleader>fr", ":!xdg-open %<CR>", {
+vim.keymap.set("n", "<localleader>fr", function()
+    vim.system({ "xdg-open", vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) }, { detach = true })
+end, {
     buffer = true,
     silent = true,
 })
