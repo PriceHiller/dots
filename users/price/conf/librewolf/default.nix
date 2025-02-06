c26e06c2b66f9ab5d722c24dcd3428878550a35e
Price Hiller <price@price-hiller.com>
2025-02-06 15:48:01 -0600

    refactor(librewolf): add function to convert attrs to string paths

--- users/price/conf/librewolf/default.nix
+++ users/price/conf/librewolf/default.nix
@@ -1,4 +1,25 @@
 { config, lib, ... }:
+let
+  attrsToStringPath =
+    attrs:
+    let
+      _attrsToStringPath =
+        _parent:
+        let
+          parent = if isNull _parent then "" else "${_parent}.";
+        in
+        attrs:
+        lib.attrsets.foldlAttrs (
+          acc: _name:
+          let
+            name = "${parent}${_name}";
+          in
+          value:
+          acc // (if builtins.isAttrs value then _attrsToStringPath name value else { "${name}" = value; })
+        ) { } attrs;
+    in
+    _attrsToStringPath null attrs;
+in
 {
 
   xdg.mimeApps.defaultApplications = lib.mkIf (config.programs.librewolf.enable) {
@@ -18,19 +39,25 @@
   };
   programs.librewolf = {
     enable = true;
-    settings = {
-      "webgl.disabled" = false;
-      "privacy.clearOnShutdown.history" = false;
-      "privacy.clearOnShutdown.downloads" = false;
-      "privacy.clearOnShutdown.cookies" = false;
-      "network.cookie.lifetimePolicy" = 0;
-      "network.trr.mode" = 3;
-      "network.trr.uri" = "https://dns.mullvad.net/dns-query";
-      "network.trr.default_provider_uri" = "https://dns10.quad9.net/dns-query";
-      "network.trr.strict_native_fallback" = false;
-      "network.trr.retry_on_recoverable_errors" = true;
-      "network.trr.disable-heuristics" = true;
-      "network.trr.allow-rfc1918" = true;
+    settings = attrsToStringPath {
+      webgl.disabled = false;
+      privacy.clearOnShutdown = {
+        history = false;
+        downloads = false;
+        cookies = false;
+      };
+      network = {
+        cookie.lifetimePolicy = 0;
+        trr = {
+          mode = 3;
+          uri = "https://dns.mullvad.net/dns-query";
+          default_provider_uri = "https://dns10.quad9.net/dns-query";
+          strict_native_fallback = false;
+          retry_on_recoverable_errors = true;
+          disable-heuristics = true;
+          allow-rfc1918 = true;
+        };
+      };
     };
   };
 }
