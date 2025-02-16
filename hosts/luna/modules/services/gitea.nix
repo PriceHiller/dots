c2dcf5568a18176d30025a61474ba5eb76e8edad
Price Hiller <price@price-hiller.com>
2025-02-16 11:06:19 -0600

    refactor(hosts/luna): clean up gitea

--- hosts/luna/modules/services/gitea.nix
+++ hosts/luna/modules/services/gitea.nix
@@ -2,7 +2,6 @@
   config,
   inputs,
   pkgs,
-  lib,
   ...
 }:
 let
@@ -145,17 +144,6 @@ in
       };
     };
   };
-
-  # TODO: Upstream the below to Nixpkgs. If the runner is using the exact same url as the gitea
-  # service and both are on the same host, then the runner should have a systemd dependency on the
-  # gitea service.
-  systemd.services.gitea-actions-default.requires = lib.mkIf (
-    config.services.gitea.enable
-    && (
-      config.services.gitea-actions-runner.instances.default.url
-      == config.services.gitea.settings.server.ROOT_URL
-    )
-  ) [ "gitea.service" ];
 
   networking.firewall.allowedTCPPorts = [ config.services.gitea.settings.server.SSH_PORT ];
 
