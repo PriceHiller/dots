8f228af6f64a2c3dcf9f7f7658025bd28aa7546e
Price Hiller <price@price-hiller.com>
2025-02-24 22:01:57 -0600

    feat(nvim): support autopairs in markdown and org fts

--- users/price/dots/.config/nvim/lua/plugins/configs/autopairs.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/autopairs.lua
@@ -2,6 +2,26 @@ return {
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
-        config = true,
+        config = function()
+            local npairs = require("nvim-autopairs")
+            local Rule = require("nvim-autopairs.rule")
+            local cond = require("nvim-autopairs.conds")
+            npairs.setup()
+
+            npairs.add_rules({
+                Rule("~", "~", { "org" }),
+                Rule("*", "*", { "org" }):with_pair(cond.not_before_regex("^$")):with_pair(cond.not_before_regex("%*")),
+                Rule("/", "/", { "org" }):with_pair(cond.not_before_regex("%[")):with_pair(cond.not_before_regex("%]")),
+                Rule("_", "_", { "org" }),
+                Rule("=", "=", { "org" }),
+                Rule("$", "$", { "org" }),
+            })
+
+            npairs.add_rules({
+                Rule("_", "_", { "markdown" }),
+                Rule("*", "*", { "markdown" }):with_pair(cond.not_before_regex("^$")),
+                Rule("$", "$", { "markdown" }),
+            })
+        end,
     },
 }
