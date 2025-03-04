50db679278dda3e58942f5cb2fc0ff5157021399
Price Hiller <price@price-hiller.com>
2025-03-04 15:33:48 -0600

    refactor(nvim): cleanup statuscol config

--- users/price/dots/.config/nvim/lua/plugins/configs/statuscol.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/statuscol.lua
@@ -1,39 +1,38 @@
 return {
     {
         "luukvbaal/statuscol.nvim",
-        opts = function()
+        config = function()
             local builtin = require("statuscol.builtin")
 
-            local std_condition = function(args)
-                return #vim.api.nvim_get_option_value("bufhidden", { buf = args.buf }) == 0
-            end
-
-            return {
+            require("statuscol").setup({
                 setopt = true,
                 relculright = false,
                 segments = {
-                    { text = { "%s" }, click = "v:lua.ScSa" },
-                    { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-                    { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
                     {
-                        -- Padding for a missing fold icon
-                        text = { " " },
+                        text = { "%s" },
+                        click = "v:lua.ScSa",
+                    },
+                    { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+                    {
+                        text = { builtin.foldfunc, " " },
+                        click = "v:lua.ScFa",
                         condition = {
                             function(args)
-                                return args.virtnum ~= 0
+                                return vim.api.nvim_get_option_value("foldcolumn", { win = args.win }) ~= "0"
                             end,
-                            std_condition,
                         },
                     },
                     {
                         text = { "▕" },
                         hl = "NonText",
                         condition = {
-                            std_condition,
+                            function(args)
+                                return #vim.api.nvim_get_option_value("bufhidden", { buf = args.buf }) == 0
+                            end,
                         },
                     },
                 },
-            }
+            })
         end,
     },
 }
