e05cc85be26fd7bbb880cdfbf51bc3d1e9c2434d
Price Hiller <price@price-hiller.com>
2025-03-15 13:36:38 -0500

    refactor(nvim): do not use `visual-whitespace` in `terminal` bufs

--- users/price/dots/.config/nvim/lua/plugins/configs/visual-whitespace.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/visual-whitespace.lua
@@ -1,16 +1,20 @@
 return {
     {
         "mcauley-penney/visual-whitespace.nvim",
-        event = "ModeChanged *:[vV]",
-        opts = {
-            highlight = { link = "visual-whitespace" },
-            excluded = {
-                filetypes = {
-                    "snacks_picker_list",
-                    "snacks_picker_input",
-                    "snacks_picker_preview",
-                }
-            }
-        },
+        config = function()
+            require("visual-whitespace").setup({
+                highlight = { link = "visual-whitespace" },
+                excluded = {
+                    filetypes = {
+                        "snacks_picker_list",
+                        "snacks_picker_input",
+                        "snacks_picker_preview",
+                    },
+                    buftypes = {
+                        "terminal",
+                    },
+                },
+            })
+        end,
     },
 }
