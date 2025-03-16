30607eb989b7135a77fc3aa907a17a52372b6a8e
Price Hiller <price@price-hiller.com>
2025-03-16 14:44:28 -0500

    fix(nvim): use org indent mode command over changing buf var

--- users/price/dots/.config/nvim/after/ftplugin/org.lua
+++ users/price/dots/.config/nvim/after/ftplugin/org.lua
@@ -1,7 +1,7 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.modeline = true
 vim.keymap.set("n", "<localleader>ff", function()
-    vim.b.org_indent_mode = not vim.b.org_indent_mode
+    vim.cmd[[Org indent_mode]]
 end, {
     buffer = true,
     desc = "Org: Toggle Indent Mode",
