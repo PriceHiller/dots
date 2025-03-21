12da05e2908bd8e641e4922cd9b5294577aa3328
Price Hiller <price@price-hiller.com>
2025-03-21 11:15:52 -0500

    feat(nvim): improve how `img-clip` chooses assets directory

--- users/price/dots/.config/nvim/lua/plugins/configs/img-clip.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/img-clip.lua
@@ -11,10 +11,35 @@ return {
         },
         config = function()
             require("img-clip").setup({
-                relative_to_current_file = true,
+                default = {
+                    relative_to_current_file = true,
+                    dir_path = function()
+                        local relpath_parts = {}
+                        local root = vim.fs.root(0, function(name, path)
+                            if not vim.list_contains(relpath_parts, path) then
+                                table.insert(relpath_parts, path)
+                            end
+                            return vim.list_contains({ "assets", ".git" }, name) and vim.fn.isdirectory(path) == 1
+                        end)
+                        local relpath = ""
+
+                        if root then
+                            relpath = string.rep("../", #relpath_parts - 1)
+                        else
+                            relpath = "./"
+                        end
+                        local assets_dir = relpath .. "assets/" .. vim.fn.fnameescape(vim.fn.expand("%:t:r")) .. "/"
+                        assets_dir = vim.fs.normalize(assets_dir, { expand_env = false })
+                        vim.print(assets_dir)
+                        return assets_dir
+                    end,
+                },
                 filetypes = {
                     typst = {
                         template = [[#image("$FILE_PATH")]],
+                    },
+                    org = {
+                        template = "[[file:$FILE_PATH]]",
                     },
                 },
             })
