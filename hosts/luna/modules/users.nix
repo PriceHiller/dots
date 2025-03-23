5023c111651a477fd88ac84fe4de80c4a1a358bc
Price Hiller <price@price-hiller.com>
2025-03-22 23:00:49 -0500

    refactor(hosts/luna): set root shell to zsh

--- hosts/luna/modules/users.nix
+++ hosts/luna/modules/users.nix
@@ -6,6 +6,7 @@
     defaultUserShell = pkgs.zsh;
     users = {
       root = {
+        shell = pkgs.zsh;
         hashedPasswordFile = config.age.secrets.users-root-pw.path;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkWsSntg1ufF40cALcIBA7WZhiU/f0cncqq0pcp+DZY openpgp:0x15993C90"
