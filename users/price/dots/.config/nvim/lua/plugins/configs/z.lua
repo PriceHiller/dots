26553962e858058a91265e89ebcdad41d8d9c758
Price Hiller <price@price-hiller.com>
2025-04-01 22:12:33 -0500

    feat(nvim): use `zoxide` in `z.nvim`

--- users/price/dots/.config/nvim/lua/plugins/configs/z.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/z.lua
@@ -2,6 +2,16 @@ return {
     {
         "PriceHiller/z.nvim",
         cmd = { "Z" },
-        config = true,
+        config = function()
+            require("z").setup({
+                z_cmd = function()
+                    return { "zoxide", "query", "--exclude", vim.fn.getcwd() }
+                end,
+                z_comp_cmd = function()
+                    return { "zoxide", "query", "--list", "--exclude", vim.fn.getcwd() }
+                end,
+                z_dir_changed_cmd = { "zoxide", "add" }
+            })
+        end,
     },
 }
