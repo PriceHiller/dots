48ef708d1fcc264c12613398a582c29ae43cdcb2
Price Hiller <price@price-hiller.com>
2025-04-12 00:54:42 -0500

    refactor(nvim): make `Neotest` use `<leader>t`

--- users/price/dots/.config/nvim/lua/plugins/configs/neotest.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/neotest.lua
@@ -10,23 +10,23 @@ return {
             "nvim-neotest/neotest-plenary",
         },
         keys = {
-            { "<leader>k", desc = "> Neotest" },
-            { "<leader>kr", "<cmd>Neotest run<CR>", desc = "Neotest: Run Test" },
-            { "<leader>kf", "<cmd>Neotest run file<CR>", desc = "Neotest: Run Test(s) in File" },
-            { "<leader>kl", "<cmd>Neotest run last<CR>", desc = "Neotest: Run Last Test(s)" },
-            { "<leader>kp", "<cmd>Neotest output-panel<CR>", desc = "Neotest: Output Panel" },
+            { "<leader>t", desc = "> Neotest" },
+            { "<leader>tr", "<cmd>Neotest run<CR>", desc = "Neotest: Run Test" },
+            { "<leader>tf", "<cmd>Neotest run file<CR>", desc = "Neotest: Run Test(s) in File" },
+            { "<leader>tl", "<cmd>Neotest run last<CR>", desc = "Neotest: Run Last Test(s)" },
+            { "<leader>tp", "<cmd>Neotest output-panel<CR>", desc = "Neotest: Output Panel" },
             {
-                "<leader>ko",
+                "<leader>to",
                 function()
                     local neotest = require("neotest")
                     neotest.output.open({ enter = true })
                 end,
                 desc = "Neotest: Output",
             },
-            { "<leader>kk", "<cmd>Neotest summary toggle<CR>", desc = "Neotest: Summary Toggle" },
-            { "<leader>kn", "<cmd>Neotest jump prev<CR>", desc = "Neotest: Jump Prev" },
-            { "<leader>kN", "<cmd>Neotest jump next<CR>", desc = "Neotest: Jump Next" },
-            { "<leader>ka", "<cmd>Neotest attach<CR>", desc = "Neotest: Attach" },
+            { "<leader>tk", "<cmd>Neotest summary toggle<CR>", desc = "Neotest: Summary Toggle" },
+            { "<leader>tn", "<cmd>Neotest jump prev<CR>", desc = "Neotest: Jump Prev" },
+            { "<leader>tN", "<cmd>Neotest jump next<CR>", desc = "Neotest: Jump Next" },
+            { "<leader>ta", "<cmd>Neotest attach<CR>", desc = "Neotest: Attach" },
         },
         config = function()
             vim.api.nvim_create_autocmd("BufWinEnter", {
