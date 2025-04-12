8da84f8b947978f2f6cece568b648d1adf321a23
Price Hiller <price@price-hiller.com>
2025-04-12 02:20:48 -0500

    refactor(hosts/luna): remove cachix for nix CI runner substituters

--- hosts/luna/modules/services/gitea.nix
+++ hosts/luna/modules/services/gitea.nix
@@ -21,13 +21,9 @@ let
     nixConf = {
       substituters = [
         "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        # insert any other binary caches here
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        # insert the public keys for those binary caches here
       ];
       # allow using the new flake commands in our workflows
       experimental-features = [
