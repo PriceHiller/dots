d2372b1c6f1ff1281c78edfaefe23f4dab2f78ec
Price Hiller <price@price-hiller.com>
2025-04-22 13:30:05 -0500

    refactor(nvim): do not tw break on normal content

--- users/price/dots/.config/nvim/lua/core/options.lua
+++ users/price/dots/.config/nvim/lua/core/options.lua
@@ -40,7 +40,7 @@ M.setup = function()
     opt.breakat = " \t;,[]()"
     opt.linebreak = true
     opt.formatlistpat = [[^\s*\(-\|\d\.\|+\)\s*]]
-    opt.formatoptions = "jtcroqnp"
+    opt.formatoptions = "jcroqnp"
     opt.breakindent = true
     opt.breakindentopt = "list:-1"
 
