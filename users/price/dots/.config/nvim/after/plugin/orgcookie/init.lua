af21bc9743a3826827c038b5f05322e7596e644d
Price Hiller <price@price-hiller.com>
2025-05-22 15:36:43 -0500

    refactor(nvim): minor improvements to orgcookie

--- users/price/dots/.config/nvim/after/plugin/orgcookie/init.lua
+++ users/price/dots/.config/nvim/after/plugin/orgcookie/init.lua
@@ -1,41 +1,25 @@
 local org = require("orgmode")
 
----@class OrgCookieWatcher
+---@class OrgCookieWatcher Updates Headline Cookies for Progress using Virtual Text
 ---@field private bufnr integer Buffer Watcher is attached to
 ---@field private attached boolean Whether the watcher is running
----@field private updating boolean
----@field private timer uv.uv_timer_t
 ---@field private ns_id integer
 local OrgCookieWatcher = {
     ns_id = vim.api.nvim_create_namespace("orgmode.ui.cookie"),
 }
 
----@type table<integer, OrgCookieWatcher>
+---@alias OrgCookieWatchers table<integer, OrgCookieWatcher> A mapping of buffer ids to watchers
+
+---@type OrgCookieWatchers
 local watchers = {}
 
----@return table<integer, OrgCookieWatcher>
+---Get all currently registered cookie watchers
+---@return OrgCookieWatchers
 function OrgCookieWatcher.watchers()
     return watchers
 end
 
---- Creates a new headline watcher
----@param bufnr? integer Buffer to watch, if unspecified then uses the current buffer
-function OrgCookieWatcher.new(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-    local watcher = OrgCookieWatcher.watchers()[bufnr]
-    if watcher then
-        return watcher
-    end
-    local this = setmetatable({
-        bufnr = bufnr,
-        attached = false,
-        updating = false,
-    }, { __index = OrgCookieWatcher })
-    watchers[this.bufnr] = this
-    return watchers[this.bufnr]
-end
-
---- Gets an existing OrgCookieWatcher for the given buffer if it exists
+---Gets an existing OrgCookieWatcher for the given buffer if it exists
 ---@param bufnr? integer Buffer to get the watcher for
 ---@return OrgCookieWatcher?
 function OrgCookieWatcher.get(bufnr)
@@ -44,9 +28,25 @@ function OrgCookieWatcher.get(bufnr)
     return watcher
 end
 
+---Creates a new headline watcher
+---@param bufnr? integer Buffer to watch, if unspecified then uses the current buffer
+function OrgCookieWatcher.new(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local watcher = OrgCookieWatcher.get(bufnr)
+    if watcher then
+        return watcher
+    end
+    local this = setmetatable({
+        bufnr = bufnr,
+        attached = false,
+    }, { __index = OrgCookieWatcher })
+    watchers[this.bufnr] = this
+    return watchers[this.bufnr]
+end
+
 ---@param headline OrgHeadline
 ---@return OrgHeadline[]
-function OrgCookieWatcher.parent_headlines(headline)
+function OrgCookieWatcher._parent_headlines(headline)
     local located_headlines = {}
     local count = 0
     while true do
@@ -65,8 +65,11 @@ end
 
 ---@param start_line integer
 ---@param end_line integer
-function OrgCookieWatcher:del_extmarks(start_line, end_line)
+function OrgCookieWatcher:_del_extmarks(start_line, end_line)
+    -- This gets us the last column of the line, we want to get all the extmarks from the first
+    -- column (0th column) to the very last column of the given range
     local end_col = vim.fn.col({ end_line + 1, "$" })
+
     local old_extmarks = vim.api.nvim_buf_get_extmarks(
         self.bufnr,
         self.ns_id,
@@ -80,15 +83,18 @@ function OrgCookieWatcher:del_extmarks(start_line, end_line)
 end
 
 ---@param headline OrgHeadline
-function OrgCookieWatcher:set_cookie(headline)
+function OrgCookieWatcher:_set_cookie(headline)
     local cookie = headline:get_cookie()
     if not cookie then
         local line = headline:node():start()
-        self:del_extmarks(line, line)
+        -- In the scenario where we are missing a cookie we want to make sure we've invalidated the
+        -- old extmarks if they exist
+        self:_del_extmarks(line, line)
         return
     end
 
-    local counts = self.get_checkbox_num(headline) or self.get_todo_num(headline)
+    -- We preference checkboxes for the count
+    local counts = self._get_checkbox_num(headline) or self._get_todo_num(headline)
     if not counts then
         return
     end
@@ -96,35 +102,48 @@ function OrgCookieWatcher:set_cookie(headline)
     local complete = counts[1]
     local total = counts[2]
 
-    local text = {
-        { "[", "@org.cookie.delimiter.left" },
-    }
-    if headline.file:get_node_text(cookie):find("%%") then
-        local num = tostring(((complete / total) * 100))
-        table.insert(text, { num, "@org.cookie.num" })
-        table.insert(text, { "%", "@org.cookie.sign.percent" })
-    else
-        table.insert(text, { tostring(complete), "@org.cookie.num.complete" })
-        table.insert(text, { "/", "@org.cookie.sign.div" })
-        table.insert(text, { tostring(total), "@org.cookie.num.total" })
-    end
+    local virt_text = {}
 
-    table.insert(text, { "]", "@org.cookie.delimiter.right" })
+    -- Now we build up our virtual cookie
+    table.insert(virt_text, { "[", "@org.cookie.delimiter.left" })
+    if headline.file:get_node_text(cookie):find("%%") then
+        -- Handling a percentage cookie, e.g. [90%]
+        local num = tostring(((complete / total) * 100))
+        table.insert(virt_text, { num, "@org.cookie.num" })
+        table.insert(virt_text, { "%", "@org.cookie.sign.percent" })
+    else
+        -- Handling an out of cookie, e.g. [10/12]
+        table.insert(virt_text, { tostring(complete), "@org.cookie.num.complete" })
+        table.insert(virt_text, { "/", "@org.cookie.sign.div" })
+        table.insert(virt_text, { tostring(total), "@org.cookie.num.total" })
+    end
+    table.insert(virt_text, { "]", "@org.cookie.delimiter.right" })
 
     local line, start_col, _ = cookie:start()
-    self:del_extmarks(line, line)
+    local end_line, end_col, _ = cookie:end_()
 
+    -- Ensure we wipe out the old extmark before setting the new one.
+    self:_del_extmarks(line, end_line)
+
+    -- The virtual cookie text we put in place
     vim.api.nvim_buf_set_extmark(self.bufnr, self.ns_id, line, start_col, {
-        virt_text_pos = "overlay",
+        virt_text_pos = "inline",
         hl_mode = "combine",
         priority = 200,
-        virt_text = text,
+        virt_text = virt_text,
+    })
+
+    -- This ensures the user can see the _actual_ cookie as well as the virtual cookie when relevant
+    vim.api.nvim_buf_set_extmark(self.bufnr, self.ns_id, line, start_col, {
+        hl_mode = "combine",
+        end_col = end_col,
+        conceal = "",
     })
 end
 
 ---@param headline OrgHeadline
 ---@return [integer, integer]?
-function OrgCookieWatcher.get_checkbox_num(headline)
+function OrgCookieWatcher._get_checkbox_num(headline)
     local section = headline:node():parent()
     if not section then
         return nil
@@ -155,7 +174,7 @@ end
 
 ---@param headline OrgHeadline
 ---@return [integer, integer]?
-function OrgCookieWatcher.get_todo_num(headline)
+function OrgCookieWatcher._get_todo_num(headline)
     -- Count done children headlines and total children with TODO keywords
     local children = headline:get_child_headlines()
     local headlines_with_todo = vim.tbl_filter(function(h)
@@ -175,7 +194,7 @@ function OrgCookieWatcher.get_todo_num(headline)
 end
 
 ---@param headline OrgHeadline
-function OrgCookieWatcher.update_cookies(headline)
+function OrgCookieWatcher._update_cookies(headline)
     if not headline:get_cookie() then
         return
     end
@@ -197,7 +216,7 @@ end
 
 ---@param start_line integer 0-index row to start from
 ---@param end_line integer 0-index row to end at
-function OrgCookieWatcher:update_cookies_in_range(start_line, end_line)
+function OrgCookieWatcher:_update_cookies_in_range(start_line, end_line)
     ---@type table<integer, OrgHeadline>
     local modified_headlines = {}
     for line = start_line, end_line, 1 do
@@ -208,14 +227,27 @@ function OrgCookieWatcher:update_cookies_in_range(start_line, end_line)
         end
     end
     for _, headline in pairs(modified_headlines) do
-        self:set_cookie(headline)
-        local parents = OrgCookieWatcher.parent_headlines(headline)
+        self:_set_cookie(headline)
+        local parents = OrgCookieWatcher._parent_headlines(headline)
         for _, parent in ipairs(parents) do
-            self:set_cookie(parent)
+            self:_set_cookie(parent)
         end
     end
 end
 
+---Get whether the watcher is currently attached
+---@return boolean
+function OrgCookieWatcher:is_attached()
+    return self.attached
+end
+
+---Get the registered buffer id for the watcher
+---@return integer
+function OrgCookieWatcher:get_bufnr()
+    return self.bufnr
+end
+
+---Starts the watcher for the watcher's buffer if it's not already attached
 function OrgCookieWatcher:attach()
     if self.attached then
         return
@@ -223,8 +255,14 @@ function OrgCookieWatcher:attach()
     self.attached = true
 
     watchers[self.bufnr] = self
-    self:update_cookies_in_range(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
+    self:_update_cookies_in_range(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
 
+    -- We cant use  `nvim_set_decoration_provider()` here because of a
+    -- `org.files.get_closest_headline` call made later "modifies" (reloads) some content which
+    -- causes the decoration provider to explode.
+    --
+    -- There's a way around this by playing entirely with the TS Nodes, but this seems fast enough
+    -- so it'll do.
     vim.api.nvim_buf_attach(self.bufnr, false, {
         on_lines = function(_, _, _, start_line, _, end_line)
             if not self.attached then
@@ -232,11 +270,11 @@ function OrgCookieWatcher:attach()
             end
 
             vim.schedule(function()
-                self:update_cookies_in_range(start_line, end_line)
+                self:_update_cookies_in_range(start_line, end_line)
             end)
         end,
         on_reload = function()
-            self:update_cookies_in_range(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
+            self:_update_cookies_in_range(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
         end,
         on_detach = function()
             self:delete()
@@ -244,24 +282,51 @@ function OrgCookieWatcher:attach()
     })
 end
 
+---Detaches the watcher if it's attached
 function OrgCookieWatcher:detach()
+    if not self.attached then
+        return
+    end
     self.attached = false
-    self:del_extmarks(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
+    self:_del_extmarks(0, vim.api.nvim_buf_line_count(self.bufnr) - 1)
 end
 
+---Toggles the attached state for the watcher
+function OrgCookieWatcher:toggle()
+    if self.attached then
+        self:detach()
+    else
+        self:attach()
+    end
+end
+
+---Completely removes the watcher, including from tracked watchers
 function OrgCookieWatcher:delete()
     self:detach()
     watchers[self.bufnr] = nil
 end
 
+-- We only want to create the autocmds for registering watchers once
 local ft_autocmd_created = false
 if not ft_autocmd_created then
     ft_autocmd_created = true
+
+    -- We want to create and attach a new watcher for every buffer
     vim.api.nvim_create_autocmd("FileType", {
         pattern = "org",
         callback = function(args)
             local watcher = OrgCookieWatcher.new(args.buf)
             watcher:attach()
+        end,
+    })
+
+    -- Ensure we clean up invalid watchers
+    vim.api.nvim_create_autocmd("BufDelete", {
+        callback = function(args)
+            local watcher = OrgCookieWatcher.get(args.buf)
+            if watcher then
+                watcher:delete()
+            end
         end,
     })
 end
