704629fa05028657fb92f72092f2eb2960b4c825
Price Hiller <price@price-hiller.com>
2025-05-24 19:44:18 -0500

    refactor(nvim): add types for statuscol.nvim

--- users/price/dots/.config/nvim/lua/plugins/configs/statuscol.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/statuscol.lua
@@ -4,9 +4,54 @@ return {
         config = function()
             local builtin = require("statuscol.builtin")
 
+            ---@class MyStatusColNvimArgsFold
+            ---@field width integer current width of the fold column
+            ---@field close string foldclose ""
+            ---@field open string foldopen ""
+            ---@field sep string foldsep " "
+
+            ---@class MyStatusColNvimArgs
+            ---@field lnum integer v:lnum
+            ---@field relnum integer v:relnum
+            ---@field virtnum integer v:virtnum
+            ---@field buf integer buffer handle of drawn window
+            ---@field win integer window handle of drawn window
+            ---@field actual_curbuf integer buffer handle of |g:actual_curwin|
+            ---@field actual_curwin integer window handle of |g:actual_curbuf|
+            ---@field nu boolean 'number' option value
+            ---@field rnu boolean 'relativenumber' option value
+            ---@field cul "line" | "screenline" | "number" | "both" 'cursorline' option value
+            ---@field empty boolean statuscolumn is currently empty
+            ---@field fold MyStatusColNvimArgsFold fold values
+            ---@field tick integer FFI display_tick value
+            ---@field wp any FFI win_T pointer handle
+
+            ---@class MyStatusColNvimSegArgsSign
+            ---@field name? string[] table of Lua patterns to match the legacy sign name against, example: { " .* " }
+            ---@field text? string[] table of Lua patterns to match the extmark sign text against, example: { " .* " }
+            ---@field namespace? string[] table of Lua patterns to match the extmark sign namespace against, example: { " .* " }
+            ---@field maxwidth? integer maximum number of signs that will be displayed in this segment
+            ---@field colwidth? integer number of display cells per sign in this segment
+            ---@field auto? boolean | string boolean or string indicating what will be drawn when no signs matching the pattern are currently placed in the buffer.
+            ---@field wrap? boolean when true, signs in this segment will also be drawn on the virtual or wrapped part of a line (when v:virtnum != 0).
+            ---@field fillchar? string character used to fill a segment with less signs than maxwidth
+            ---@field fillcharhl? string highlight group used for fillchar (SignColumn/CursorLineSign if omitted)
+            ---@field foldclosed? boolean when true, show signs from lines in a closed fold on the first line
+
+            ---@alias MyStatusColNvimSegArgsText (string | fun(args: MyStatusColNvimArgs): string)
+            ---@alias MyStatusColNvimSegArgsCond (boolean | fun(args: MyStatusColNvimArgs): boolean?)
+
+            ---@class MyStatusColNvimSegArgs
+            ---@field text MyStatusColNvimSegArgsText[] table of strings or functions returning a string
+            ---@field click? string %@ click function label, applies to each text element, example: "v:lua.ScFa"
+            ---@field hl? string %# highlight group label, applies to each text element
+            ---@field condition? MyStatusColNvimSegArgsCond[] table of booleans or functions returning a boolean
+            ---@field sign? MyStatusColNvimSegArgsSign sign values
+
             require("statuscol").setup({
                 setopt = true,
                 relculright = false,
+                ---@type MyStatusColNvimSegArgs[]
                 segments = {
                     {
                         sign = {
@@ -18,19 +63,49 @@ return {
                             builtin.not_empty,
                         },
                     },
-                    { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+                    {
+                        text = { builtin.lnumfunc },
+                        click = "v:lua.ScLa",
+                        condition = {
+                            function(args)
+                                if not args.rnu and not args.nu then
+                                    return false
+                                end
+
+                                local buftype = vim.bo[args.buf]
+                                if buftype ~= "terminal" then
+                                    return true
+                                end
+
+                                local wincfg = vim.api.nvim_win_get_config(args.win)
+                                vim.print(wincfg)
+                                if #wincfg.relative > 0 then
+                                    vim.print(
+                                        ("Win: %d"):format(args.win),
+                                        ("Buf: %d [%s]"):format(args.buf, buftype),
+                                        wincfg
+                                    )
+                                end
+                                return #wincfg.relative == 0
+                            end,
+                        },
+                    },
                     {
                         text = {
                             function(args)
+                                ---@type string
                                 local fold = builtin.foldfunc(args)
-                                if args.virtnum == 0 then
-                                    return fold
+                                if #fold > 0 then
+                                    return fold .. " "
                                 else
                                     return ""
                                 end
                             end,
                         },
                         click = "v:lua.ScFa",
+                        condition = {
+                            function(args) end,
+                        },
                     },
                     {
                         text = {
