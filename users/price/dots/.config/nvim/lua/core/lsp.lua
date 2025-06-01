8cbb45a730c3c13c75d519793856e96adf69dadc
Price Hiller <price@price-hiller.com>
2025-06-01 18:31:12 -0500

    refactor(nvim): ensure lsp servers are enabled

--- users/price/dots/.config/nvim/lua/core/lsp.lua
+++ users/price/dots/.config/nvim/lua/core/lsp.lua
@@ -88,6 +88,25 @@ M.setup = function()
             end
         end,
     })
+
+    -- Enable all language servers defined in `lsp/` & `after/lsp/`
+    for _, dir in ipairs({
+        "lsp",
+        "after/lsp",
+    }) do
+        local lspcfg_dir = ("%s/%s"):format(vim.fn.stdpath("config"), dir)
+        local fd = vim.uv.fs_scandir(lspcfg_dir)
+        if fd then
+            for lspcfg_file in
+                function()
+                    return vim.uv.fs_scandir_next(fd)
+                end
+            do
+                local lsp_name = lspcfg_file:gsub(".lua\z", "")
+                vim.lsp.enable(lsp_name)
+            end
+        end
+    end
 end
 
 return M
