c9dcd85ee2615287743f814e74c767b72bb3e547
Price Hiller <price@price-hiller.com>
2025-06-03 22:10:56 -0500

    refactor(nvim): remove `octo.nvim`

--- users/price/dots/.config/nvim/lua/plugins/configs/heirline.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/heirline.lua
@@ -155,7 +155,6 @@ return {
                         ["sshconfig"] = { icon = "󰴳", icon_color = colors.carpYellow },
                         ["sshdconfig"] = "sshconfig",
                         ["help"] = { icon = "󰋗", icon_color = colors.springGreen },
-                        ["octo"] = { icon = "", icon_color = colors.fujiWhite },
                     }
 
                     local buftype_overrides = {
