8a6be65ece3c0e8ad43a926037a8b125ccee8130
Price Hiller <price@price-hiller.com>
2025-07-09 17:47:23 -0500

    fix(nvim): wrap toggle term sh exec in quotes

--- users/price/dots/.config/nvim/after/ftplugin/sh.lua
+++ users/price/dots/.config/nvim/after/ftplugin/sh.lua
@@ -2,7 +2,7 @@ vim.opt_local.expandtab = false
 
 vim.keymap.set("n", "<localleader>fr", function()
     vim.cmd.write()
-    require("toggleterm").exec("bash " .. vim.api.nvim_buf_get_name(0))
+    require("toggleterm").exec(("bash '%s'"):format(vim.api.nvim_buf_get_name(0)))
 end, {
     buffer = true,
     desc = "Bash: Run Current Buffer",
