2833f3294d40d61fef9444353760df04d1a7b857
Price Hiller <price@price-hiller.com>
2025-07-09 18:32:23 -0500

    Revert "refactor(nvim): remove `nvim-ufo`"
    This reverts commit c772cf116621b56e677b09dd7c36f57de20424fe.

--- users/price/dots/.config/nvim/lua/plugins/configs/heirline.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/heirline.lua
@@ -634,7 +634,7 @@ return {
                     {
                         provider = function()
                             ---@diagnostic disable-next-line: undefined-field
-                            return vim.opt_local.foldlevel:get() .. " "
+                            return (vim.b.ufo_foldlevel or vim.opt_local.foldlevel:get()) .. " "
                         end,
                         hl = {
                             fg = colors.fujiWhite,
---
+++ users/price/dots/.config/nvim/lua/plugins/configs/nvim-ufo.lua
@@ -0,0 +1,125 @@
+vim.api.nvim_create_autocmd("BufReadPre", {
+    callback = function()
+        vim.b.ufo_foldlevel = 0
+    end,
+})
+
+---@param num integer Set the fold level to this number
+local set_buf_foldlevel = function(num)
+    vim.b.ufo_foldlevel = num
+    require("ufo").closeFoldsWith(num)
+end
+
+---@param num integer The amount to change the UFO fold level by
+local change_buf_foldlevel_by = function(num)
+    local foldlevel = vim.b.ufo_foldlevel or 0
+    -- Ensure the foldlevel can't be set negatively
+    if foldlevel + num >= 0 then
+        foldlevel = foldlevel + num
+    else
+        foldlevel = 0
+    end
+    if vim.b.ufo_foldlevel_max and foldlevel > vim.b.ufo_foldlevel_max then
+        return
+    end
+    set_buf_foldlevel(foldlevel)
+end
+
+return {
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        event = { "BufRead", "BufNewFile" },
+        keys = {
+            {
+                "zR",
+                function()
+                    require("ufo").openAllFolds()
+                end,
+                desc = "UFO: Open All Folds",
+            },
+            {
+                "zM",
+                function()
+                    require("ufo").closeAllFolds()
+                end,
+                desc = "UFO: Close All Folds",
+            },
+            {
+                "zm",
+                function()
+                    local count = vim.v.count
+                    if count == 0 then
+                        count = 1
+                    end
+                    change_buf_foldlevel_by(-count)
+                end,
+                desc = "UFO: Fold More",
+            },
+            {
+                "zr",
+                function()
+                    local count = vim.v.count
+                    if count == 0 then
+                        count = 1
+                    end
+                    change_buf_foldlevel_by(count)
+                end,
+                desc = "UFO: Fold Less",
+            },
+            {
+                "zS",
+                function()
+                    if vim.v.count == 0 then
+                        vim.notify("No foldlevel given to set!", vim.log.levels.WARN)
+                    else
+                        set_buf_foldlevel(vim.v.count)
+                    end
+                end,
+                desc = "UFO: Set Foldlevel",
+            },
+        },
+        opts = function()
+            vim.opt.foldlevel = 99
+
+            -- Show numbers for fold text
+            local handler = function(virtText, lnum, endLnum, width, truncate)
+                local newVirtText = {}
+                local suffix = ("... 󰁂 %d "):format(endLnum - lnum)
+                local sufWidth = vim.fn.strdisplaywidth(suffix)
+                local targetWidth = width - sufWidth
+                local curWidth = 0
+                for _, chunk in ipairs(virtText) do
+                    local chunkText = chunk[1]
+                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    if targetWidth > curWidth + chunkWidth then
+                        table.insert(newVirtText, chunk)
+                    else
+                        chunkText = truncate(chunkText, targetWidth - curWidth)
+                        local hlGroup = chunk[2]
+                        table.insert(newVirtText, { chunkText, hlGroup })
+                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        -- str width returned from truncate() may less than 2nd argument, need padding
+                        if curWidth + chunkWidth < targetWidth then
+                            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+                        end
+                        break
+                    end
+                    curWidth = curWidth + chunkWidth
+                end
+                table.insert(newVirtText, { suffix, "@label" })
+                return newVirtText
+            end
+
+            local ft_options = { norg = "", octo = "", NeogitStatus = "" }
+            return {
+                provider_selector = function(_, filetype, _)
+                    return ft_options[filetype] or { "treesitter", "indent" }
+                end,
+                fold_virt_text_handler = handler,
+            }
+        end,
+    },
+}
