a4a9958672ebf3b88156e3b87cd00d1fc7e0fcb7
Price Hiller <price@price-hiller.com>
2025-07-19 15:29:45 -0500

    feat(nvim): install ts context commentstring

---
+++ users/price/dots/.config/nvim/lua/plugins/configs/nvim-ts-context-commenstring.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        config = function()
+            require("ts_context_commentstring").setup({
+                enable_autocmd = false,
+            })
+            local prev_get_option = vim.filetype.get_option
+            vim.filetype.get_option = function(filetype, option)
+                return option == "commentstring"
+                        and require("ts_context_commentstring.internal").calculate_commentstring()
+                    or prev_get_option(filetype, option)
+            end
+        end,
+    },
+}
