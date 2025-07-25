c732856246a642cc10ee49bd2d4fddb90764a257
Price Hiller <price@price-hiller.com>
2025-07-25 11:55:23 -0500

    feat(clang): add some common clang tools

--- users/price/conf/clang/default.nix
+++ users/price/conf/clang/default.nix
@@ -3,5 +3,9 @@
   home.packages = with pkgs; [
     llvmPackages_20.clang
     llvmPackages_20.clang-tools
+    llvmPackages_20.libstdcxxClang
+    llvmPackages_20.libcxx
+    ninja
+    meson
   ];
 }
