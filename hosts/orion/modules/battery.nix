cbb3d391c21aa0fc0eef8d346f8f0226906bcbbd
Price Hiller <price@price-hiller.com>
2025-08-11 15:51:36 -0500

    feat(host/orion): enable `tlp`

---
+++ hosts/orion/modules/battery.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  services = {
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT0 = 95;
+      };
+    };
+  };
+}
