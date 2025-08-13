62c7cd8bd1c8c3588f40738efc07066f12b3d788
Price Hiller <price@price-hiller.com>
2025-08-13 15:08:23 -0500

    feat(hosts/luna): improve gpg wkd integration

--- hosts/luna/modules/services/nginx/default.nix
+++ hosts/luna/modules/services/nginx/default.nix
@@ -25,24 +25,36 @@
       forceSSL = true;
       enableACME = true;
       root = inputs.blog.packages.${pkgs.system}.default;
-      locations = {
-        "/" = {
-          extraConfig = ''
-            if ($request_uri ~ ^/(.*)\.html(\?|$)) {
-                return 302 /$1;
-            }
-            try_files $uri $uri.html $uri/ =404;
-          '';
-        };
-        "^~ /.well-known/openpgpkey/hu/" = {
-          alias = "${./gpg-wkd}/$1";
-          extraConfig = ''
-            default_type application/octet-stream;
-            add_header Access-Control-Allow-Origin "*";
+      locations =
+        let
+          wkd-default-cfg = ''
+            default_type "application/octet-stream";
+            add_header Access-Control-Allow-Origin * always;
             add_header Last-Modified "";
           '';
+        in
+        {
+          "/" = {
+            extraConfig = ''
+              if ($request_uri ~ ^/(.*)\.html(\?|$)) {
+                  return 302 /$1;
+              }
+              try_files $uri $uri.html $uri/ =404;
+            '';
+          };
+          "^~ /.well-known/openpgpkey/hu/" = {
+            alias = "${./gpg-wkd}/$1";
+            extraConfig = wkd-default-cfg;
+          };
+          "^~ /.well-known/openpgpkey/policy/hu/" = {
+            alias = "${./gpg-wkd}/$1";
+            extraConfig = wkd-default-cfg;
+          };
+          "= /.well-known/openpgpkey/policy" = {
+            extraConfig = wkd-default-cfg;
+            return = "200";
+          };
         };
-      };
     };
   };
 }
