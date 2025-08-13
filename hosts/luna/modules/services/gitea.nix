00e6ecb74fe11e7a07f4899a4cc6e9e9ed2c0c9e
Price Hiller <price@price-hiller.com>
2025-08-13 12:56:39 -0500

    Revert "refactor(luna/gitea): remove gitea runner"
    This reverts commit c6a29176788e87538eb808cd958292a9fbc7dbf9.

--- hosts/luna/modules/services/gitea.nix
+++ hosts/luna/modules/services/gitea.nix
@@ -1,11 +1,54 @@
 {
   config,
+  inputs,
+  pkgs,
+  lib,
   ...
 }:
 let
   gitea_host = "git.${config.networking.domain}";
+  # TODO: Move this docker image out to a separate package and NixOS Module
+  # Huge thank you to https://icewind.nl/entry/gitea-actions-nix/ -- wouldn't have figured this out
+  # without that post 🙂
+  base = import (inputs.nix + "/docker.nix") {
+    inherit pkgs;
+    name = "nix-ci-base";
+    extraPkgs = with pkgs; [
+      nodejs_20
+      bash
+    ];
+    nixConf = {
+      sandbox = "true";
+      experimental-features = [
+        "pipe-operators"
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+  runner = pkgs.dockerTools.buildImage {
+    name = "nix-runner";
+    tag = "latest";
+
+    fromImage = base;
+    fromImageName = null;
+    fromImageTag = "latest";
+
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = [ pkgs.coreutils-full ];
+      pathsToLink = [ "/bin" ]; # add coreutuls (which includes sleep) to /bin
+    };
+  };
 in
 {
+  virtualisation.oci-containers.containers = {
+    "nix-runner" = {
+      image = "nix-runner:latest";
+      imageFile = runner;
+      autoStart = false;
+    };
+  };
 
   age.secrets.gitea-db-pass = {
     owner = config.services.gitea.user;
@@ -61,6 +104,23 @@ in
         "repository.upload".FILE_MAX_SIZE = 1024;
       };
     };
+    gitea-actions-runner.instances = {
+      default = {
+        enable = true;
+        url = config.services.gitea.settings.server.ROOT_URL;
+        tokenFile = config.age.secrets.gitea-runner-token.path;
+        name = "Default";
+        settings = {
+          runner.capacity = 8;
+        };
+        labels = [
+          "default:docker://nix-runner:latest"
+          "nix:docker://nix-runner:latest"
+          "alpine:docker://alpine:latest"
+          "debian:docker://debian:latest"
+        ];
+      };
+    };
     nginx.virtualHosts."${gitea_host}" = {
       enableACME = true;
       forceSSL = true;
@@ -77,6 +137,13 @@ in
   };
 
   networking.firewall.allowedTCPPorts = [ config.services.gitea.settings.server.SSH_PORT ];
+
+  systemd.services.gitea-runner-default.serviceConfig.ExecStartPre = lib.mkBefore [
+    # HACK: Delay startup by 10 seconds. This should ensure that the gitea service is good to
+    # go. This will make deployments delay by 10 seconds though if the runner is changed. I'm
+    # willing to accept that trade off.
+    "${pkgs.coreutils}/bin/sleep 10"
+  ];
 
   environment.persistence.save.directories = [
     {
