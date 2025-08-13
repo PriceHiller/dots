401aa42c0d4b86e031c270b0cf31d139c99b3b36
Price Hiller <price@price-hiller.com>
2025-08-12 22:24:11 -0500

    refactor(nvim): disable conceal in markdown fts

--- users/price/dots/.config/nvim/after/ftplugin/markdown.lua
+++ users/price/dots/.config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.textwidth = 0
+vim.opt_local.conceallevel = 0
 
 vim.keymap.set("n", "<localleader>fr", "<cmd>GithubPreviewToggle<CR>", {
     buffer = true,
