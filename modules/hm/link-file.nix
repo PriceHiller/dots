14468022c15d94d4c15bbc3e29343d004a604cf2
Price Hiller <price@pricehiller.com>
2025-08-30 09:40:44 -0500

    refactor(mod/hm): include service name & target for `link` module

--- modules/hm/link-file.nix
+++ modules/hm/link-file.nix
@@ -36,6 +36,21 @@ in
                 default = "${name}";
                 description = "The (optionally with unexpanded variables) path to link to";
               };
+              svc-name = lib.mkOption {
+                type = lib.types.str;
+                default =
+                  let
+                    replaceNonAlnum =
+                      rep: str:
+                      (builtins.foldl' (x: y: if builtins.isString y then x + y else x + rep) "" (
+                        builtins.split "[^[:alnum:]]" str
+                      ));
+                    mkLinkName = targetPath: "hm-link-${(replaceNonAlnum "-" targetPath)}";
+                  in
+                  mkLinkName name;
+                description = "The full systemd service name.";
+                readOnly = true;
+              };
             };
           }
         )
@@ -45,19 +60,25 @@ in
 
   config =
     let
-      replaceNonAlnum =
-        rep: str:
-        (builtins.foldl' (x: y: if builtins.isString y then x + y else x + rep) "" (
-          builtins.split "[^[:alnum:]]" str
-        ));
-      mkLinkName = targetPath: "hm-link-${(replaceNonAlnum "-" targetPath)}";
-      eachEnabledLink =
-        f: lib.mapAttrs' (targetPath: config: lib.nameValuePair (mkLinkName targetPath) (f config)) cfg;
+      eachEnabledLink = f: lib.mapAttrs' (_: config: lib.nameValuePair config.svc-name (f config)) cfg;
     in
     {
+      systemd.user.targets.hm-link-files = {
+        Unit = {
+          Description = "Target to trigger symlink job for home manager";
+          Requires = [
+            "default.target"
+            "agenix.service"
+          ];
+          After = [
+            "default.target"
+            "agenix.service"
+          ];
+        };
+        Install.WantedBy = [ "default.target" ];
+      };
       systemd.user.services = eachEnabledLink (cfg: {
         Unit = {
-          After = [ "agenix.service" ];
           Description = "Link '${cfg.source}' -> '${cfg.target}' after expanding shell variables";
         };
         Service = {
@@ -76,7 +97,7 @@ in
               ${pkgs.coreutils}/bin/ln -sf "${source}" "${target}"
             ''}/bin/link";
         };
-        Install.WantedBy = [ "default.target" ];
+        Install.WantedBy = [ "hm-link-files.target" ];
       });
     };
 }
