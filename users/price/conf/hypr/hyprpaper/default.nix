f11a7394834d19756e43719fabfc91f4133c8b52
Price Hiller <price@pricehiller.com>
2025-08-30 09:41:12 -0500

    refactor(hyprpaper): use `link` to avoid gc removing wallpapers

--- users/price/conf/hypr/hyprpaper/default.nix
+++ users/price/conf/hypr/hyprpaper/default.nix
@@ -1,21 +1,29 @@
-{ ... }:
+{ config, ... }:
+let
+  wpapers-dir = "${config.xdg.dataHome}/wallpapers/";
+  eDP-1-wallpaper = "${wpapers-dir}/edp1.jpg";
+  default-wallpaper = "${wpapers-dir}/default.jpg";
+in
 {
-  services.hyprpaper =
-    let
-      eDP-1-wallpaper = builtins.toString ../wallpapers/Autumn-Leaves.jpg;
-      default-wallpaper = builtins.toString ../wallpapers/Nebula.jpg;
-    in
-    {
-      enable = true;
-      settings = {
-        preload = [
-          eDP-1-wallpaper
-          default-wallpaper
-        ];
-        wallpaper = [
-          "eDP-1,${eDP-1-wallpaper}"
-          ",${default-wallpaper}"
-        ];
-      };
+  link = {
+    "${eDP-1-wallpaper}".source = builtins.toString ../wallpapers/Autumn-Leaves.jpg;
+    "${default-wallpaper}".source = builtins.toString ../wallpapers/Nebula.jpg;
+  };
+  systemd.user.services.hyperpaper.Unit.After = [
+    config.wayland.systemd.target
+    "hm-link-files.target"
+  ];
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [
+        eDP-1-wallpaper
+        default-wallpaper
+      ];
+      wallpaper = [
+        "eDP-1,${eDP-1-wallpaper}"
+        ",${default-wallpaper}"
+      ];
     };
+  };
 }
