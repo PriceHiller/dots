f387ff6f17f0cdd2c74664766d627530565186b8
Price Hiller <price@pricehiller.com>
2025-09-23 01:03:50 -0500

    feat(hosts/luna): add github runners

---
+++ hosts/luna/modules/services/github-runner.nix
@@ -0,0 +1,31 @@
+{ pkgs, config, ... }:
+{
+  services.github-runners =
+    let
+      baseRunnerCfg = {
+        enable = true;
+        tokenFile = config.age.secrets.gh-token.path;
+        url = "https://github.com/phobost";
+        extraLabels = [
+          "nix"
+          "luna"
+        ];
+        extraPackages = with pkgs; [
+          nix
+          bash
+          coreutils-full
+          ripgrep
+          jq
+          curlFull
+        ];
+      };
+      baseRunners =
+        numBaseRunners:
+        (builtins.genList (num: {
+          name = "nix-${builtins.toString (num + 1)}";
+          value = baseRunnerCfg;
+        }) numBaseRunners)
+        |> builtins.listToAttrs;
+    in
+    baseRunners 8;
+}
