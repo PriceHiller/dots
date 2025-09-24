08f5b782a0e6c4728e7104c2e3617b8266849452
Price Hiller <price@pricehiller.com>
2025-09-24 01:01:50 -0500

    refactor(nvim): remove opts settings from rest.lua

--- users/price/dots/.config/nvim/lua/plugins/configs/rest.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/rest.lua
@@ -9,9 +9,5 @@ return {
     },
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            table.insert(opts.ensure_installed, "http")
-        end,
     },
 }
