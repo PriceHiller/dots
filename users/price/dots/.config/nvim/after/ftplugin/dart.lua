921c88b39ece90bcf1953789c7781656c8e794c0
Price Hiller <price@pricehiller.com>
2025-10-01 21:27:54 -0500

    feat(nvim): add keybind to show flutter outline

--- users/price/dots/.config/nvim/after/ftplugin/dart.lua
+++ users/price/dots/.config/nvim/after/ftplugin/dart.lua
@@ -6,3 +6,9 @@ end, {
     buffer = true,
     silent = true,
 })
+
+vim.keymap.set("n", "<localleader>ff", "<cmd>FlutterOutlineOpen<CR>", {
+    buffer = true,
+    silent = true,
+    desc = "Flutter: Open Outline",
+})
