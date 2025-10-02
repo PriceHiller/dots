494364517efc88f5fecec0621cd8368b75c0c970
Price Hiller <price@pricehiller.com>
2025-10-01 22:06:38 -0500

    refactor(nvim): various improvements to Ccc

--- users/price/dots/.config/nvim/lua/plugins/configs/highlight-colors.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/highlight-colors.lua
@@ -2,6 +2,9 @@ return {
     {
         "uga-rosa/ccc.nvim",
         event = { "BufReadPre", "BufNewFile" },
+        keys = {
+            { "<leader>kk", "<cmd>CccHighlighterToggle<CR>", desc = "Ccc: Toggle" },
+        },
         config = function()
             local ccc = require("ccc")
             ccc.setup({
@@ -9,6 +12,19 @@ return {
                     ccc.picker.hex,
                     ccc.picker.css_rgb,
                     ccc.picker.css_hsl,
+                    ccc.picker.css_oklab,
+                    ccc.picker.css_oklch,
+                    ccc.picker.ansi_escape(),
+                },
+                outputs = {
+                    (function()
+                        ccc.output.hex.setup({ uppercase = true })
+                        return ccc.output.hex
+                    end)(),
+                    ccc.output.css_rgb,
+                    ccc.output.css_hsl,
+                    ccc.output.css_oklab,
+                    ccc.output.css_oklch,
                 },
                 highlighter = {
                     auto_enable = true,
