03b29928bf94f28840351df92d7f178eb524c1c2
Price Hiller <price@pricehiller.com>
2025-10-04 13:50:56 -0500

    fix(nvim): do not decorate flutter outline

--- users/price/dots/.config/nvim/lua/plugins/configs/flutter-tools.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/flutter-tools.lua
@@ -6,6 +6,21 @@ return {
             "nvim-lua/plenary.nvim",
         },
         config = function()
+            vim.api.nvim_create_autocmd("BufWinEnter", {
+                pattern = "*Flutter Outline",
+                callback = function(args)
+                    local buf = args.buf
+                    for _, winnr in ipairs(vim.fn.win_findbuf(buf)) do
+                        local wo = vim.wo[winnr]
+                        wo.winbar = ""
+                        wo.wrap = false
+                        wo.statuscolumn = ""
+                        wo.foldcolumn = "0"
+                        wo.number = false
+                        wo.signcolumn = "no"
+                    end
+                end,
+            })
             require("flutter-tools").setup({
                 widget_guides = {
                     enabled = true,
