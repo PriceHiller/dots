dcb6c860927259ebb6a822ae50d065c65f098746
Price Hiller <price@pricehiller.com>
2025-10-07 14:57:06 -0500

    refactor(nvim): improve cmdbuf binds & options

--- users/price/dots/.config/nvim/lua/plugins/configs/cmdbuf.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/cmdbuf.lua
@@ -2,6 +2,13 @@ vim.api.nvim_create_autocmd({ "User" }, {
     group = vim.api.nvim_create_augroup("cmdbuf_setting", {}),
     pattern = { "CmdbufNew" },
     callback = function()
+        vim.bo.bufhidden = "wipe"
+        vim.keymap.set("n", "q", [[<Cmd>quit<CR>]], { nowait = true, buffer = true })
+        vim.keymap.set("n", "dd", require("cmdbuf").delete, { buffer = true })
+        vim.keymap.set({ "n", "i" }, "<C-c>", function()
+            return require("cmdbuf").cmdline_expr()
+        end, { buffer = true, expr = true })
+
         vim.keymap.set("n", "dd", require("cmdbuf").delete, { buffer = true })
     end,
 })
@@ -26,6 +33,12 @@ return {
                 "q?",
                 function()
                     require("cmdbuf").split_open(vim.o.cmdwinheight, { type = "vim/search/backward" })
+                end,
+            },
+            {
+                "q,",
+                function()
+                    require("cmdbuf").split_open(vim.o.cmdwinheight, { type = "lua/cmd" })
                 end,
             },
             {
