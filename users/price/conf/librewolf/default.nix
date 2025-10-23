d7b1b9bcad9456285806d860dc0ab0fef53918e5
Price Hiller <price@pricehiller.com>
2025-10-22 23:03:08 -0500

    refactor(librewolf): improved anti-fingerprinting setup

--- users/price/conf/librewolf/default.nix
+++ users/price/conf/librewolf/default.nix
@@ -5,6 +5,9 @@
   osConfig,
   ...
 }:
+let
+  concatComma = list: (lib.concatStringsSep "," list);
+in
 {
   xdg.mimeApps.defaultApplications = lib.mkIf (config.programs.librewolf.enable) {
     "default-web-browser" = [ "librewolf.desktop" ];
@@ -23,64 +26,91 @@
   };
   programs.librewolf = {
     enable = true;
-    settings = clib.attrsToMozillaPref {
-      identity.fxaccounts.enabled = true;
-      webgl.disabled = false;
-      browser.policies.runOncePerModification = {
-        extensionsInstall = [ ];
-        removeSearchEngines = [
-          "Bing"
-          "Amazon.com"
-          "eBay"
-          "Twitter"
-        ];
-        extensionsUninstall = [
-          "bing@search.mozilla.org"
-          "amazondotcom@search.mozilla.org"
-          "ebay@search.mozilla.org"
-          "twitter@search.mozilla.org"
-        ];
-      };
-      privacy = {
-        resistFingerprinting = true;
-        fingerprintingProtection.overrides = [
-          "+AllTargets"
-          "-CSSPrefersColorScheme"
-        ];
-        clearOnShutdown = {
-          history = false;
-          downloads = false;
-          cookies = false;
+    settings =
+      clib.attrsToMozillaPref {
+        identity.fxaccounts.enabled = true;
+        webgl.disabled = false;
+        browser.policies.runOncePerModification = {
+          extensionsInstall = [ ];
+          removeSearchEngines = [
+            "Bing"
+            "Amazon.com"
+            "eBay"
+            "Twitter"
+          ];
+          extensionsUninstall = [
+            "bing@search.mozilla.org"
+            "amazondotcom@search.mozilla.org"
+            "ebay@search.mozilla.org"
+            "twitter@search.mozilla.org"
+          ];
         };
-      };
-      sidebar = {
-        main.tools = "syncedtabs,history,bookmarks";
-        verticalTabs = true;
-        revamp = true;
-      };
-      font = {
-        default.x-western = "sans-serif";
-        minimum-size.x-western = 18;
-      };
-      network = {
-        cookie.lifetimePolicy = 0;
-        # We use the local DNS resolver, it should support encryption
-        trr =
-          let
-            dnscryptCfg = osConfig.services.dnscrypt-proxy;
-            dohCfg = dnscryptCfg.settings.local_doh;
-            dohUri = "https://${(builtins.elemAt dohCfg.listen_addresses 0)}${dohCfg.path}";
-          in
-          (lib.mkIf dnscryptCfg.enable {
-            mode = 3;
-            uri = dohUri;
-            custom_uri = dohUri;
-          });
-        dns = {
-          echconfig.enabled = true;
-          use_https_rr_as_altsvc = true;
+        privacy = {
+          resistFingerprinting = false;
+          fingerprintingProtection = {
+            overrides = concatComma [ "+AllTargets,+JSLocale,+FontVisibilityRestrictGenerics,-CSSPrefersColorScheme" ];
+            pbmode = true;
+            granularOverrides = builtins.toJSON [
+              {
+                firstPartyDomain = "localhost";
+                overrides = [ "-AllTargets" ];
+              }
+              {
+                firstPartyDomain = "dnscrypt.localhost";
+                overrides = [ "-AllTargets" ];
+              }
+              {
+                firstPartyDomain = "print.localhost";
+                overrides = [ "-AllTargets" ];
+              }
+              {
+                firstPartyDomain = "pricehiller.com";
+                overrides = [ "-AllTargets" ];
+              }
+            ];
+            remoteOverrides.enabled = true;
+          };
+          clearOnShutdown = {
+            history = false;
+            downloads = false;
+            cookies = false;
+          };
         };
+        sidebar = {
+          main.tools = concatComma [
+            "syncedtabs"
+            "history"
+            "bookmarks"
+          ];
+          verticalTabs = true;
+          revamp = true;
+        };
+        font = {
+          default.x-western = "sans-serif";
+          minimum-size.x-western = 18;
+        };
+        network = {
+          cookie.lifetimePolicy = 0;
+          # We use the local DNS resolver, it should support encryption
+          trr =
+            let
+              dnscryptCfg = osConfig.services.dnscrypt-proxy;
+              dohCfg = dnscryptCfg.settings.local_doh;
+              dohUri = "https://${(builtins.elemAt dohCfg.listen_addresses 0)}${dohCfg.path}";
+            in
+            (lib.mkIf dnscryptCfg.enable {
+              mode = 3;
+              uri = dohUri;
+              custom_uri = dohUri;
+            });
+          dns = {
+            echconfig.enabled = true;
+            use_https_rr_as_altsvc = true;
+          };
+        };
+      }
+      // {
+        "privacy.fingerprintingProtection" = true;
       };
-    };
   };
 }
