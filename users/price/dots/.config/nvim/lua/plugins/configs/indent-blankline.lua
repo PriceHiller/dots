c50b27be55c1fe0e971d9c7ab2cf0ff56f6c013e
Price Hiller <price@pricehiller.com>
2025-11-13 10:38:18 -0600

    refactor(nvim): remove dependency on `nvim-treesitter` for `indent-blankline`

--- users/price/dots/.config/nvim/lua/plugins/configs/indent-blankline.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/indent-blankline.lua
@@ -2,9 +2,6 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
         config = function()
             require("ibl").setup({
                 exclude = {
