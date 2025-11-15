3ae4c13c3e170c6713014de86008fa5b9645292f
Price Hiller <price@pricehiller.com>
2025-11-15 09:53:41 -0600

    refactor(nix): improve `repl.nix`

--- repl.nix
+++ repl.nix
@@ -1,10 +1,11 @@
 let
   flake = builtins.getFlake (toString ./.);
-  pkgs = import <nixpkgs> { };
+  pkgs = import flake.inputs.nixpkgs {};
   lib = pkgs.lib;
 in
 flake
 // {
   nc = flake.nixosConfigurations;
+  inherit pkgs;
   inherit lib;
 }
