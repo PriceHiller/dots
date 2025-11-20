41aa67a47844dee3f1603c9465232ce1a906a560
Price Hiller <price@pricehiller.com>
2025-11-20 14:52:49 -0600

    feat(hosts/luna): add phobost api (swe backend)

--- flake.nix
+++ flake.nix
@@ -61,6 +61,12 @@
       url = "https://big.oisd.nl/domainswild";
       flake = false;
     };
+    swe-project = {
+      url = "github:phobost/SWE-Shopping-App?dir=src/backend/api";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
     self.submodules = true;
     secrets = {
       url = ./secrets;
@@ -272,6 +278,7 @@
                 inputs.impermanence.nixosModules.impermanence
                 inputs.agenix.nixosModules.default
                 inputs.disko.nixosModules.disko
+                inputs.swe-project.nixosModules.phobost
                 {
                   config = inputs.secrets.secrets.${hostname};
                 }
