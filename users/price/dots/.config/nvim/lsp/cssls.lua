29f8754ff9c1cfd6801ff28d7c8b71227db7f093
Price Hiller <price@pricehiller.com>
2025-11-20 14:59:23 -0600

    refactor(nvim): disable `css-ls` formatting

--- users/price/dots/.config/nvim/lsp/cssls.lua
+++ users/price/dots/.config/nvim/lsp/cssls.lua
@@ -1,1 +1,16 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client or client.name ~= "cssls" then
+            return
+        end
+
+        -- Disable formatting
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+        client.server_capabilities.documentOnTypeFormattingProvider = nil
+    end,
+    desc = "LSP: CSS LS Modifications",
+})
+
 return {}
