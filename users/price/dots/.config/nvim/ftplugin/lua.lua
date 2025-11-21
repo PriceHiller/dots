9dd7f34ee516a1264bdc74bea651767808e8d501
Price Hiller <price@pricehiller.com>
2025-11-21 16:58:21 -0600

    feat(nvim): integrate snacks debug run for lua files

--- users/price/dots/.config/nvim/ftplugin/lua.lua
+++ users/price/dots/.config/nvim/ftplugin/lua.lua
@@ -4,14 +4,25 @@ opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
+local success, snacks = pcall(require, "snacks")
+if success then
+    vim.keymap.set({ "n", "v" }, "<localleader>fd", snacks.debug.run, {
+        buffer = true,
+        silent = true,
+        desc = "Evaluate lua with `Snacks.debug.run`",
+    })
+end
+
 vim.keymap.set("n", "<localleader>fr", ":%lua<CR>", {
     buffer = true,
     silent = true,
+    desc = "Evaluate lua file",
 })
 
 vim.keymap.set("v", "<localleader>fr", ":lua<CR>", {
     buffer = true,
     silent = true,
+    desc = "Evaluate selected lua",
 })
 
 opt.formatlistpat = require("utils.formatpat")
