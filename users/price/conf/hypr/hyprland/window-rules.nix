856688e114a4ba5e25b50d2060122b21eb3a46e1
Price Hiller <price@pricehiller.com>
2025-11-30 10:21:15 -0600

    fix(hyprland): do not use deprecated/removed windowrulev2 syntax

--- users/price/conf/hypr/hyprland/window-rules.nix
+++ users/price/conf/hypr/hyprland/window-rules.nix
@@ -1,12 +1,12 @@
 { ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    windowrulev2 = [
-      "opacity 0.0 override, class:^(xwaylandvideobridge)$"
-      "noanim, class:^(xwaylandvideobridge)$"
-      "noinitialfocus, class:^(xwaylandvideobridge)$"
-      "maxsize 1 1, class:^(xwaylandvideobridge)$"
-      "noblur, class:^(xwaylandvideobridge)$"
+    windowrule = [
+      "match:class ^(xwaylandvideobridge)$, opacity 0.0 override"
+      "match:class ^(xwaylandvideobridge)$, no_anim on"
+      "match:class ^(xwaylandvideobridge)$, no_initial_focus on"
+      "match:class ^(xwaylandvideobridge)$, max_size 1 1"
+      "match:class ^(xwaylandvideobridge)$, no_blur on"
     ];
   };
 }
