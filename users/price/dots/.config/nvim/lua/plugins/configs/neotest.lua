ed81f52cab9ffb13a37d68a7058a6d872a92a187
Price Hiller <price@pricehiller.com>
2025-11-30 10:12:31 -0600

    refactor(nvim): remove fixcursorhold.nvim

--- users/price/dots/.config/nvim/lua/plugins/configs/neotest.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/neotest.lua
@@ -10,7 +10,6 @@ return {
         "nvim-neotest/neotest",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
             "nvim-neotest/neotest-go",
             "mrcjkb/rustaceanvim",
