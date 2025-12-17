770d6e795e5cd5807527d3d6df35323161ca8006
Price Hiller <price@pricehiller.com>
2025-12-17 13:55:01 -0600

    Revert "refactor(nvim): remove orgmode notifications"
    This reverts commit f81fdb844957c9c884838ed3c3a2db5497a5e054.

--- users/price/dots/.config/nvim/lua/plugins/configs/org-mode.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/org-mode.lua
@@ -92,8 +92,8 @@ return {
                     vim.fn.stdpath("config") .. "/**/*",
                 },
                 notifications = {
-                    enabled = false,
-                    cron_enabled = false,
+                    enabled = true,
+                    cron_enabled = true,
                     repeater_reminder_time = reminder_times,
                     deadline_warning_reminder_time = reminder_times,
                     reminder_time = reminder_times,
@@ -186,6 +186,14 @@ return {
                     },
                 },
             })
+            -- NOTE: We want to ensure orgmode loads its files on entry, a lot of things depend on
+            -- them being loaded.
+            -- PERF: We defer this because it has a huge impact on start up time, better to wait a
+            -- bit before trying to load the files to keep Neovim from doing too much on startup.
+            vim.defer_fn(function()
+                org.files:ensure_loaded()
+            end, 500)
+
             vim.api.nvim_set_hl(0, "org_code_delimiter", { link = "@punctuation.delimiter" })
             vim.api.nvim_set_hl(0, "org_verbatim_delimiter", { link = "@punctuation.delimiter" })
             vim.api.nvim_set_hl(0, "org_italic_delimiter", { link = "@punctuation.delimiter" })
