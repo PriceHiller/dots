db61008495856a4c68b938b2d5d30be5abae683f
Price Hiller <price@pricehiller.com>
2026-01-16 15:15:46 -0600

    fix(nvim): ensure astro ls picks up workspace changes

--- users/price/dots/.config/nvim/lsp/astro.lua
+++ users/price/dots/.config/nvim/lsp/astro.lua
@@ -1,1 +1,31 @@
-return {}
+return {
+    -- Thanks https://github.com/withastro/language-tools/issues/958#issuecomment-2755897254
+    on_attach = function(client, _)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            group = vim.api.nvim_create_augroup("Astro-OnDidChange-External-Faile", { clear = true }),
+            desc = "Update Astro's language server with change events for non-astro files",
+            pattern = {
+                "*.js",
+                "*.ts",
+                "*.jsx",
+                "*.tsx",
+                "*.json",
+                "*.scss",
+                "*.css",
+                "*.mdx",
+                "*.md",
+            },
+            callback = function(ctx)
+                client.notify("workspace/didChangeWatchedFiles", {
+                    changes = {
+                        {
+                            uri = ctx.match,
+                            -- 1 = Created, 2 = Changed, 3 = Deleted
+                            type = 2,
+                        },
+                    },
+                })
+            end,
+        })
+    end,
+}
