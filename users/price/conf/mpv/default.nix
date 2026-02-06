913fc9a5456bba533ebe7d8de5be040d231542c5
Price Hiller <price@pricehiller.com>
2026-02-06 10:25:40 -0600

    feat(mpv): add `uosc` to mpv

--- users/price/conf/mpv/default.nix
+++ users/price/conf/mpv/default.nix
@@ -1,11 +1,32 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.mpv = {
     enable = true;
+    package = (
+      pkgs.mpv.override {
+        scripts = with pkgs.mpvScripts; [
+          thumbfast
+          sponsorblock
+          uosc
+        ];
+        mpv-unwrapped = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+          ffmpeg = pkgs.ffmpeg-full;
+        };
+      }
+    );
+    bindings = {
+      tab = "script-binding uosc/toggle-ui";
+      "?" = "script-binding uosc/keybinds";
+      "mbtn_right" = "script-binding uosc/toggle-ui";
+      "menu" = "script-binding uosc/menu";
+    };
     config = {
       keep-open = "yes";
       hwdec = "auto-safe";
       vo = "gpu";
+      osd-bar = "no";
+      border = "no";
       profile = "gpu-hq";
       gpu-context = "wayland";
     };
