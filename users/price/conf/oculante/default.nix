71089cd357a3d677914199bff91a9d541996f773
Price Hiller <price@pricehiller.com>
2026-02-07 11:02:24 -0600

    feat(oculante): re-enable oculante as default image viewer

--- users/price/conf/oculante/default.nix
+++ users/price/conf/oculante/default.nix
@@ -1,14 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, clib, ... }:
 {
   home.packages = with pkgs; [
-    # Waiting on https://github.com/NixOS/nixpkgs/pull/476565
-    # oculante
+    oculante
   ];
   xdg.mimeApps = {
-    defaultApplications = {
-      "image/png" = [ "oculante.desktop" ];
-      "image/bmp" = [ "oculante.desktop" ];
-      "image/jpeg" = [ "oculante.desktop" ];
-    };
+    defaultApplications = clib.getMimeDefaults pkgs.oculante "oculante.desktop";
   };
 }
