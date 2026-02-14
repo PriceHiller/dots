5f3926d83bb6d160e587ffd8d5e3b64851db1518
Price Hiller <price@pricehiller.com>
2026-02-14 16:50:55 -0600

    feat(hosts/luna): setup fail2ban

--- hosts/luna/modules/services/nginx/default.nix
+++ hosts/luna/modules/services/nginx/default.nix
@@ -1,7 +1,5 @@
 {
-  inputs,
   config,
-  pkgs,
   ...
 }:
 {
@@ -51,5 +49,82 @@
     80
     443
   ];
+
+  environment.etc = {
+    "fail2ban/filter.d/nginx-unauthorized.conf".text = ''
+      [Definition]
+      failregex = ^\{.*"status":40(1|3),.*"remote_addr":"<HOST>".*\}$
+      ignoreregex =
+    '';
+
+    "fail2ban/filter.d/nginx-direct-ip-access.conf".text = ''
+      [Definition]
+      failregex = ^\{.*"http_host":"\d+\.\d+\.\d+\.\d+.*?",.*"remote_addr":"<HOST>".*\}$
+      ignoreregex =
+    '';
+
+    "fail2ban/filter.d/nginx-404s.conf".text = ''
+      [Definition]
+      failregex = ^\{.*"status":404,.*"remote_addr":"<HOST>".*\}$
+      ignoreregex =
+    '';
+
+    "fail2ban/filter.d/nginx-empty-user-agent.conf".text = ''
+      [Definition]
+      failregex = ^\{.*"http_user_agent":"\s*"\,.*"remote_addr":"<HOST>".*\}$
+      ignoreregex =
+    '';
+  };
+
+  services.fail2ban = {
+    enable = true;
+    bantime-increment = {
+      enable = true;
+      overalljails = true;
+    };
+    ignoreIP = [
+      "192.168.0.0/16"
+      "127.0.0.1/8"
+      "::1"
+    ];
+    jails = {
+      nginx-too-many-404s.settings = {
+        enabled = true;
+        filter = "nginx-404s";
+        logpath = "/var/log/nginx/access.log";
+        backend = "auto";
+        maxretry = 5;
+        findtime = "30m";
+        bantime = "10m";
+      };
+      nginx-direct-ip-access.settings = {
+        enabled = true;
+        filter = "nginx-direct-ip-access";
+        logpath = "/var/log/nginx/access.log";
+        backend = "auto";
+        maxretry = 2;
+        findtime = "5m";
+        bantime = "5m";
+      };
+      nginx-unauthorized.settings = {
+        enabled = true;
+        filter = "nginx-unauthorized";
+        logpath = "/var/log/nginx/access.log";
+        backend = "auto";
+        maxretry = 3;
+        findtime = "1h";
+        bantime = "30m";
+      };
+      nginx-empty-user-agent.settings = {
+        enabled = true;
+        filter = "nginx-empty-user-agent";
+        logpath = "/var/log/nginx/access.log";
+        backend = "auto";
+        maxretry = 1;
+        findtime = "30m";
+        bantime = "5m";
+      };
+    };
+  };
 
 }
