f2f04c743b0136d371d82cd7892c569c8c6d3037
Price Hiller <price@pricehiller.com>
2026-02-14 18:34:20 -0600

    refactor(hosts/luna): disable anubis on blog

--- hosts/luna/modules/services/nginx/blog.nix
+++ hosts/luna/modules/services/nginx/blog.nix
@@ -1,46 +1,10 @@
 {
   inputs,
   pkgs,
-  config,
   ...
 }:
-let
-  anubisBlog = config.services.anubis.instances.blog;
-in
 {
   services = {
-    # Anubis set up according to
-    # https://anubis.techaro.lol/docs/admin/configuration/subrequest-auth
-    anubis.instances.blog = {
-      settings = {
-        TARGET = " ";
-        SERVE_ROBOTS_TXT = false;
-        BIND_NETWORK = "tcp";
-        BIND = ":8991";
-      };
-      policy = {
-        settings = {
-          status_codes = {
-            CHALLENGE = 200;
-            DENY = 403;
-          };
-        };
-        extraBots = [
-          {
-            name = "Tries to access PHP";
-            action = "DENY";
-            expression = ''
-              path.endsWith(".php")
-            '';
-          }
-          {
-            name = "Empty User Agent";
-            action = "DENY";
-            user_agent_regex = "^\s*$";
-          }
-        ];
-      };
-    };
     nginx.virtualHosts = {
       "pricehiller.com" = {
         default = true;
@@ -58,24 +22,10 @@ in
             '';
           in
           {
-            # This allows Anubis to check the connection
-            "^~ /.within.website" = {
-              extraConfig =
-                # nginx
-                ''
-                  proxy_pass http://localhost${anubisBlog.settings.BIND};
-                  proxy_set_header X-Real-IP $remote_addr;
-                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                  auth_request off;
-                '';
-            };
             "/" = {
               extraConfig =
                 # nginx
                 ''
-                  auth_request /.within.website/x/cmd/anubis/api/check;
-                  error_page 401 403 =200 /.within.website/?redir=$request_uri;
-
                   try_files $uri $uri.html $uri/ =404;
                   add_header Cache-Control "no-cache";
                   add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
