5f9e559de9b27bf98f9bb0c695dcc9364fcefb6e
Price Hiller <price@pricehiller.com>
2026-02-20 13:57:04 -0600

    feat(hosts/luna): put anubis in front of copyparty

--- hosts/luna/modules/services/copyparty.nix
+++ hosts/luna/modules/services/copyparty.nix
@@ -2,14 +2,11 @@
 let
   host = "127.0.0.1";
   port = 3210;
+  anubis = config.services.anubis.instances.copyparty;
 in
 {
   age.secrets = {
     copyparty-users-price-pw = {
-      owner = config.services.copyparty.user;
-      group = config.services.copyparty.group;
-    };
-    copyparty-users-jeff-pw = {
       owner = config.services.copyparty.user;
       group = config.services.copyparty.group;
     };
@@ -28,19 +25,18 @@ in
       rproxy = 1;
       unpost = 5 * 60;
       magic = true;
+      ah-alg = "argon2";
       shr = "/share";
       hash-mt = 12;
       shr-adm = "price";
       chpw-no = "price";
       chpw = true;
       chpw-len = 12;
+      usernames = true;
     };
     accounts = {
       price = {
         passwordFile = config.age.secrets.copyparty-users-price-pw.path;
-      };
-      jeff = {
-        passwordFile = config.age.secrets.copyparty-users-jeff-pw.path;
       };
     };
 
@@ -83,15 +79,6 @@ in
             dks = true;
           };
         };
-        "/jah" = {
-          path = "${basePath}/jeff";
-          flags = {
-            dks = true;
-          };
-          access = {
-            "rwdm." = [ "jeff" ];
-          };
-        };
       }
       |> builtins.mapAttrs (
         _: value:
@@ -102,36 +89,53 @@ in
       );
   };
 
+  services.anubis.instances.copyparty = {
+    settings = {
+      TARGET = "http://${host}:${builtins.toString port}";
+      BIND = ":8993";
+      BIND_NETWORK = "tcp";
+      DIFFICULTY = 12;
+    };
+    policy = {
+      settings = {
+        status_codes = {
+          CHALLENGE = 200;
+          DENY = 403;
+        };
+      };
+      extraBots = [
+        {
+          import = "(data)/common/allow-private-addresses.yaml";
+        }
+      ];
+    };
+  };
+
   services.nginx.virtualHosts = {
     "fs.pricehiller.com" = {
       forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://${host}:${builtins.toString port}";
-        extraConfig =
-          # nginx
-          ''
-            proxy_redirect off;
-            # disable buffering (next 4 lines)
-            proxy_http_version 1.1;
-            client_max_body_size 0;
-            proxy_buffering off;
-            proxy_request_buffering off;
-            # improve download speed from 600 to 1500 MiB/s
-            proxy_buffers 32 8k;
-            proxy_buffer_size 16k;
-            proxy_busy_buffers_size 24k;
+      locations = {
+        "/" = {
+          proxyPass = "http://localhost${anubis.settings.BIND}";
+          proxyWebsockets = true;
+          extraConfig =
+            # nginx
+            ''
+              # disable buffering
+              client_max_body_size 0;
+              proxy_buffering off;
+              proxy_request_buffering off;
+              # improve download speed from 600 to 1500 MiB/s
+              proxy_buffers 32 8k;
+              proxy_buffer_size 16k;
+              proxy_busy_buffers_size 24k;
 
-            proxy_set_header   Connection        "Keep-Alive";
-            proxy_set_header   Host              $host;
-            proxy_set_header   X-Real-IP         $remote_addr;
-            proxy_set_header   X-Forwarded-Proto $scheme;
-            proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;
-
-            uwsgi_read_timeout 24h;
-            uwsgi_send_timeout 24h;
-            client_body_timeout 24h;
-            send_timeout 24h;
-          '';
+              uwsgi_read_timeout 24h;
+              uwsgi_send_timeout 24h;
+              client_body_timeout 24h;
+              send_timeout 24h;
+            '';
+        };
       };
     };
   };
