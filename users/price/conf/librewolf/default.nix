42d8960d959e3c6adf92345b2415a0e624490adb
Price Hiller <price@pricehiller.com>
2026-04-20 12:12:07 -0500

    refactor(hm/price): ensure flatpak manifest use perma url

--- users/price/conf/librewolf/default.nix
+++ users/price/conf/librewolf/default.nix
@@ -47,7 +47,7 @@ in
       imports = [ pkgs.bwrapperPresets.desktop ];
       app.package = pkgs.librewolf;
       flatpak.manifestFile = pkgs.fetchurl {
-        url = "https://github.com/flathub/io.gitlab.librewolf-community/raw/refs/heads/master/io.gitlab.librewolf-community.json";
+        url = "https://raw.githubusercontent.com/flathub/io.gitlab.librewolf-community/05944558333309709fcb2408d01780fc95c13711/io.gitlab.librewolf-community.json";
         hash = "sha256-W6Lt4MCKewFa7dIGgxQ6ScnH3G8L1rxYGmkm5OZG8u4=";
       };
     };
--- users/price/conf/spotify/default.nix
+++ users/price/conf/spotify/default.nix
@@ -7,7 +7,7 @@
         pkgs.bwrapperPresets.desktop
       ];
       flatpak.manifestFile = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/flathub/com.spotify.Client/refs/heads/master/com.spotify.Client.json";
+        url = "https://raw.githubusercontent.com/flathub/com.spotify.Client/23a1c107d11bdfed6a66fa009e41c71003acd99c/com.spotify.Client.json";
         hash = "sha256-ULCoj/s+vZC6pLip1wgLmcv5naE+pCrZOoekFSeR5jM=";
       };
       mounts = {
