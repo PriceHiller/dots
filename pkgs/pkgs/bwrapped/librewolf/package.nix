b73c8d49d3ea954749efb458c0bf5278f73377e7
Price Hiller <price@pricehiller.com>
2026-04-30 10:54:14 -0500

    refactor(pkgs): create bwrapped spotify & librewolf

---
+++ pkgs/pkgs/bwrapped/librewolf/package.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+pkgs.mkBwrapper {
+  imports = [ pkgs.bwrapperPresets.desktop ];
+  app.package = pkgs.librewolf;
+  flatpak.manifestFile = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/flathub/io.gitlab.librewolf-community/05944558333309709fcb2408d01780fc95c13711/io.gitlab.librewolf-community.json";
+    hash = "sha256-W6Lt4MCKewFa7dIGgxQ6ScnH3G8L1rxYGmkm5OZG8u4=";
+  };
+}
--- users/price/conf/spotify/default.nix
+++ users/price/conf/spotify/default.nix
@@ -1,21 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = [
-    (pkgs.mkBwrapper {
-      app.package = pkgs.spotify;
-      imports = [
-        pkgs.bwrapperPresets.desktop
-      ];
-      flatpak.manifestFile = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/flathub/com.spotify.Client/23a1c107d11bdfed6a66fa009e41c71003acd99c/com.spotify.Client.json";
-        hash = "sha256-ULCoj/s+vZC6pLip1wgLmcv5naE+pCrZOoekFSeR5jM=";
-      };
-      mounts = {
-        readWrite = [
-          "$XDG_CACHE_HOME/spotify"
-          "$XDG_CONFIG_HOME/spotify"
-        ];
-      };
-    })
+    pkgs.bwrapped.spotify
   ];
 }
