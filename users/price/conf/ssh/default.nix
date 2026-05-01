a919df2c0ceb95eb8da850b96d9422de979b9568
Price Hiller <price@pricehiller.com>
2026-05-01 14:48:27 -0500

    fix(ssh): fix perms of written ssh config

--- users/price/conf/ssh/default.nix
+++ users/price/conf/ssh/default.nix
@@ -11,6 +11,26 @@ in
         run mkdir -p '${ssh-state-dir}/controllers'
         run chmod 0700 '${ssh-state-dir}'
       '';
+
+  # See https://github.com/nix-community/home-manager/issues/322#issuecomment-3730266609
+  # To ensure ssh picks up the right perms for `~/.ssh/config`
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/322
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
+  };
+
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
