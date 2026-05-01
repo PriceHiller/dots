2014d74172d4ac06729ad31b3861375df7c17d94
Price Hiller <price@pricehiller.com>
2026-05-01 14:49:14 -0500

    refactor(pkgs/opencode): permit access to nix

--- pkgs/pkgs/bwrapped/opencode/package.nix
+++ pkgs/pkgs/bwrapped/opencode/package.nix
@@ -12,10 +12,14 @@ pkgs.mkBwrapper {
 
   mounts = {
     readWrite = [
+      "$PWD"
       "\${XDG_CONFIG_HOME:-$HOME/.config}/opencode"
       "\${XDG_CACHE_HOME:-$HOME/.cache}/opencode"
       "\${XDG_DATA_HOME:-$HOME/.local/share}/opencode"
       "\${XDG_STATE_HOME:-$HOME/.local/state}/opencode"
+      "\${XDG_STATE_HOME:-$HOME/.local/cache}/nix"
+      "\${XDG_STATE_HOME:-$HOME/.local/share}/nix"
+      "\${XDG_STATE_HOME:-$HOME/.local/state}/nix"
     ];
 
     # Read-only binds.
@@ -24,6 +28,8 @@ pkgs.mkBwrapper {
       # operations use the user's identity. (XDG path per git's own rules.)
       "\${XDG_CONFIG_HOME:-$HOME/.config}/git"
       "\${HOME}/.gitconfig"
+      "$SSH_AUTH_SOCK"
+      "/nix/var"
     ];
   };
 
