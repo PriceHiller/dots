cf83f363a7bb7c8c429b7a4a1ace68101443a1ab
Price Hiller <price@pricehiller.com>
2026-05-11 11:07:42 -0500

    fix(nvim): ensure neovide can load on error

--- users/price/dots/.config/nvim/init.lua
+++ users/price/dots/.config/nvim/init.lua
@@ -1,2 +1,12 @@
 vim.loader.enable()
-require("main")
+
+-- This pcall dance is done to ensure Neovim isn't blocked on load for certain applications. For
+--
+-- instance, an immediate error causes Neovide to never show its main window, by delaying the error
+-- message for a short interval, we ensure it can appear.
+local success, err_msg = pcall(require, "main")
+if not success then
+    vim.defer_fn(function()
+        vim.notify(err_msg, vim.log.levels.ERROR)
+    end, 10)
+end
