d064203917952683414ac9c2e4aafbb3dd73f373
Price Hiller <price@pricehiller.com>
2026-05-18 12:46:01 -0500

    refactor(nvim): replace `codecompanion` with `opencode.nvim`

---
+++ users/price/dots/.config/nvim/lua/plugins/configs/opencode.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "sudo-tee/opencode.nvim",
+        config = function()
+            -- Default configuration with all available options
+            require("opencode").setup({
+                preferred_picker = "snacks", -- 'telescope', 'fzf', 'mini.pick', 'snacks', 'select', if nil, it will use the best available picker. Note mini.pick does not support multiple selections
+                preferred_completion = "blink", -- 'blink', 'nvim-cmp','vim_complete' if nil, it will use the best available completion
+                default_global_keymaps = true, -- If false, disables all default global keymaps
+                default_mode = "plan", -- 'build' or 'plan' or any custom configured. @see [OpenCode Agents](https://opencode.ai/docs/modes/)
+                keymap_prefix = "<leader>z", -- Default keymap prefix for global keymaps change to your preferred prefix and it will be applied to all keymaps starting with <leader>o
+                opencode_executable = "opencode", -- Name of your opencode binary
+            })
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    anti_conceal = { enabled = false },
+                    file_types = { "opencode_output" },
+                },
+                ft = { "opencode_output" },
+            },
+            "saghen/blink.cmp",
+            "folke/snacks.nvim",
+        },
+    },
+}
