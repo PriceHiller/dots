7b6e87c641e46553e14f9c859edce3d8762b8e7e
Price Hiller <price@pricehiller.com>
2026-05-28 16:56:58 -0500

    fix(nvim): seed lua's `math.randomseed`

--- users/price/dots/.config/nvim/init.lua
+++ users/price/dots/.config/nvim/init.lua
@@ -1,3 +1,13 @@
+-- This might look kind of cursed, but lua 5.1 (which Neovim uses) doesn't actually pre-seed the
+-- pseudo-random generator by default -- thus, we need to seed it with something that is
+-- non-deterministic
+--
+-- Without this, each call of `math.random` actually results in the exact same outputs across a
+-- session of Neovim
+--
+-- See https://www.lua.org/manual/5.1/manual.html#pdf-math.random
+math.randomseed(vim.uv.hrtime())
+
 vim.loader.enable()
 
 -- This pcall dance is done to ensure Neovim isn't blocked on load for certain applications. For
