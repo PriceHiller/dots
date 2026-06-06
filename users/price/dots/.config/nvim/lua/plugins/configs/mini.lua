d57941687dc07dc6c2b84c03b8e8c55b043a72ba
Price Hiller <price@pricehiller.com>
2026-06-06 16:25:15 -0500

    feat(nvim): setup `mini.ai`

--- users/price/dots/.config/nvim/lua/plugins/configs/mini.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/mini.lua
@@ -4,6 +4,7 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             require("mini.align").setup({})
+            require("mini.ai").setup({})
         end,
     },
 }
