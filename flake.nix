5da6f5848d3e6df27a0bee0b533e8d2a0c962d3f
Price Hiller <price@pricehiller.com>
2026-06-10 13:29:56 -0500

    Revert "refactor(rust): remove `fenix`"
    This reverts commit 606fc559ef079a223dbb852376221e59e78c92eb.

--- flake.nix
+++ flake.nix
@@ -23,6 +23,10 @@
     };
     resume.url = "git+https://git.pricehiller.com/price/resume";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?shallow=1";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
     };
--- users/price/conf/rust/default.nix
+++ users/price/conf/rust/default.nix
@@ -1,25 +1,35 @@
 {
+  inputs,
   config,
   pkgs,
   ...
 }:
+let
+  fenix = inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   home = {
     sessionVariables = {
       CARGO_HOME = "${config.xdg.dataHome}/cargo";
       RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
-      RUST_SRC_PATH = "${pkgs.rustPlatform.rustcSrc}";
+      RUST_SRC_PATH = "${fenix.complete.rust-src}/lib/rustlib/src/rust/library";
+      #  HACK: Specify openssl info for rust, this is really not a good idea, but it saves me from
+      #  writing per-project shell.nix or `nix-shell -p` nonsense. I'm willing to compromise for my
+      #  laziness.
       OPENSSL_DIR = "${pkgs.openssl.dev}";
       OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
       SCCACHE_SERVER_UDS = "${config.xdg.stateHome}/sccache.sock";
       SCCACHE_CACHE_SIZE = "40G";
     };
     packages = with pkgs; [
-      rustc
-      cargo
-      clippy
-      rustfmt
-      rust-analyzer
+      (fenix.complete.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ])
+      fenix.rust-analyzer
       cargo-audit
       cargo-deny
       cargo-watch
