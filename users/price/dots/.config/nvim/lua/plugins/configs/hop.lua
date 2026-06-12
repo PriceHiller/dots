0fced27f662293d5e41b70e0018735f5aac0d34f
Price Hiller <price@pricehiller.com>
2026-06-12 10:54:00 -0500

    feat(nvim): add special `hop` behavior in `neotest-summary`

--- users/price/dots/.config/nvim/lua/plugins/configs/hop.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/hop.lua
@@ -10,6 +10,7 @@ return {
                             "neo-tree",
                             "NeogitStatus",
                             "lazy",
+                            "neotest-summary"
                         }, vim.bo.filetype)
                     then
                         require("hop").hint_lines({})
