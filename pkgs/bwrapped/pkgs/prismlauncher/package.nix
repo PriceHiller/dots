9ab84b2bb0a2100185148498654498887b10e7a2
Price Hiller <price@pricehiller.com>
2026-06-18 19:10:31 -0500

    refactor(pkgs/bwrapped/prismlauncher): add needed read mounts

--- pkgs/bwrapped/pkgs/prismlauncher/package.nix
+++ pkgs/bwrapped/pkgs/prismlauncher/package.nix
@@ -19,11 +19,48 @@ pkgs.mkBwrapper {
   sockets = {
     wayland = true;
     pipewire = true;
+    x11 = true;
   };
-  dbus.enable = false;
-  flatpak.enable = false;
+  flatpak.enable = true;
+  dbus = {
+    enable = true;
+    session = {
+      broadcasts = [
+        "org.freedesktop.portal.Desktop=*@/org/freedesktop/portal/desktop"
+      ];
+      talks = [
+        "org.freedesktop.portal.Desktop"
+        "org.freedesktop.portal.Documents"
+        "org.freedesktop.portal.FileChooser"
+        "org.freedesktop.UDisks2"
+        "org.freedesktop.UPower"
+        "org.a11y.Bus"
+        "com.canonical.AppMenu.Registrar"
+        "org.gnome.SessionManager"
+      ];
+      calls = [
+        "org.freedesktop.portal.*=*@/org/freedesktop/portal/desktop"
+      ];
+    };
+  };
 
   mounts = {
+    read = [
+      # Needed for GPU acceleration
+      "/sys"
+      # For themeing support
+      ''$(readlink -f "/etc/profiles/per-user/$USER/share/themes")''
+      ''$(readlink -f "/etc/profiles/per-user/$USER/share/icons")''
+      ''$(readlink -f "$XDG_CONFIG_HOME/qt5ct/")''
+      ''$(readlink -f "$XDG_CONFIG_HOME/qt6ct/")''
+
+      # For reading mime handlers
+      ''$(readlink -f "$XDG_CONFIG_HOME/mimeapps.list")''
+    ];
+    readWrite = [
+      "$XDG_CACHE_HOME/PrismLauncher"
+      "$XDG_DATA_HOME/PrismLauncher"
+    ];
     sandbox = [
       {
         name = "config";
