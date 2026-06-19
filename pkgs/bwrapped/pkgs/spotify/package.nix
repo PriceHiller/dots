e1dc1c2793f5ea7ea72ad229db00e241f36a8da1
Price Hiller <price@pricehiller.com>
2026-06-18 19:10:55 -0500

    refactor(pkgs/bwrapped/spotify): add additional mounts

--- pkgs/bwrapped/pkgs/spotify/package.nix
+++ pkgs/bwrapped/pkgs/spotify/package.nix
@@ -9,8 +9,16 @@ pkgs.mkBwrapper {
     hash = "sha256-ULCoj/s+vZC6pLip1wgLmcv5naE+pCrZOoekFSeR5jM=";
   };
   mounts = {
+    read = [
+      # Needed for GPU acceleration
+      "/sys"
+      # For themeing support
+      ''$(readlink -f "/etc/profiles/per-user/$USER/share/themes")''
+      ''$(readlink -f "/etc/profiles/per-user/$USER/share/icons")''
+    ];
     readWrite = [
       "$XDG_CACHE_HOME/spotify"
+      "$XDG_CACHE_HOME/fontconfig"
       "$XDG_CONFIG_HOME/spotify"
     ];
   };
