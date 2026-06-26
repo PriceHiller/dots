3e04487f764ed5860e538ff460941abd2cda93d4
Price Hiller <price@pricehiller.com>
2026-06-26 13:10:02 -0500

    refactor(nvim): guard `on_type_formatting` for old versions

--- users/price/dots/.config/nvim/plugin/lsp/lsp.lua
+++ users/price/dots/.config/nvim/plugin/lsp/lsp.lua
@@ -1,5 +1,7 @@
 -- Enable lsp on type formatting for all clients by default
-vim.lsp.on_type_formatting.enable()
+if vim.lsp.on_type_formatting then
+    vim.lsp.on_type_formatting.enable()
+end
 
 -- Enable lsp inlay hints on all clients by default
 vim.lsp.inlay_hint.enable(true)
