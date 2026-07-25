da6361bf14a08019288df52775ab7a2b18d48279
Price Hiller <price@pricehiller.com>
2026-07-24 23:18:05 -0500

    perf(nvim): enable lang servers asynchronously

--- users/price/dots/.config/nvim/after/plugin/load-lang-servers.lua
+++ users/price/dots/.config/nvim/after/plugin/load-lang-servers.lua
@@ -7,32 +7,37 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
             "after/lsp",
         }) do
             local lspcfg_dir = ("%s/%s"):format(vim.fn.stdpath("config"), dir)
-            local fd = vim.uv.fs_scandir(lspcfg_dir)
-            if fd then
+            vim.uv.fs_scandir(lspcfg_dir, function(_, fd)
+                if not fd then
+                    return
+                end
+
                 for lspcfg_file in
                     function()
                         return vim.uv.fs_scandir_next(fd)
                     end
                 do
-                    local lsp_name = lspcfg_file:gsub(".lua\z", "")
-                    local success, err_msg = pcall(vim.lsp.enable, lsp_name)
-                    if not success then
-                        vim.notify(
-                            table.concat({
+                    local lsp_name = lspcfg_file:gsub("%.lua", "")
+                    vim.schedule(function()
+                        local success, err_msg = pcall(vim.lsp.enable, lsp_name)
+                        if not success then
+                            vim.notify(
+                                table.concat({
 
-                                "==========",
-                                ("Failed to enable language server: '%s'"):format(lsp_name),
-                                "----------",
-                                "",
-                                err_msg,
-                                "----------",
-                                "",
-                            }, "\n"),
-                            vim.log.levels.WARN
-                        )
-                    end
+                                    "==========",
+                                    ("Failed to enable language server: '%s'"):format(lsp_name),
+                                    "----------",
+                                    "",
+                                    err_msg,
+                                    "----------",
+                                    "",
+                                }, "\n"),
+                                vim.log.levels.WARN
+                            )
+                        end
+                    end)
                 end
-            end
+            end)
         end
 
         return true
