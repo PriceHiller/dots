9f4f94315b37766dac0a5d646058568707f35eee
Price Hiller <price@pricehiller.com>
2026-08-01 09:31:01 -0500

    refactor(nvim): remove unneeded lua_ls hack in LazyDev

--- users/price/dots/.config/nvim/lua/plugins/configs/lazydev.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/lazydev.lua
@@ -3,21 +3,6 @@ return {
         "folke/lazydev.nvim",
         cmd = "LazyDev",
         ft = "lua",
-        init = function()
-            vim.lsp.config("lua_ls", {
-                settings = {
-                    Lua = {
-                        workspace = {
-                            library = {
-                                -- HACK: Ensure lazydev is loaded to pick up Neovim runtime
-                                -- see https://github.com/folke/lazydev.nvim/issues/136
-                                vim.api.nvim_get_runtime_file("", true),
-                            },
-                        },
-                    },
-                },
-            })
-        end,
         opts = {
             library = {
                 "lazy.nvim",
