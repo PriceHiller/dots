00a245bd2931b8fccfc866934936aa994e7661c4
Price Hiller <price@pricehiller.com>
2026-08-02 21:24:49 -0500

    fix(orion/sec/polkit): use nix settings for polkit

--- hosts/orion/modules/security/polkit/default.nix
+++ hosts/orion/modules/security/polkit/default.nix
@@ -5,17 +5,6 @@ let
   stripExt = name: name |> lib.removeSuffix ".rules" |> lib.removeSuffix ".js";
 in
 {
-  # See `man 5 polkitd.conf`
-  # https://man.archlinux.org/man/polkitd.conf.5.en
-  environment.etc."polkit-1/polkitd.conf".text = (
-    lib.generators.toINI { } {
-      Polkitd = {
-        # Increase default expiration time for polkit auth
-        ExprationSeconds = 60 * 60;
-      };
-    }
-  );
-
   ext.polkit.rules =
     rulesDir
     |> lib.filesystem.listFilesRecursive
@@ -31,5 +20,14 @@ in
 
   security.polkit = {
     enable = true;
+    settings = {
+      # See `man 5 polkitd.conf`
+      # https://man.archlinux.org/man/polkitd.conf.5.en
+      Polkitd = {
+        # Increase default expiration time for polkit auth
+        ExprationSeconds = 60 * 60;
+      };
+
+    };
   };
 }
