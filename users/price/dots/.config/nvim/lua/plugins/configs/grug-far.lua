086645ca784d0055ca91ddaa38972925ebd53ea6
Price Hiller <price@pricehiller.com>
2026-09-07 08:48:17 -0500

    refactor(nvim): change grug-far trigger keys

--- users/price/dots/.config/nvim/lua/plugins/configs/grug-far.lua
+++ users/price/dots/.config/nvim/lua/plugins/configs/grug-far.lua
@@ -3,7 +3,7 @@ return {
         "MagicDuck/grug-far.nvim",
         lazy = false,
         keys = {
-            { ",,", "<cmd>GrugFar<CR>", desc = "GrugFar: Open" },
+            { "<leader>,", "<cmd>GrugFar<CR>", desc = "GrugFar: Open" },
         },
         config = function()
             require("grug-far").setup({})
